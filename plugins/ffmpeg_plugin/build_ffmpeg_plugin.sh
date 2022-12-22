2e056b71bbbb15071bc2155a06bd68085cd0e4dd
SUN, Jing <jing.a.sun@intel.com>
2022-12-22 17:04:47 +0800

    ffmpeg: Add the steps to connect kahawai with openh264 encoder (#123)
    * plugin/ffmpeg: add the steps to connect kahawai with openh264 encoder
    
    Signed-off-by: Jing Sun <jing.a.sun@intel.com>
    (cherry picked from commit e4750e9fab321f6d4ebe60201347c0a9df92abe2)

--- plugins/ffmpeg_plugin/build_ffmpeg_plugin.sh
+++ plugins/ffmpeg_plugin/build_ffmpeg_plugin.sh
@@ -2,6 +2,19 @@
 
 EX_PATH=${PWD}
 
+build_openh264(){
+    if [ ! -d "./openh264" ];then
+        git clone https://github.com/cisco/openh264.git
+    fi
+
+    cd ./openh264
+    git checkout -b openh264v2.3.1 origin/openh264v2.3.1
+    make -j32
+    sudo make install
+    sudo ldconfig
+    cd ../
+}
+
 build_ffmpeg(){
     if [ ! -d "./ffmpeg" ];then
         git clone https://git.ffmpeg.org/ffmpeg.git
@@ -13,13 +26,15 @@ build_ffmpeg(){
     git reset --hard aa28df74ab197c49a05fecc40c81e0f8ec4ad0c3
     cp -f ../kahawai.c ./libavdevice/
     git am --whitespace=fix ../0001-avdevice-kahawai-Add-the-kahawai-input-device-plugin.patch
-    ./configure --enable-shared --disable-static --enable-nonfree --enable-pic --enable-gpl --enable-mtl
+    ./configure --enable-shared --disable-static --enable-nonfree --enable-pic --enable-gpl --enable-mtl --enable-libopenh264 --enable-encoder=libopenh264
     make clean
     make -j32
     sudo make install
+    sudo ldconfig
     cd ../
 }
 
+build_openh264
 build_ffmpeg
 
 echo "Building ffmpeg plugin is finished"
