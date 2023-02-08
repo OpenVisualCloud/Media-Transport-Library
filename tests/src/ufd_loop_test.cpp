56f78342e3055f00ef118308e942f76b91de743b
Frank Du <frank.du@intel.com>
2023-02-08 14:37:12 +0800

    test/ufd: add dual_loop and shared_max case
    also shared mempool for shared queue since mempool nb is limited
    
    Signed-off-by: Frank Du <frank.du@intel.com>

--- tests/src/ufd_loop_test.cpp
+++ tests/src/ufd_loop_test.cpp
@@ -13,6 +13,8 @@ struct loop_para {
   int max_rx_timeout_pkts;
   int tx_sleep_us;
   int rx_timeout_us;
+
+  bool dual_loop;
 };
 
 static int loop_para_init(struct loop_para* para) {
@@ -23,6 +25,7 @@ static int loop_para_init(struct loop_para* para) {
   para->max_rx_timeout_pkts = para->tx_pkts / 100;
   para->tx_sleep_us = 100;
   para->rx_timeout_us = 1000;
+  para->dual_loop = false;
   return 0;
 }
 
@@ -30,6 +33,7 @@ static int loop_sanity_test(struct utest_ctx* ctx, struct loop_para* para) {
   int sessions = para->sessions;
   uint16_t udp_port = para->udp_port;
   int udp_len = para->udp_len;
+  bool dual_loop = para->dual_loop;
 
   int tx_fds[sessions];
   int rx_fds[sessions];
@@ -52,6 +56,19 @@ static int loop_sanity_test(struct utest_ctx* ctx, struct loop_para* para) {
     EXPECT_GE(ret, 0);
     if (ret < 0) goto exit;
     tx_fds[i] = ret;
+
+    if (dual_loop) {
+      ret = mufd_bind(tx_fds[i], (const struct sockaddr*)&tx_addr[i], sizeof(tx_addr[i]));
+      EXPECT_GE(ret, 0);
+      if (ret < 0) goto exit;
+
+      struct timeval tv;
+      tv.tv_sec = 0;
+      tv.tv_usec = para->rx_timeout_us;
+      ret = mufd_setsockopt(tx_fds[i], SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv));
+      EXPECT_GE(ret, 0);
+      if (ret < 0) goto exit;
+    }
 
     ret = mufd_socket_port(AF_INET, SOCK_DGRAM, 0, MTL_PORT_R);
     EXPECT_GE(ret, 0);
@@ -98,6 +115,25 @@ static int loop_sanity_test(struct utest_ctx* ctx, struct loop_para* para) {
       ret = memcmp(recv_buf + payload_len, sha_result, SHA256_DIGEST_LENGTH);
       EXPECT_EQ(ret, 0);
       // test_sha_dump("upd_loop_sha", sha_result);
+
+      if (dual_loop) {
+        send = mufd_sendto(rx_fds[i], send_buf, sizeof(send_buf), 0,
+                           (const struct sockaddr*)&tx_addr[i], sizeof(tx_addr[i]));
+        EXPECT_EQ(send, sizeof(send_buf));
+        st_usleep(para->tx_sleep_us);
+
+        recv = mufd_recvfrom(tx_fds[i], recv_buf, sizeof(recv_buf), 0, NULL, NULL);
+        if (recv < 0) { /* timeout */
+          rx_timeout[i]++;
+          err("%s, back recv fail at session %d pkt %d\n", __func__, i, loop);
+          continue;
+        }
+        EXPECT_EQ(recv, sizeof(send_buf));
+        /* check sha */
+        SHA256((unsigned char*)recv_buf, payload_len, sha_result);
+        ret = memcmp(recv_buf + payload_len, sha_result, SHA256_DIGEST_LENGTH);
+        EXPECT_EQ(ret, 0);
+      }
     }
   }
 
@@ -138,6 +174,72 @@ TEST(Loop, multi_no_sleep) {
 
   loop_para_init(&para);
   para.sessions = 10;
+  para.tx_sleep_us = 0;
+  loop_sanity_test(ctx, &para);
+}
+
+TEST(Loop, multi_shared_max) {
+  struct utest_ctx* ctx = utest_get_ctx();
+  struct loop_para para;
+
+  if (!(ctx->init_params.mt_params.flags & MTL_FLAG_SHARED_QUEUE)) {
+    err("%s, skip as it's not shared mode\n", __func__);
+    return;
+  }
+
+  loop_para_init(&para);
+  para.sessions = mufd_get_sessions_max_nb() / 2;
+  para.tx_pkts = 32;
+  para.max_rx_timeout_pkts = para.tx_pkts / 2;
+  para.tx_sleep_us = 0;
+  loop_sanity_test(ctx, &para);
+}
+
+TEST(Loop, dual_single) {
+  struct utest_ctx* ctx = utest_get_ctx();
+  struct loop_para para;
+
+  loop_para_init(&para);
+  para.dual_loop = true;
+  loop_sanity_test(ctx, &para);
+}
+
+TEST(Loop, dual_multi) {
+  struct utest_ctx* ctx = utest_get_ctx();
+  struct loop_para para;
+
+  loop_para_init(&para);
+  para.dual_loop = true;
+  para.sessions = 5;
+  para.tx_sleep_us = 100;
+  loop_sanity_test(ctx, &para);
+}
+
+TEST(Loop, dual_multi_no_sleep) {
+  struct utest_ctx* ctx = utest_get_ctx();
+  struct loop_para para;
+
+  loop_para_init(&para);
+  para.dual_loop = true;
+  para.sessions = 10;
+  para.tx_sleep_us = 0;
+  loop_sanity_test(ctx, &para);
+}
+
+TEST(Loop, dual_multi_shared_max) {
+  struct utest_ctx* ctx = utest_get_ctx();
+  struct loop_para para;
+
+  if (!(ctx->init_params.mt_params.flags & MTL_FLAG_SHARED_QUEUE)) {
+    err("%s, skip as it's not shared mode\n", __func__);
+    return;
+  }
+
+  loop_para_init(&para);
+  para.dual_loop = true;
+  para.sessions = mufd_get_sessions_max_nb() / 2;
+  para.tx_pkts = 32;
+  para.max_rx_timeout_pkts = para.tx_pkts / 2;
   para.tx_sleep_us = 0;
   loop_sanity_test(ctx, &para);
 }
