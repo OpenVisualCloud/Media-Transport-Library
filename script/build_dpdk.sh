9bce3861d0cd2b2b73aad6b8a0b41d45ebdf3d94
Frank Du <frank.du@intel.com>
2023-02-13 09:12:53 +0800

    CI: use actions/checkout for DPDK code (#164)
    * script/build_dpdk: support version input
    usage: ./script/build_dpdk 22.12
    * ci/win: use env for the port
    * CI: not use build_dpdk.sh
    * dpdk_patches_build: disable format check
    * update git version before checkout
    * add kill KahawaiUfdTest also
    
    Signed-off-by: Frank Du <frank.du@intel.com>

--- script/build_dpdk.sh
+++ script/build_dpdk.sh
@@ -3,11 +3,22 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # Copyright 2022 Intel Corporation
 
-# Run from top, for CI job
-dpdk_ver=22.11
+set -e
 
-# check out dpdk code
-git clone https://github.com/DPDK/dpdk.git
+if [ -n "$1" ];  then
+  dpdk_ver=$1
+else
+  # default to latest 22.11
+  dpdk_ver=22.11
+fi
+
+echo "DPDK version: $dpdk_ver"
+
+if [ ! -d "dpdk" ];then
+  # check out dpdk code
+  echo "Clone DPDK source code"
+  git clone https://github.com/DPDK/dpdk.git
+fi
 cd dpdk
 git checkout v$dpdk_ver
 git switch -c v$dpdk_ver
