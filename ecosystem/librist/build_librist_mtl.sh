183d6809e5c9c1aa287e301439a0fb2029db2d7d
Frank Du <frank.du@intel.com>
2023-03-02 16:05:14 +0800

    librist: introduce the support (#190)
    * librist: introduce the support
    
    Signed-off-by: Frank Du <frank.du@intel.com>

---
+++ ecosystem/librist/build_librist_mtl.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright 2023 Intel Corporation
+
+set -e
+
+# check out librist code
+rm librist -rf
+git clone https://code.videolan.org/rist/librist.git
+cd librist
+
+# apply the patches
+git am ../*.patch
+
+# build now
+meson build -Duse_mtl=true
+ninja -C build
