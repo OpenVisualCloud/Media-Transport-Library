385ad61f87f09eac6fc5b1d740e0abf6e6c5c9fc
Ric Li <ming3.li@intel.com>
2023-03-22 15:48:55 +0800

    fix format printing
    Signed-off-by: Ric Li <ming3.li@intel.com>

--- lib/src/mt_dhcp.c
+++ lib/src/mt_dhcp.c
@@ -220,7 +220,7 @@ static int dhcp_recv_offer(struct mtl_main_impl* impl, struct mt_dhcp_hdr* offer
   mt_pthread_mutex_unlock(&dhcp_impl->mutex);
   info("%s(%d), received dhcp offer\n", __func__, port);
   info("%s(%d), ip address: %s\n", __func__, port,
-       inet_ntoa(*(struct in_addr*)&offer->yiaddr));
+       inet_ntoa(*(struct in_addr*)(char*)&offer->yiaddr));
   uint8_t* options = offer->options;
   while (*options != DHCP_OPTION_END) {
     if (*options == DHCP_OPTION_SUBNET_MASK)
@@ -425,7 +425,8 @@ int mt_dhcp_parse(struct mtl_main_impl* impl, struct mt_dhcp_hdr* hdr,
                   enum mtl_port port) {
   struct mt_dhcp_impl* dhcp_impl = get_dhcp(impl, port);
   if (ntohl(hdr->magic_cookie) != DHCP_MAGIC_COOKIE) {
-    err("%s(%d), invalid magic cookie %u\n", __func__, port, ntohl(hdr->magic_cookie));
+    err("%s(%d), invalid magic cookie 0x%08X\n", __func__, port,
+        ntohl(hdr->magic_cookie));
     return -EINVAL;
   }
 
@@ -435,7 +436,7 @@ int mt_dhcp_parse(struct mtl_main_impl* impl, struct mt_dhcp_hdr* hdr,
   }
 
   if (ntohl(hdr->xid) != dhcp_impl->xid) {
-    err("%s(%d), xid mismatch %u : %u\n", __func__, port, ntohl(hdr->xid),
+    err("%s(%d), xid mismatch 0x%08X : 0x%08X\n", __func__, port, ntohl(hdr->xid),
         dhcp_impl->xid);
     return -EINVAL;
   }
