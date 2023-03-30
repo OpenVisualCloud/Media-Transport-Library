3ed725d5d88650e48232910b37b2cd25be6467f5
Frank Du <frank.du@intel.com>
2023-03-30 14:55:18 +0800

    udp: add SOCK_NONBLOCK support
    Signed-off-by: Frank Du <frank.du@intel.com>

--- ld_preload/udp/udp_preload.h
+++ ld_preload/udp/udp_preload.h
@@ -67,6 +67,7 @@ struct upl_functions {
   int (*socket)(int domain, int type, int protocol);
   int (*close)(int sockfd);
   int (*bind)(int sockfd, const struct sockaddr* addr, socklen_t addrlen);
+  ssize_t (*send)(int sockfd, const void* buf, size_t len, int flags);
   ssize_t (*sendto)(int sockfd, const void* buf, size_t len, int flags,
                     const struct sockaddr* dest_addr, socklen_t addrlen);
   ssize_t (*sendmsg)(int sockfd, const struct msghdr* msg, int flags);
