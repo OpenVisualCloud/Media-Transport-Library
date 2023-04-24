03bf7cb2d774ab7fac2869c24432991508d7683b
Frank Du <frank.du@intel.com>
2023-04-24 13:34:19 +0800

    upl/test: add select, epoll, mix_fd case
    Signed-off-by: Frank Du <frank.du@intel.com>

--- tests/src/upl_loop_test.cpp
+++ tests/src/upl_loop_test.cpp
@@ -1,6 +1,10 @@
 /* SPDX-License-Identifier: BSD-3-Clause
- * Copyright(c) 2022 Intel Corporation
+ * Copyright(c) 2023 Intel Corporation
  */
+
+#include <signal.h>
+#include <sys/epoll.h>
+#include <sys/signalfd.h>
 
 #include "log.h"
 #include "upl_test.h"
@@ -17,6 +21,9 @@ struct loop_para {
   bool dual_loop;
   bool mcast;
   bool use_poll;
+  bool use_select;
+  bool use_epoll;
+  bool mix_fd;
 };
 
 static int loop_para_init(struct loop_para* para) {
@@ -31,6 +38,9 @@ static int loop_para_init(struct loop_para* para) {
   para->dual_loop = false;
   para->mcast = false;
   para->use_poll = false;
+  para->use_select = false;
+  para->use_epoll = false;
+  para->mix_fd = false;
   return 0;
 }
 
@@ -47,8 +57,9 @@ static int loop_sanity_test(struct uplt_ctx* ctx, struct loop_para* para) {
   struct sockaddr_in rx_addr[sessions];
   struct sockaddr_in tx_bind_addr[sessions]; /* for dual loop */
   struct sockaddr_in rx_bind_addr[sessions];
-  struct pollfd fds[sessions];
   int ret;
+  int epoll_fd = -1;
+  int sfd = -1;
 
   char send_buf[udp_len];
   char recv_buf[udp_len];
@@ -124,6 +135,33 @@ static int loop_sanity_test(struct uplt_ctx* ctx, struct loop_para* para) {
     }
   }
 
+  if (para->mix_fd) {
+    sigset_t set;
+    sigemptyset(&set);
+    sigaddset(&set, SIGINT);
+    sfd = signalfd(-1, &set, SFD_NONBLOCK);
+    EXPECT_GE(sfd, 0);
+  }
+
+  if (para->use_epoll) {
+    epoll_fd = epoll_create1(0);
+    EXPECT_GE(epoll_fd, 0);
+    for (int i = 0; i < sessions; i++) {
+      struct epoll_event ev;
+      ev.events = EPOLLIN;
+      ev.data.fd = rx_fds[i];
+      ret = epoll_ctl(epoll_fd, EPOLL_CTL_ADD, rx_fds[i], &ev);
+      EXPECT_GE(ret, 0);
+    }
+    if (sfd > 0) {
+      struct epoll_event ev;
+      ev.events = EPOLLIN;
+      ev.data.fd = sfd;
+      ret = epoll_ctl(epoll_fd, EPOLL_CTL_ADD, sfd, &ev);
+      EXPECT_GE(ret, 0);
+    }
+  }
+
   for (int loop = 0; loop < para->tx_pkts; loop++) {
     /* tx */
     for (int i = 0; i < sessions; i++) {
@@ -138,30 +176,83 @@ static int loop_sanity_test(struct uplt_ctx* ctx, struct loop_para* para) {
     }
     if (para->tx_sleep_us) st_usleep(para->tx_sleep_us);
 
-    if (para->use_poll) {
-      int poll_succ = 0;
-      int poll_retry = 0;
-      int max_retry = 10;
+    int poll_succ = 0;
+    int poll_retry = 0;
+    int max_retry = 10;
 
+    if (para->use_poll) {
       while (poll_retry < max_retry) {
+        struct pollfd fds[sessions + 1];
         memset(fds, 0, sizeof(fds));
         for (int i = 0; i < sessions; i++) {
           fds[i].fd = rx_fds[i];
           fds[i].events = POLLIN;
         }
-        ret = poll(fds, sessions, para->rx_timeout_us / 1000);
+        if (sfd > 0) {
+          fds[sessions].fd = sfd;
+          fds[sessions].events = POLLIN;
+          ret = poll(fds, sessions + 1, para->rx_timeout_us / 1000);
+        } else {
+          ret = poll(fds, sessions, para->rx_timeout_us / 1000);
+        }
         EXPECT_GE(ret, 0);
         poll_succ = 0;
         for (int i = 0; i < sessions; i++) {
           if (fds[i].revents) poll_succ++;
         }
-        dbg("%s, %d succ on sessions %d on %d\n", __func__, poll_succ, sessions,
+        dbg("%s, poll %d succ on sessions %d on %d\n", __func__, poll_succ, sessions,
             poll_retry);
-        if ((poll_succ >= sessions) && (poll_retry > 0)) break;
+        if (poll_succ >= sessions) break;
 
         poll_retry++;
         st_usleep(1000);
       }
+    } else if (para->use_select) {
+      while (poll_retry < max_retry) {
+        fd_set readfds;
+        FD_ZERO(&readfds);
+        int nfds = 0;
+        struct timeval timeout;
+        timeout.tv_sec = 0;
+        timeout.tv_usec = para->rx_timeout_us;
+        for (int i = 0; i < sessions; i++) {
+          FD_SET(rx_fds[i], &readfds);
+          if (rx_fds[i] > nfds) nfds = rx_fds[i];
+          dbg("%s, i %d fd %d\n", __func__, i, rx_fds[i]);
+        }
+        if (sfd > 0) {
+          FD_SET(sfd, &readfds);
+          if (sfd > nfds) nfds = sfd;
+        }
+        nfds += 1; /*  highest-numbered fd plus 1 */
+        ret = select(nfds, &readfds, NULL, NULL, &timeout);
+        EXPECT_GE(ret, 0);
+        dbg("%s, ret %d nfds %d\n", __func__, ret, nfds);
+        poll_succ = 0;
+        for (int i = 0; i < sessions; i++) {
+          if (FD_ISSET(rx_fds[i], &readfds)) poll_succ++;
+        }
+        dbg("%s, select %d succ on sessions %d on %d\n", __func__, poll_succ, sessions,
+            poll_retry);
+        if (poll_succ >= sessions) break;
+
+        poll_retry++;
+        st_usleep(1000);
+      }
+    } else if (para->use_epoll) {
+      while (poll_retry < max_retry) {
+        struct epoll_event events[sessions];
+        ret = epoll_wait(epoll_fd, events, sessions, para->rx_timeout_us / 1000);
+        EXPECT_GE(ret, 0);
+        poll_succ = ret;
+        dbg("%s, ret %d\n", __func__, ret);
+        if (poll_succ >= sessions) break;
+        poll_retry++;
+        st_usleep(1000);
+      }
+    }
+
+    if (para->use_poll || para->use_select || para->use_epoll) {
       /* expect 50% succ at least */
       EXPECT_GT(poll_succ, sessions / 2);
       dbg("%s, %d succ on sessions %d\n", __func__, poll_succ, sessions);
@@ -221,6 +312,14 @@ static int loop_sanity_test(struct uplt_ctx* ctx, struct loop_para* para) {
   }
 
 exit:
+  if (epoll_fd > 0) {
+    close(epoll_fd);
+    epoll_fd = -1;
+  }
+  if (sfd > 0) {
+    close(sfd);
+    sfd = -1;
+  }
   for (int i = 0; i < sessions; i++) {
     if (tx_fds[i] > 0) close(tx_fds[i]);
     if (rx_fds[i] > 0) {
@@ -259,6 +358,18 @@ TEST(Loop, poll_multi_no_sleep) {
   loop_sanity_test(ctx, &para);
 }
 
+TEST(Loop, poll_multi_mix_fd) {
+  struct uplt_ctx* ctx = uplt_get_ctx();
+  struct loop_para para;
+
+  loop_para_init(&para);
+  para.use_poll = true;
+  para.sessions = 10;
+  para.tx_sleep_us = 0;
+  para.mix_fd = true;
+  loop_sanity_test(ctx, &para);
+}
+
 TEST(Loop, dual_single) {
   struct uplt_ctx* ctx = uplt_get_ctx();
   struct loop_para para;
@@ -279,15 +390,6 @@ TEST(Loop, dual_multi_no_sleep) {
   loop_sanity_test(ctx, &para);
 }
 
-TEST(Loop, mcast_single) {
-  struct uplt_ctx* ctx = uplt_get_ctx();
-  struct loop_para para;
-
-  loop_para_init(&para);
-  para.mcast = true;
-  loop_sanity_test(ctx, &para);
-}
-
 TEST(Loop, mcast_multi) {
   struct uplt_ctx* ctx = uplt_get_ctx();
   struct loop_para para;
@@ -296,5 +398,51 @@ TEST(Loop, mcast_multi) {
   para.mcast = true;
   para.sessions = 5;
   para.tx_sleep_us = 100;
+  loop_sanity_test(ctx, &para);
+}
+
+TEST(Loop, select_multi_no_sleep) {
+  struct uplt_ctx* ctx = uplt_get_ctx();
+  struct loop_para para;
+
+  loop_para_init(&para);
+  para.use_select = true;
+  para.sessions = 10;
+  para.tx_sleep_us = 0;
+  loop_sanity_test(ctx, &para);
+}
+
+TEST(Loop, select_multi_mix_fd) {
+  struct uplt_ctx* ctx = uplt_get_ctx();
+  struct loop_para para;
+
+  loop_para_init(&para);
+  para.use_select = true;
+  para.sessions = 10;
+  para.tx_sleep_us = 0;
+  loop_sanity_test(ctx, &para);
+}
+
+TEST(Loop, epoll_multi_no_sleep) {
+  struct uplt_ctx* ctx = uplt_get_ctx();
+  struct loop_para para;
+
+  loop_para_init(&para);
+  para.use_epoll = true;
+  para.sessions = 10;
+  para.tx_sleep_us = 0;
+  para.mix_fd = true;
+  loop_sanity_test(ctx, &para);
+}
+
+TEST(Loop, epoll_multi_mix_fd) {
+  struct uplt_ctx* ctx = uplt_get_ctx();
+  struct loop_para para;
+
+  loop_para_init(&para);
+  para.use_epoll = true;
+  para.sessions = 10;
+  para.tx_sleep_us = 0;
+  para.mix_fd = true;
   loop_sanity_test(ctx, &para);
 }
