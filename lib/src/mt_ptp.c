6910430e4e9bc9d4dce77c93fce87bd898995d3a
gongxiao-intel <xiaoyan.gong@intel.com>
2023-07-21 11:11:09 +0000

    ptp: turn reading tx timestamp err message to dbg message.
    Because the first few reading tx timestamp attempts always failed,
    we have set total 50 attempts to tolerate the fails. So, it is
    unnecessary to print error messages of the first few attempts.
    Error message is required only when the number of attempts is
    larger than 50

--- lib/src/mt_ptp.c
+++ lib/src/mt_ptp.c
@@ -91,7 +91,7 @@ static inline int ptp_timesync_read_tx_time(struct mt_ptp_impl* ptp, uint64_t* t
   ret = rte_eth_timesync_read_tx_timestamp(port_id, &spec);
   ptp_timesync_unlock(ptp);
 
-  // if (ret < 0) err("%s(%d), err %d\n", __func__, port, ret);
+  if (ret < 0) dbg("%s(%d), err %d\n", __func__, port, ret);
   if (tai) *tai = mt_timespec_to_ns(&spec);
   return ret;
 }
