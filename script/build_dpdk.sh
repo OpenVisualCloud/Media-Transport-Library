aa371ba2f07848ee7756b00b2c7dd93c5f87d9d7
Frank Du <frank.du@intel.com>
2023-07-24 10:06:15 +0800

    fix build
    Signed-off-by: Frank Du <frank.du@intel.com>

--- script/build_dpdk.sh
+++ script/build_dpdk.sh
@@ -20,8 +20,7 @@ if [ ! -d "dpdk" ];then
   git clone https://github.com/DPDK/dpdk.git
 fi
 cd dpdk
-git checkout v"$dpdk_ver"
-git switch -c v"$dpdk_ver"
+git checkout -b v"$dpdk_ver"
 
 # apply the patches
 git am ../patches/dpdk/"$dpdk_ver"/*.patch
