61f49fcaca6037340e5c0ec19e5b85c1dec14654
Frank Du <frank.du@intel.com>
2023-08-11 14:14:29 +0800

    add uname -a info
    Signed-off-by: Frank Du <frank.du@intel.com>

--- script/status_report.sh
+++ script/status_report.sh
@@ -12,21 +12,27 @@ mkdir "$REPORT_DIR"
 echo "Collect dmesg:"
 sudo dmesg | tee "$REPORT_DIR"/kernel_log.txt > /dev/null
 
-echo "Collect kernel cmdline:" | tee "$REPORT_DIR"/system_info.txt
-sudo cat /proc/cmdline | tee -a "$REPORT_DIR"/system_info.txt > /dev/null
-echo "" | tee -a "$REPORT_DIR"/system_info.txt > /dev/null
+SYS_INFO_OUT=$REPORT_DIR/system_info.txt
 
-echo "Collect HugePages info:" | tee -a "$REPORT_DIR"/system_info.txt
-grep -i "HugePages" /proc/meminfo | tee -a "$REPORT_DIR"/system_info.txt > /dev/null
-echo "" | tee -a "$REPORT_DIR"/system_info.txt > /dev/null
+echo "Collect system info:" | tee "$SYS_INFO_OUT"
+uname -a | tee -a "$SYS_INFO_OUT" > /dev/null
+echo "" | tee -a "$SYS_INFO_OUT" > /dev/null
 
-echo "Collect status of dpdk-devbind:" | tee -a "$REPORT_DIR"/system_info.txt
-dpdk-devbind.py -s | tee -a "$REPORT_DIR"/system_info.txt > /dev/null
-echo "" | tee -a "$REPORT_DIR"/system_info.txt > /dev/null
+echo "Collect kernel cmdline:" | tee -a "$SYS_INFO_OUT"
+sudo cat /proc/cmdline | tee -a "$SYS_INFO_OUT" > /dev/null
+echo "" | tee -a "$SYS_INFO_OUT" > /dev/null
 
-echo "Collect iommu_groups:" | tee -a "$REPORT_DIR"/system_info.txt
-find /sys/kernel/iommu_groups/ -maxdepth 1 -mindepth 1 -type d | tee -a "$REPORT_DIR"/system_info.txt > /dev/null
-echo "" | tee -a "$REPORT_DIR"/system_info.txt > /dev/null
+echo "Collect HugePages info:" | tee -a "$SYS_INFO_OUT"
+grep -i "HugePages" /proc/meminfo | tee -a "$SYS_INFO_OUT" > /dev/null
+echo "" | tee -a "$SYS_INFO_OUT" > /dev/null
+
+echo "Collect status of dpdk-devbind:" | tee -a "$SYS_INFO_OUT"
+dpdk-devbind.py -s | tee -a "$SYS_INFO_OUT" > /dev/null
+echo "" | tee -a "$SYS_INFO_OUT" > /dev/null
+
+echo "Collect iommu_groups:" | tee -a "$SYS_INFO_OUT"
+find /sys/kernel/iommu_groups/ -maxdepth 1 -mindepth 1 -type d | tee -a "$SYS_INFO_OUT" > /dev/null
+echo "" | tee -a "$SYS_INFO_OUT" > /dev/null
 
 echo "Collect ethernet interface info:"
 for iface in $(ip -o link show | awk -F': ' '{print $2}')
@@ -35,19 +41,19 @@ do
         continue
     fi
 
-    echo "Collect ethtool info for $iface" | tee -a "$REPORT_DIR"/system_info.txt
-    sudo ethtool "$iface" | tee -a "$REPORT_DIR"/system_info.txt > /dev/null
-    ethtool -i "$iface" | tee -a "$REPORT_DIR"/system_info.txt > /dev/null
-    echo "" | tee -a "$REPORT_DIR"/system_info.txt > /dev/null
+    echo "Collect ethtool info for $iface" | tee -a "$SYS_INFO_OUT"
+    sudo ethtool "$iface" | tee -a "$SYS_INFO_OUT" > /dev/null
+    ethtool -i "$iface" | tee -a "$SYS_INFO_OUT" > /dev/null
+    echo "" | tee -a "$SYS_INFO_OUT" > /dev/null
 done
 
 echo "Collect ethernet lspci info:"
 ethernet_lines=$(lspci | grep -i ethernet)
 for pci_address in $(echo "$ethernet_lines" | awk '{print $1}')
 do
-    echo "Collect lspci info for $pci_address" | tee -a "$REPORT_DIR"/system_info.txt
-    sudo lspci -vv -s "$pci_address" | tee -a "$REPORT_DIR"/system_info.txt > /dev/null
-    echo "" | tee -a "$REPORT_DIR"/system_info.txt > /dev/null
+    echo "Collect lspci info for $pci_address" | tee -a "$SYS_INFO_OUT"
+    sudo lspci -vv -s "$pci_address" | tee -a "$SYS_INFO_OUT" > /dev/null
+    echo "" | tee -a "$SYS_INFO_OUT" > /dev/null
 done
 
 echo "Create $REPORT_DIR.tar"
