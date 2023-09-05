0cbeb4aa982914998356d906c03681a516b89816
Ric Li <ming3.li@intel.com>
2023-09-05 13:04:32 +0800

    rtcp/tx: use independent mempool and queue (#470)
    Signed-off-by: Ric Li <ming3.li@intel.com>

--- lib/src/mt_rtcp.c
+++ lib/src/mt_rtcp.c
@@ -6,6 +6,7 @@
 
 #include "mt_dev.h"
 #include "mt_log.h"
+#include "mt_queue.h"
 #include "mt_stat.h"
 #include "mt_util.h"
 
@@ -105,8 +106,7 @@ static int rtcp_tx_retransmit_rtp_packets(struct mt_rtcp_tx* tx, uint16_t seq,
 
   /* deep copy the mbuf then send */
   for (int i = 0; i < bulk; i++) {
-    struct rte_mbuf* copied = rte_pktmbuf_copy(
-        mbufs[i], mt_get_tx_mempool(tx->parent, tx->port), 0, UINT32_MAX);
+    struct rte_mbuf* copied = rte_pktmbuf_copy(mbufs[i], tx->mbuf_pool, 0, UINT32_MAX);
     if (!copied) {
       dbg("%s(%s), failed to copy mbuf\n", __func__, tx->name);
       tx->stat_rtp_retransmit_fail_nobuf += bulk - i;
@@ -122,7 +122,7 @@ static int rtcp_tx_retransmit_rtp_packets(struct mt_rtcp_tx* tx, uint16_t seq,
       rtp->row_length = htons(line1_length | ST20_RETRANSMIT);
     }
   }
-  send = mt_dev_tx_sys_queue_burst(tx->parent, tx->port, copy_mbufs, nb_rt);
+  send = mt_txq_burst(tx->mbuf_queue, copy_mbufs, nb_rt);
   if (send < nb_rt) {
     uint16_t burst_fail = nb_rt - send;
     rte_pktmbuf_free_bulk(&copy_mbufs[send], burst_fail);
@@ -368,6 +368,28 @@ struct mt_rtcp_tx* mt_rtcp_tx_create(struct mtl_main_impl* impl,
   tx->port = ops->port;
   tx->payload_format = ops->payload_format;
 
+  struct rte_mempool* pool =
+      mt_mempool_create(impl, ops->port, ops->name, ops->buffer_size, MT_MBUF_CACHE_SIZE,
+                        0, MTL_MTU_MAX_BYTES);
+  if (!pool) {
+    err("%s(%s), failed to create mempool for mt_rtcp_tx\n", __func__, ops->name);
+    mt_rtcp_tx_free(tx);
+    return NULL;
+  }
+  tx->mbuf_pool = pool;
+
+  struct mt_txq_flow flow;
+  memset(&flow, 0, sizeof(flow));
+  mtl_memcpy(&flow.dip_addr, &ops->udp_hdr->ipv4.dst_addr, MTL_IP_ADDR_LEN);
+  flow.dst_port = ntohs(ops->udp_hdr->udp.dst_port) - 1; /* rtp port */
+  struct mt_txq_entry* q = mt_txq_get(impl, ops->port, &flow);
+  if (!q) {
+    err("%s(%s), failed to create queue for mt_rtcp_tx\n", __func__, ops->name);
+    mt_rtcp_tx_free(tx);
+    return NULL;
+  }
+  tx->mbuf_queue = q;
+
   struct mt_u64_fifo* ring =
       mt_u64_fifo_init(ops->buffer_size, mt_socket_id(impl, ops->port));
   if (!ring) {
@@ -376,6 +398,7 @@ struct mt_rtcp_tx* mt_rtcp_tx_create(struct mtl_main_impl* impl,
     return NULL;
   }
   tx->mbuf_ring = ring;
+
   tx->ipv4_packet_id = 0;
   tx->ssrc = ops->ssrc;
   snprintf(tx->name, sizeof(tx->name) - 1, "%s", ops->name);
@@ -390,7 +413,10 @@ struct mt_rtcp_tx* mt_rtcp_tx_create(struct mtl_main_impl* impl,
 }
 
 void mt_rtcp_tx_free(struct mt_rtcp_tx* tx) {
-  mt_stat_unregister(tx->parent, rtcp_tx_stat, tx);
+  struct mtl_main_impl* impl = tx->parent;
+  enum mtl_port port = tx->port;
+
+  mt_stat_unregister(impl, rtcp_tx_stat, tx);
 
   tx->active = false;
 
@@ -400,6 +426,17 @@ void mt_rtcp_tx_free(struct mt_rtcp_tx* tx) {
     mt_fifo_mbuf_clean(tx->mbuf_ring);
     mt_u64_fifo_uinit(tx->mbuf_ring);
     tx->mbuf_ring = NULL;
+  }
+
+  if (tx->mbuf_queue) {
+    mt_txq_flush(tx->mbuf_queue, mt_get_pad(impl, port));
+    mt_txq_put(tx->mbuf_queue);
+    tx->mbuf_queue = NULL;
+  }
+
+  if (tx->mbuf_pool) {
+    mt_mempool_free(tx->mbuf_pool);
+    tx->mbuf_pool = NULL;
   }
 
   mt_rte_free(tx);
