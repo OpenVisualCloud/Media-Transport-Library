935137fb57314198e446cfe6d38fb384e4c0881a
Ric Li <ming3.li@intel.com>
2023-09-05 09:02:33 +0800

    use 14th bit of row_number
    Signed-off-by: Ric Li <ming3.li@intel.com>

--- include/st20_api.h
+++ include/st20_api.h
@@ -525,9 +525,10 @@ struct st22_rx_frame_meta {
  */
 #define ST20_SECOND_FIELD (0x1 << 15)
 /**
- * The retransmit bit in row_length shall be set to 1 if it is a retransmit packet.
+ * The retransmit bit in row_number shall be set to 1 if it is a retransmit packet.
+ * Do not use it when row number can be larger than 16383.
  */
-#define ST20_RETRANSMIT (0x1 << 15)
+#define ST20_RETRANSMIT (0x1 << 14)
 
 /**
  * A structure describing a st2110-20(video) rfc4175 rtp header, size: 20
