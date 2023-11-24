06333e94f857ca78cf5383fef06588b8e94e94e3
Ric Li <ming3.li@intel.com>
2023-11-24 17:34:30 +0800

    manager: clear flow rules when removing interface
    Signed-off-by: Ric Li <ming3.li@intel.com>

--- manager/mtl_interface.hpp
+++ manager/mtl_interface.hpp
@@ -11,9 +11,12 @@
 #include <xdp/xsk.h>
 #endif
 
+#include <net/if.h>
+
 #include <memory>
 #include <string>
 #include <unordered_map>
+#include <vector>
 
 #include "logging.hpp"
 
@@ -27,6 +30,11 @@ class mtl_interface {
   void log(const log_level& level, const std::string& message) const {
     logger::log(level, "[Interface " + std::to_string(ifindex) + "] " + message);
   }
+  int clear_flow_rules();
+#ifdef MTL_HAS_XDP_BACKEND
+  int load_xdp();
+  void unload_xdp();
+#endif
 
  public:
   mtl_interface(int ifindex);
@@ -39,70 +47,17 @@ class mtl_interface {
 mtl_interface::mtl_interface(int ifindex)
     : ifindex(ifindex), xdp_prog(nullptr), xsks_map_fd(-1) {
 #ifdef MTL_HAS_XDP_BACKEND
-  /* get xdp prog path from env or use manager default path */
-  std::string xdp_prog_path;
-  char* xdp_prog_path_env = getenv("MTL_XDP_PROG_PATH");
-  if (xdp_prog_path_env != nullptr) {
-    xdp_prog_path = xdp_prog_path_env;
-  } else {
-    xdp_prog_path = "/tmp/imtl/xdp_prog.o";
-  }
-
-  if (!std::filesystem::is_regular_file(xdp_prog_path)) {
-    log(log_level::WARNING,
-        "Fallback to use libxdp default prog for " + xdp_prog_path + " is not valid.");
-  } else {
-    xdp_prog = xdp_program__open_file(xdp_prog_path.c_str(), NULL, NULL);
-    if (libxdp_get_error(xdp_prog)) {
-      log(log_level::WARNING,
-          "Fallback to use libxdp default prog for " + xdp_prog_path + " cannot load.");
-      xdp_prog = nullptr;
-    } else {
-      if (xdp_program__attach(xdp_prog, ifindex, XDP_MODE_NATIVE, 0) < 0) {
-        xdp_program__close(xdp_prog);
-        throw std::runtime_error("Failed to attach XDP program " + xdp_prog_path);
-      }
-    }
-  }
-
-  if (xsk_setup_xdp_prog(ifindex, &xsks_map_fd) < 0 || xsks_map_fd < 0) {
-    if (xdp_prog != nullptr) {
-      xdp_program__detach(xdp_prog, ifindex, XDP_MODE_NATIVE, 0);
-      xdp_program__close(xdp_prog);
-    }
-    /* unload all xdp programs for the interface */
-    struct xdp_multiprog* multiprog = xdp_multiprog__get_from_ifindex(ifindex);
-    if (multiprog != nullptr) {
-      xdp_multiprog__detach(multiprog);
-      xdp_multiprog__close(multiprog);
-    }
-
-    throw std::runtime_error("Failed to setup AF_XDP socket.");
-  }
-
-  if (xdp_prog == nullptr) xdp_prog_path = "<libxdp_default>";
-  log(log_level::INFO, "Added interface with xdp prog " + xdp_prog_path);
-#else
-  log(log_level::INFO, "Added interface.");
+  if (load_xdp() < 0) throw std::runtime_error("Failed to load XDP program.");
 #endif
+
+  log(log_level::INFO, "Added interface.");
 }
 
 mtl_interface::~mtl_interface() {
 #ifdef MTL_HAS_XDP_BACKEND
-  if (xdp_prog != nullptr) {
-    xdp_program__detach(xdp_prog, ifindex, XDP_MODE_NATIVE, 0);
-    xdp_program__close(xdp_prog);
-    xdp_prog = nullptr;
-  } else {
-    log(log_level::WARNING, "Using libxdp default prog. Try to unload all.");
-  }
-  /* unload all xdp programs for the interface */
-  struct xdp_multiprog* multiprog = xdp_multiprog__get_from_ifindex(ifindex);
-  if (multiprog != nullptr) {
-    xdp_multiprog__detach(multiprog);
-    xdp_multiprog__close(multiprog);
-  }
+  unload_xdp();
 #endif
+  clear_flow_rules();
 
   log(log_level::INFO, "Removed interface.");
 }
@@ -140,6 +95,112 @@ int mtl_interface::update_udp_dp_filter(uint16_t dst_port, bool add) {
   return -1;
 #endif
 }
+
+int mtl_interface::clear_flow_rules() {
+  char ifname[IF_NAMESIZE];
+  if (!if_indextoname(ifindex, ifname)) {
+    log(log_level::ERROR, "Failed to get interface name");
+    return -1;
+  }
+
+  FILE* fp;
+  char buffer[1024];
+  std::string command = "ethtool -n " + std::string(ifname);
+
+  fp = popen(command.c_str(), "r");
+  if (fp == nullptr) {
+    log(log_level::ERROR, "Failed to run " + command);
+    return -1;
+  }
+
+  std::vector<int> rule_ids;
+  int rule_id;
+  while (fgets(buffer, sizeof(buffer), fp) != nullptr) {
+    if (sscanf(buffer, "Filter: %d", &rule_id) == 1) {
+      rule_ids.push_back(rule_id);
+    }
+  }
+
+  pclose(fp);
+
+  for (int id : rule_ids) {
+    log(log_level::INFO, "Delete flow rule " + std::to_string(id));
+    std::string delete_command =
+        "ethtool -N " + std::string(ifname) + " delete " + std::to_string(id);
+    if (system(delete_command.c_str()))
+      log(log_level::WARNING, "Failed to run " + delete_command);
+  }
+
+  return 0;
+}
+
+#ifdef MTL_HAS_XDP_BACKEND
+int mtl_interface::load_xdp() {
+  /* get xdp prog path from env or use manager default path */
+  std::string xdp_prog_path;
+  char* xdp_prog_path_env = getenv("MTL_XDP_PROG_PATH");
+  if (xdp_prog_path_env != nullptr) {
+    xdp_prog_path = xdp_prog_path_env;
+  } else {
+    xdp_prog_path = "/tmp/imtl/xdp_prog.o";
+  }
+
+  if (!std::filesystem::is_regular_file(xdp_prog_path)) {
+    log(log_level::WARNING,
+        "Fallback to use libxdp default prog for " + xdp_prog_path + " is not valid.");
+  } else {
+    xdp_prog = xdp_program__open_file(xdp_prog_path.c_str(), NULL, NULL);
+    if (libxdp_get_error(xdp_prog)) {
+      log(log_level::WARNING,
+          "Fallback to use libxdp default prog for " + xdp_prog_path + " cannot load.");
+      xdp_prog = nullptr;
+    } else {
+      if (xdp_program__attach(xdp_prog, ifindex, XDP_MODE_NATIVE, 0) < 0) {
+        log(log_level::ERROR, "Failed to attach XDP program " + xdp_prog_path);
+        xdp_program__close(xdp_prog);
+        return -1;
+      }
+    }
+  }
+
+  if (xsk_setup_xdp_prog(ifindex, &xsks_map_fd) < 0 || xsks_map_fd < 0) {
+    if (xdp_prog != nullptr) {
+      xdp_program__detach(xdp_prog, ifindex, XDP_MODE_NATIVE, 0);
+      xdp_program__close(xdp_prog);
+    }
+    /* unload all xdp programs for the interface */
+    struct xdp_multiprog* multiprog = xdp_multiprog__get_from_ifindex(ifindex);
+    if (multiprog != nullptr) {
+      xdp_multiprog__detach(multiprog);
+      xdp_multiprog__close(multiprog);
+    }
+
+    log(log_level::ERROR, "Failed to setup AF_XDP socket.");
+    return -1;
+  }
+
+  if (xdp_prog == nullptr) xdp_prog_path = "<libxdp_default>";
+  log(log_level::INFO, "Loaded xdp prog " + xdp_prog_path);
+  return 0;
+}
+
+void mtl_interface::unload_xdp() {
+  if (xdp_prog != nullptr) {
+    xdp_program__detach(xdp_prog, ifindex, XDP_MODE_NATIVE, 0);
+    xdp_program__close(xdp_prog);
+    xdp_prog = nullptr;
+  } else {
+    log(log_level::WARNING, "Using libxdp default prog. Try to unload all.");
+  }
+  /* unload all xdp programs for the interface */
+  struct xdp_multiprog* multiprog = xdp_multiprog__get_from_ifindex(ifindex);
+  if (multiprog != nullptr) {
+    xdp_multiprog__detach(multiprog);
+    xdp_multiprog__close(multiprog);
+  }
+  log(log_level::INFO, "Unloaded xdp prog");
+}
+#endif
 
 std::unordered_map<unsigned int, std::weak_ptr<mtl_interface>> g_interfaces;
 
