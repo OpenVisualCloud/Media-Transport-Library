42cae74f3320085622ab67e040047ad71d8cca31
Ric Li <ming3.li@intel.com>
2023-11-30 14:24:39 +0800

    fix rte_pcapng_copy
    Signed-off-by: Ric Li <ming3.li@intel.com>

--- lib/src/mt_util.c
+++ lib/src/mt_util.c
@@ -400,7 +400,9 @@ struct rte_mbuf* mt_pcapng_copy(struct mtl_main_impl* impl, enum mtl_port port,
   uint16_t port_id = mt_port_id(impl, port);
   uint32_t queue_id = mt_rxq_queue_id(rxq);
 
-#if RTE_VERSION >= RTE_VERSION_NUM(23, 3, 0, 0)
+#if RTE_VERSION >= RTE_VERSION_NUM(23, 11, 0, 0)
+  mc = rte_pcapng_copy(port_id, queue_id, m, mp, length, direction, NULL);
+#elif RTE_VERSION >= RTE_VERSION_NUM(23, 3, 0, 0)
   mc = rte_pcapng_copy(port_id, queue_id, m, mp, length, timestamp, tm_ns, direction,
                        NULL);
 #else
