a16241ccd806a642c825afe2ca9f8f380cf03aa6
Ric Li <ming3.li@intel.com>
2024-02-02 16:22:54 +0800

    fix create flow
    Signed-off-by: Ric Li <ming3.li@intel.com>

--- lib/src/mt_flow.c
+++ lib/src/mt_flow.c
@@ -201,7 +201,7 @@ static struct mt_rx_flow_rsp* rx_flow_create(struct mt_interface* inf, uint16_t 
   struct mtl_main_impl* impl = inf->parent;
   uint8_t* ip = flow->dip_addr;
 
-  if (q >= inf->nb_rx_q) {
+  if (!mt_drv_kernel_based(impl, port) && q >= inf->nb_rx_q) {
     err("%s(%d), invalid q %u\n", __func__, port, q);
     return NULL;
   }
@@ -279,7 +279,7 @@ struct mt_rx_flow_rsp* mt_rx_flow_create(struct mtl_main_impl* impl, enum mtl_po
   struct mt_rx_flow_rsp* rsp;
   struct mt_flow_impl* flow_impl = impl->flow[port];
 
-  if (q >= inf->nb_rx_q) {
+  if (!mt_drv_kernel_based(impl, port) && q >= inf->nb_rx_q) {
     err("%s(%d), invalid q %u max allowed %u\n", __func__, port, q, inf->nb_rx_q);
     return NULL;
   }
