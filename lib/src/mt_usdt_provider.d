877a5e431712593928d479e5a9f29c36f8955049
Frank Du <frank.du@intel.com>
2024-03-21 19:09:27 +0800

    usdt: use dtrace to generate the usdt probe list (#796)
    for semaphore support
    
    Signed-off-by: Frank Du <frank.du@intel.com>

---
+++ lib/src/mt_usdt_provider.d
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright(c) 2024 Intel Corporation
+ *
+ * The USDT provider and probe define.
+ */
+
+provider ptp {
+  probe ptp_msg(int port, int stage, uint64_t value);
+  probe ptp_result(int port, int64_t delta, int64_t correct);
+}
