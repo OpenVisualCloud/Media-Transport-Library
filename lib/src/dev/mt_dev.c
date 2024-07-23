dc0d02a4a79d62a3be4c20f8fe86d8877bd44c83
Wei <72336341+weic6@users.noreply.github.com>
2024-07-23 00:47:06 -0500

    dev: add support for ixgbe driver (#932)
    This PR adds support for the IXGBE driver, according #930 . 
    Both the Intel 10-Gigabit X540-AT2 (1528) and Intel 10G X550T (1563)
    have been tested. In the test where PF was used to bind with the PMD,
    both RxTxApp and ffmpeg-plugin app transmit media successfully.
    
    Co-authored-by: wei <wchen@dtvinnovations.com>
    Co-authored-by: Frank Du <frank.du@intel.com>

--- lib/src/dev/mt_dev.c
+++ lib/src/dev/mt_dev.c
@@ -22,6 +22,12 @@ static const struct mt_dev_driver_info dev_drvs[] = {
         .flow_type = MT_FLOW_ALL, /* or MT_FLOW_NONE? */
     },
     {
+        .name = "net_ixgbe", /* For ixgbe */
+        .port_type = MT_PORT_PF,
+        .drv_type = MT_DRV_IXGBE,
+        .flow_type = MT_FLOW_NONE,
+    },
+    {
         .name = "net_ice",
         .port_type = MT_PORT_PF,
         .drv_type = MT_DRV_ICE,
@@ -1268,7 +1274,9 @@ static int dev_if_init_rx_queues(struct mtl_main_impl* impl, struct mt_interface
           mbuf_pool = mt_mempool_create_common(impl, inf->port, pool_name, mbuf_elements);
         else {
           uint16_t data_room_sz = ST_PKT_MAX_ETHER_BYTES;
-          if (inf->drv_info.drv_type == MT_DRV_IGC) /* to avoid igc nic split mbuf */
+          /* to avoid igc/igxbe nic split mbuf */
+          if (inf->drv_info.drv_type == MT_DRV_IGC ||
+              inf->drv_info.drv_type == MT_DRV_IXGBE)
             data_room_sz = MT_MBUF_DEFAULT_DATA_SIZE;
           if (impl->rx_pool_data_size) /* user suggested data room size */
             data_room_sz = impl->rx_pool_data_size;
