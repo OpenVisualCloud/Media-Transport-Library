e213f8e5a365f45f1637a76afc77ac235de5d309
zLukas <lukasz.grab@intel.com>
2024-09-19 11:11:11 +0200

    add update info in format-coding.sh

--- format-coding.sh
+++ format-coding.sh
@@ -5,6 +5,17 @@
 
 # Based on clang-format
 # For ubuntu, pls "apt-get install clang-format"
+# When updating clang format version, remeber to update also GHA clang lister version in
+#  - uses: DoozyX/clang-format-lint-action@v0.18.2 
+#     with:
+#       clangFormatVersion: '14'
+#       source: '.'
+#       extensions: 'hpp,h,cpp,c,cc'
+# in 
+# .github/workflows/afxdp_build.yml
+# .github/workflows/centos_build.yml
+# .github/workflows/tools_build.yml
+# .github/workflows/ubuntu_build.yml
 
 set -e
 
