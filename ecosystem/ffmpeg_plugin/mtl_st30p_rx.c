OpenVisualCloud/Media-Transport-Library

commit 065923f3f768ee1adeb3315e55cdd080757aba4a

Refactor FFmpeg audio plugin sample rate parsing
This commit adds a helper function mtl_parse_st30_sample_rate()
to improve code reuse and eliminate multiple hardcoded frequency values.

Signed-off-by: Konstantin Ilichev <konstantin.ilichev@intel.com>

ecosystem/ffmpeg_plugin/mtl_st30p_rx.c
@@ -118,15 +118,10 @@ static int mtl_st30p_read_header(AVFormatContext* ctx) {
   }
   ops_rx.ptime = s->ptime;
   ops_rx.channel = s->channels;
-  if (s->sample_rate == 48000)
-    ops_rx.sampling = ST30_SAMPLING_48K;
-  else if (s->sample_rate == 96000)
-    ops_rx.sampling = ST30_SAMPLING_96K;
-  else if (s->sample_rate == 44100)
-    ops_rx.sampling = ST31_SAMPLING_44K;
-  else {
+  ret = mtl_parse_st30_sample_rate(&ops_rx.sampling, s->sample_rate);
+  if (!ret) {
     err(ctx, "%s, invalid sample_rate: %d\n", __func__, s->sample_rate);
-    return AVERROR(EINVAL);
+    return ret;
   }
   frame_buf_size = st30_calculate_framebuff_size(
       ops_rx.fmt, ops_rx.ptime, ops_rx.sampling, ops_rx.channel, 10 * NS_PER_MS, NULL);

ecosystem/ffmpeg_plugin/mtl_st30p_tx.c
@@ -110,15 +110,10 @@ static int mtl_st30p_write_header(AVFormatContext* ctx) {
   info(ctx, "%s, nb_channels %d\n", __func__, codecpar->ch_layout.nb_channels);
   ops_tx.channel = codecpar->ch_layout.nb_channels;
 #endif
-  if (codecpar->sample_rate == 48000) {
-    ops_tx.sampling = ST30_SAMPLING_48K;
-  } else if (codecpar->sample_rate == 96000) {
-    ops_tx.sampling = ST30_SAMPLING_96K;
-  } else if (codecpar->sample_rate == 44100) {
-    ops_tx.sampling = ST31_SAMPLING_44K;
-  } else {
+  ret = mtl_parse_st30_sample_rate(&ops_tx.sampling, codecpar->sample_rate);
+  if (!ret) {
     err(ctx, "%s, unknown sample_rate %d\n", __func__, codecpar->sample_rate);
-    return AVERROR(EINVAL);
+    return ret;
   }
   if (codecpar->codec_id == AV_CODEC_ID_PCM_S24BE) {
     ops_tx.fmt = ST30_FMT_PCM24;

ecosystem/ffmpeg_plugin/mtl_common.c
@@ -171,3 +171,20 @@ int mtl_parse_tx_port(AVFormatContext* ctx, const struct StDevArgs* devArgs,
 
   return 0;
 }
+
+int mtl_parse_st30_sample_rate(enum st30_sampling *sample_rate, int value)
+{
+  switch (value) {
+  case 48000:
+    *sample_rate = ST30_SAMPLING_48K;
+    return 0;
+  case 96000:
+    *sample_rate = ST30_SAMPLING_96K;
+    return 0;
+  case 44100:
+    *sample_rate = ST31_SAMPLING_44K;
+    return 0;
+  default:
+    return AVERROR(EINVAL);
+  }
+}

ecosystem/ffmpeg_plugin/mtl_common.h
@@ -19,6 +19,7 @@
 
 #include <arpa/inet.h>
 #include <mtl/st_pipeline_api.h>
+#include <mtl/st30_api.h>
 
 // clang-format off
 /* MTL FFMPEG version */
@@ -164,3 +165,4 @@ int mtl_parse_rx_port(AVFormatContext* ctx, const struct StDevArgs* devArgs,
                       const StRxSessionPortArgs* args, struct st_rx_port* port);
 int mtl_parse_tx_port(AVFormatContext* ctx, const struct StDevArgs* devArgs,
                       const StTxSessionPortArgs* args, struct st_tx_port* port);
+int mtl_parse_st30_sample_rate(enum st30_sampling *sample_rate, int value);

commit fc8a0f252629428ccf24e88249f9f90e6df51e83

Fix error handling in parsing sample freq in FFmpeg audio plugins
Commit 065923f3f768 ("Refactor FFmpeg audio plugin sample rate parsing")
added a helper function for parsing the sample frequency. However, the error
returned from the function was checked incorrectly (inverted). This led to
a segmentation fault when starting FFmpeg with MTL audio plugins.

This commit fixes the error handling by checking if the return value is non zero.

Fixes: 065923f3f768 ("Refactor FFmpeg audio plugin sample rate parsing")
Signed-off-by: Konstantin Ilichev <konstantin.ilichev@intel.com>
Change-type: DefectResolution

ecosystem/ffmpeg_plugin/mtl_st30p_rx.c
@@ -119,7 +119,7 @@ static int mtl_st30p_read_header(AVFormatContext* ctx) {
   ops_rx.ptime = s->ptime;
   ops_rx.channel = s->channels;
   ret = mtl_parse_st30_sample_rate(&ops_rx.sampling, s->sample_rate);
-  if (!ret) {
+  if (ret) {
     err(ctx, "%s, invalid sample_rate: %d\n", __func__, s->sample_rate);
     return ret;
   }

ecosystem/ffmpeg_plugin/mtl_st30p_tx.c
@@ -111,7 +111,7 @@ static int mtl_st30p_write_header(AVFormatContext* ctx) {
   ops_tx.channel = codecpar->ch_layout.nb_channels;
 #endif
   ret = mtl_parse_st30_sample_rate(&ops_tx.sampling, codecpar->sample_rate);
-  if (!ret) {
+  if (ret) {
     err(ctx, "%s, unknown sample_rate %d\n", __func__, codecpar->sample_rate);
     return ret;
   }