2fcca8bdd5c62a7893a0241eae1720d4d659539f
Pawel Staszczuk <41273017+staszczuk@users.noreply.github.com>
2024-12-03 09:00:21 +0000

    Format files with shfmt

--- tests/validation/functional/run_linux_function.sh
+++ tests/validation/functional/run_linux_function.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-while IFS= read -r line
-do
+while IFS= read -r line; do
 	echo "$line"
-	python2 Execut.py -t kahawai_function_test -c "$line" >> run.log 2>&1
+	python2 Execut.py -t kahawai_function_test -c "$line" >>run.log 2>&1
 
 	mv logs logs_"$line"
-done < linux_function_case_list.txt
+done <linux_function_case_list.txt
