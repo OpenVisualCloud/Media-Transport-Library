2d2e8ff91c028cbd303a37887c6d35b397b3cfb5
Pawel Staszczuk <41273017+staszczuk@users.noreply.github.com>
2025-01-09 09:14:38 +0000

    Add st30p framebuff size calculation

--- tests/validation/tests/Engine/integrity.py
+++ tests/validation/tests/Engine/integrity.py
@@ -1,6 +1,7 @@
 import os
 import re
 import subprocess
+from math import floor
 
 from tests.Engine.execute import LOG_FOLDER, log_fail
 
@@ -88,3 +89,83 @@ def check_st30p_integrity(src_url: str, out_url: str):
                 return False
 
     return True
+
+
+def calculate_st30p_framebuff_size(
+    format: str, ptime: str, sampling: str, channel: str
+):
+    match format:
+        case "PCM8":
+            sample_size = 1
+        case "PCM16":
+            sample_size = 2
+        case "PCM24":
+            sample_size = 3
+
+    match sampling:
+        case "48kHz":
+            match ptime:
+                case "1":
+                    sample_num = 48
+                case "0.12":
+                    sample_num = 6
+                case "0.25":
+                    sample_num = 12
+                case "0.33":
+                    sample_num = 16
+                case "4":
+                    sample_num = 192
+        case "96kHz":
+            match ptime:
+                case "1":
+                    sample_num = 96
+                case "0.12":
+                    sample_num = 12
+                case "0.25":
+                    sample_num = 24
+                case "0.33":
+                    sample_num = 32
+                case "4":
+                    sample_num = 384
+
+    match channel:
+        case "M":
+            channel_num = 1
+        case "DM" | "ST" | "LtRt" | "AES3":
+            channel_num = 2
+        case "51":
+            channel_num = 6
+        case "71":
+            channel_num = 8
+        case "222":
+            channel_num = 24
+        case "SGRP":
+            channel_num = 4
+        case _:
+            match = re.match(r"^U(\d{2})$", channel)
+
+            if match:
+                channel_num = int(match.group(1))
+
+    packet_size = sample_size * sample_num * channel_num
+
+    match ptime:
+        case "1":
+            packet_time = 1_000_000 * 1
+        case "0.12":
+            packet_time = 1_000_000 * 0.125
+        case "0.25":
+            packet_time = 1_000_000 * 0.25
+        case "0.33":
+            packet_time = 1_000_000 * 1 / 3
+        case "4":
+            packet_time = 1_000_000 * 4
+
+    desired_frame_time = 10_000_000
+
+    packet_per_frame = 1
+
+    if desired_frame_time > packet_time:
+        packet_per_frame = floor(desired_frame_time / packet_time)
+
+    return packet_per_frame * packet_size
