5e85a13a52a90ca0c5c53d10b10d94b9756bfa47
Paweł Staszczuk <41273017+staszczuk@users.noreply.github.com>
2025-01-21 15:36:49 +0100

    Add: Integrity tests (#1032)
    Add integrity tests that compare source and output files for st20p and
    st30p. In st20p the files are compared by frame checksums, in st30p the
    files are compared by bytes.

--- tests/validation/tests/Engine/RxTxApp.py
+++ tests/validation/tests/Engine/RxTxApp.py
@@ -195,6 +195,7 @@ def add_st20p_sessions(
     packing: str = "BPM",
     enable_rtcp: bool = False,
     measure_latency: bool = False,
+    out_url: str = "",
 ) -> dict:
     config = add_interfaces(
         config=config, nic_port_list=nic_port_list, test_mode=test_mode
@@ -224,6 +225,7 @@ def add_st20p_sessions(
     config["rx_sessions"][0]["st20p"][0]["enable_rtcp"] = enable_rtcp
     config["rx_sessions"][0]["st20p"][0]["measure_latency"] = measure_latency
     config["tx_sessions"][0]["st20p"][0]["st20p_url"] = st20p_url
+    config["rx_sessions"][0]["st20p"][0]["st20p_url"] = out_url
 
     return config
 
@@ -286,6 +288,7 @@ def add_st30p_sessions(
     audio_channel: list = ["U02"],
     audio_sampling: str = "96kHz",
     audio_ptime: str = "1",
+    out_url: str = "",
 ) -> dict:
     config = add_interfaces(
         config=config, nic_port_list=nic_port_list, test_mode=test_mode
@@ -304,7 +307,8 @@ def add_st30p_sessions(
     config["tx_sessions"][0]["st30p"][0]["audio_ptime"] = audio_ptime
     config["rx_sessions"][0]["st30p"][0]["audio_ptime"] = audio_ptime
     config["tx_sessions"][0]["st30p"][0]["audio_url"] = filename
-    config["rx_sessions"][0]["st30p"][0]["audio_url"] = filename
+    config["rx_sessions"][0]["st30p"][0]["audio_url"] = out_url
+
     return config
 
 
