913e5669af2340271002b8923e0c09775945d5c8
Mateusz Grabuszyński <mateusz.grabuszynski@intel.com>
2025-01-22 10:12:02 +0100

    Fix: Correct wrong license headers in /tests/validation/ (#1033)
    Headers in the tests/validation/ folder contain "INTEL CONFIDENTIAL".
    Fix by exchanging with SPDX-based BSD-3-Clause identifier:
    
    SPDX-License-Identifier: BSD-3-Clause
    Copyright(c) 2024-2025 Intel Corporation
    
    fixes: 1580c7ff04a93664bf3751b5b533ccf23b940da1

--- tests/validation/tests/Engine/rxtxapp_config.py
+++ tests/validation/tests/Engine/rxtxapp_config.py
@@ -1,13 +1,5 @@
-# INTEL CONFIDENTIAL
-# Copyright 2024-2024 Intel Corporation.
-#
-# This software and the related documents are Intel copyrighted materials, and your use of them is governed
-# by the express license under which they were provided to you ("License"). Unless the License provides otherwise,
-# you may not use, modify, copy, publish, distribute, disclose or transmit this software or the related documents
-# without Intel's prior written permission.
-#
-# This software and the related documents are provided as is, with no express or implied warranties,
-# other than those that are expressly stated in the License.
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright(c) 2024-2025 Intel Corporation
 
 config_empty = {
     "tx_no_chain": False,
