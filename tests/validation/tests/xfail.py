b8c4533a4f54181ca66a9790b5a8b65e6fe2cc0f
Kasiewicz, Marek <marek.kasiewicz@intel.com>
2025-02-10 14:27:59 +0000

    Fix: Remove unsupported 720p resolution from tests
    - Add disclaimer about 720p in docs.
    - The 720p resolution is not supported by MTL for v210 at the moment.
    This is a MTL bug, but the GStreamer plugin does not need to support it.
    
    Signed-off-by: Kasiewicz, Marek <marek.kasiewicz@intel.com>

--- tests/validation/tests/xfail.py
+++ tests/validation/tests/xfail.py
@@ -26,3 +26,14 @@ def SDBQ1002_pg_format_error_check(video_format: str, pg_format: str, request):
             "XFAIL: SDBQ-1002 - Video, i720p50fps with V210 pg_format Error: tv_frame_free_cb",
             request,
         )
+
+
+def SDBQ1971_conversion_v210_720p_error(
+    video_format: str, resolution_width: int, request
+):
+    if video_format == "v210" and resolution_width == 720:
+        add_issue(
+            "XFAIL: SDBQ-1971 - Conversion from v210 format does not work on 720p",
+            request,
+        )
+        assert False
