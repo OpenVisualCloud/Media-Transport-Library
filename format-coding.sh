9f8f0bb8ce7cfa10256f13fef897048ad4142ebd
zLukas <lukasz.grab@intel.com>
2025-02-25 12:56:50 +0000

    update format-coding.sh

--- format-coding.sh
+++ format-coding.sh
@@ -13,11 +13,10 @@ cp .github/linters/.clang-format .clang-format
 
 echo "clang-format check"
 find . -path ./build -prune -o -regex '.*\.\(cpp\|hpp\|cc\|c\|h\)' ! -name 'pymtl_wrap.c' \
-	! -name 'vmlinux.h' ! -name '.clang-format' \
-	-exec clang-format-17 --verbose -i {} +
+	! -name 'vmlinux.h' ! -name '.clang-format' -exec clang-format-17 --verbose -i {} +
 
 # clean-up the copied .clang-format
-rm -rf	.clang-format
+rm .clang-format
 CONFIG=".github/linters/.pyproject.toml"
 black --config "$CONFIG" python/
 isort --settings-path "$CONFIG" python/
