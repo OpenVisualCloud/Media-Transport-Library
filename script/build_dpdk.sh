ae7516281c4acc932473ebb2edabdd2e1443d164
Wesierski, Dawid <dawid.wesierski@intel.com>
2025-05-16 14:57:53 +0200

    Fix: fix cicd builds
    CiCd setup is broken due to the actions failing to build the
    project. Fix via switching building to the script.

--- script/build_dpdk.sh
+++ script/build_dpdk.sh
@@ -5,8 +5,8 @@
 
 set -e
 
-script_name=$(basename "$0")
-script_path=$(readlink -qe "$0")
+script_name=$(basename "${BASH_SOURCE[0]}")
+script_path=$(readlink -qe "${BASH_SOURCE[0]}")
 script_folder=${script_path/$script_name/}
 cd "${script_folder}"
 
@@ -17,22 +17,26 @@ else
 	dpdk_ver=25.03
 fi
 
-echo "DPDK version: $dpdk_ver"
+(return 0 2>/dev/null) && sourced=1 || sourced=0
 
-if [ ! -d "dpdk" ]; then
-	# check out dpdk code
-	echo "Clone DPDK source code"
-	git clone https://github.com/DPDK/dpdk.git
+if [ "$sourced" -eq 0 ]; then
+	echo "DPDK version: $dpdk_ver"
+
+	if [ ! -d "dpdk" ]; then
+		# check out dpdk code
+		echo "Clone DPDK source code"
+		git clone https://github.com/DPDK/dpdk.git
+	fi
+	cd dpdk
+	git checkout v"$dpdk_ver"
+	git switch -c v"$dpdk_ver"
+
+	# apply the patches
+	git am ../../patches/dpdk/"$dpdk_ver"/*.patch
+
+	# build and install dpdk now
+	meson build
+	ninja -C build
+	cd build
+	sudo ninja install
 fi
-cd dpdk
-git checkout v"$dpdk_ver"
-git switch -c v"$dpdk_ver"
-
-# apply the patches
-git am ../../patches/dpdk/"$dpdk_ver"/*.patch
-
-# build and install dpdk now
-meson build
-ninja -C build
-cd build
-sudo ninja install
