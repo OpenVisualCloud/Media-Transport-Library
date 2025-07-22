5bbfe980a0994d3628656f3cbd62b23546ef9011
Tomasz <tomasz.szumski@intel.com>
2025-07-22 10:09:26 +0200

    Fix: handling case where RL is lower then expected (#1205)
    1. Fix handling case where RL is lower then expected
    2. Update ice driver to version 2.2.8
    
    ---------
    
    Signed-off-by: Szumski, Tomasz<tomasz.szumski@intel.com>
    Co-authored-by: Kasiewicz, Marek <marek.kasiewicz@intel.com>

--- tests/tools/RxTxApp/src/args.c
+++ tests/tools/RxTxApp/src/args.c
@@ -57,7 +57,7 @@ enum st_args_cmd {
   ST_ARG_PACING_WAY,
   ST_ARG_START_VRX,
   ST_ARG_PAD_INTERVAL,
-  ST_ARG_NO_PAD_STATIC,
+  ST_ARG_PAD_STATIC,
   ST_ARG_SHAPING,
   ST_ARG_TIMESTAMP_FIRST_PKT,
   ST_ARG_TIMESTAMP_EPOCH,
@@ -209,7 +209,7 @@ static struct option st_app_args_options[] = {
     {"pacing_way", required_argument, 0, ST_ARG_PACING_WAY},
     {"start_vrx", required_argument, 0, ST_ARG_START_VRX},
     {"pad_interval", required_argument, 0, ST_ARG_PAD_INTERVAL},
-    {"no_static_pad", no_argument, 0, ST_ARG_NO_PAD_STATIC},
+    {"static_pad", no_argument, 0, ST_ARG_PAD_STATIC},
     {"shaping", required_argument, 0, ST_ARG_SHAPING},
     {"ts_first_pkt", no_argument, 0, ST_ARG_TIMESTAMP_FIRST_PKT},
     {"ts_delta_us", required_argument, 0, ST_ARG_TIMESTAMP_DELTA_US},
@@ -600,8 +600,8 @@ int st_app_parse_args(struct st_app_context* ctx, struct mtl_init_params* p, int
       case ST_ARG_PAD_INTERVAL:
         ctx->tx_pad_interval = atoi(optarg);
         break;
-      case ST_ARG_NO_PAD_STATIC:
-        ctx->tx_no_static_pad = true;
+      case ST_ARG_PAD_STATIC:
+        ctx->tx_static_pad = true;
         break;
       case ST_ARG_TIMESTAMP_FIRST_PKT:
         ctx->tx_ts_first_pkt = true;
