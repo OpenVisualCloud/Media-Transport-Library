a2c0eb3246c2a86e903e5602525659ddf715a96d
Pawel Staszczuk <41273017+staszczuk@users.noreply.github.com>
2025-07-28 15:03:59 +0000

    Fix st41 payload type test

--- tests/validation/tests/single/st41/payload_type/test_payload_type.py
+++ tests/validation/tests/single/st41/payload_type/test_payload_type.py
@@ -58,11 +58,13 @@ def test_payload_type(
     capture_cfg = dict(test_config.get("capture_cfg", {}))
     capture_cfg["test_name"] = f"test_payload_type_{payload_type}_{type_mode}"
 
+    host = list(hosts.values())[0]
+
     config = rxtxapp.create_empty_config()
     config = rxtxapp.add_st41_sessions(
         config=config,
         no_chain=False,
-        nic_port_list=nic_port_list,
+        nic_port_list=host.vfs,
         test_mode="unicast",
         payload_type=payload_type_mapping[payload_type],
         type_=type_mode,
@@ -72,7 +74,6 @@ def test_payload_type(
         fastmetadata_url=os.path.join(media, st41_file),
     )
 
-    host = list(hosts.values())[0]
     rxtxapp.execute_test(
         config=config,
         build=build,
