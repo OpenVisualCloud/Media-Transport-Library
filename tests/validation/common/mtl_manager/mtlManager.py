7ad050e561a7b207a9b93dfaaa33fe0e1c809db6
Your Name <you@example.com>
2025-07-29 09:25:13 +0200

    blank lines

--- tests/validation/common/mtl_manager/mtlManager.py
+++ tests/validation/common/mtl_manager/mtlManager.py
@@ -4,6 +4,7 @@ from mfd_connect.exceptions import ConnectionCalledProcessError
 
 logger = logging.getLogger(__name__)
 
+
 class MtlManager:
     """
     Class to manage the lifecycle of the MtlManager process on a remote host.
@@ -22,7 +23,6 @@ class MtlManager:
         self.cmd = None
         self.mtl_manager_process = None
 
-
     def start(self):
         """
         Starts the MtlManager process on the remote host using sudo.
@@ -33,13 +33,17 @@ class MtlManager:
             self.cmd = "sudo MtlManager"
             try:
                 logger.info(f"Running command on host {self.host.name}: {self.cmd}")
-                self.mtl_manager_process = connection.start_process(self.cmd, stderr_to_stdout=True)
+                self.mtl_manager_process = connection.start_process(
+                    self.cmd, stderr_to_stdout=True
+                )
 
                 if not self.mtl_manager_process.running:
                     err = self.mtl_manager_process.stderr_text
                     logger.error(f"MtlManager failed to start. Error output:\n{err}")
                     return False
-                logger.info(f"MtlManager started with PID {self.mtl_manager_process.pid}.")
+                logger.info(
+                    f"MtlManager started with PID {self.mtl_manager_process.pid}."
+                )
                 return True
             except ConnectionCalledProcessError as e:
                 logger.error(f"Failed to start MtlManager: {e}")
@@ -47,9 +51,7 @@ class MtlManager:
         else:
             logger.info("MtlManager is already running.")
             return True
-        pass
 
-      
     def stop(self):
         """
         Stops all MtlManager processes on the remote host using sudo pkill.
