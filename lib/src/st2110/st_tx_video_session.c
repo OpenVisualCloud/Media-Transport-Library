OpenVisualCloud/Media-Transport-Library

commit acea200ef8c935cdff64b04a46951bfd4b67f8f0

Fix: User controlled sync for pacing st2110-20 (#1240)
A mechanism that allows the session to move
the pacing window if the current epoch is just
a little bit behind was applied to user controlled pacing,
which in turn made the scenarios where
big PTP updates or other small issues may cause
incremental delays, which is not really a problem
as long as we don't enable user controlled pacing.

Fix it by ignoring all discrepancies for user
controlled pacing scenarios (required_tai) and
just forcing the session to send the packets asap.

lib/src/st2110/st_tx_video_session.c
@@ -649,25 +649,32 @@ static int tv_sync_pacing(struct mtl_main_impl* impl, struct st_tx_video_session
         ", ptp_time %" PRIu64 "ms\n",
         __func__, idx, to_epoch, epochs, pacing->cur_epochs, ptp_time / 1000 / 1000);
     ST_SESSION_STAT_INC(s, port_user_stats, stat_epoch_troffset_mismatch);
-    epochs++; /* assign to next */
-    start_time_ptp = pacing_start_time(pacing, epochs);
-    to_epoch = start_time_ptp - ptp_time;
-  }
 
-  if (to_epoch < 0) {
-    /* should never happen */
-    err("%s(%d), error to_epoch %f, ptp_time %" PRIu64 ", epochs %" PRIu64 " %" PRIu64
-        "\n",
-        __func__, idx, to_epoch, ptp_time, epochs, pacing->cur_epochs);
-    to_epoch = 0;
+    /* in case of user controlled pacing we need to send this even if late
+    as close to the user defiend time as possible*/
+    if (required_tai) {
+      to_epoch = 0;
+    } else {
+      to_epoch = start_time_ptp - ptp_time;
+      epochs++; /* assign to next */
+      start_time_ptp = pacing_start_time(pacing, epochs);
+
+      if (to_epoch < 0) {
+        /* should never happen */
+        err("%s(%d), error to_epoch %f, ptp_time %" PRIu64 ", epochs %" PRIu64 " %" PRIu64
+            "\n",
+            __func__, idx, to_epoch, ptp_time, epochs, pacing->cur_epochs);
+        to_epoch = 0;
+      }
+    }
   }
 
   if (epochs > next_epochs) {
     dbg("%s(%d), epochs %" PRIu64 " next_epochs %" PRIu64 "\n", __func__, idx, epochs,
         next_epochs);
-    s->stat_epoch_drop += (epochs - next_epochs);
-  }
-  if (epochs < next_epochs) {
+    ST_SESSION_STAT_ADD(s, port_user_stats.common, stat_epoch_drop,
+                        (epochs - next_epochs));
+  } else if (epochs < next_epochs) {
     ST_SESSION_STAT_ADD(s, port_user_stats.common, stat_epoch_onward,
                         (next_epochs - epochs));
   }
@@ -677,10 +684,12 @@ static int tv_sync_pacing(struct mtl_main_impl* impl, struct st_tx_video_session
   pacing->rtp_time_stamp = pacing_time_stamp(s, pacing, epochs);
   dbg("%s(%d), old time_cursor %fms\n", __func__, idx,
       pacing->tsc_time_cursor / 1000 / 1000);
+
   pacing->tsc_time_cursor = (double)mt_get_tsc(impl) + to_epoch;
   dbg("%s(%d), epochs %" PRIu64 " time_stamp %u time_cursor %fms to_epoch %fms\n",
       __func__, idx, pacing->cur_epochs, pacing->rtp_time_stamp,
       pacing->tsc_time_cursor / 1000 / 1000, to_epoch / 1000 / 1000);
+
   pacing->ptp_time_cursor = start_time_ptp;
   pacing->tsc_time_frame_start = pacing->tsc_time_cursor;
 

commit be2a47dd5651cd7bc66473a2b17ae0f17e3ede97

Fix: calculating pacing start_time
Commit wrongly reversed the order of allocation
time for enw pacing start time.
Reverse it back.

Fixes: acea200ef8c935cdff64b04a46951bfd4b67f8f0

lib/src/st2110/st_tx_video_session.c
@@ -655,9 +655,9 @@ static int tv_sync_pacing(struct mtl_main_impl* impl, struct st_tx_video_session
     if (required_tai) {
       to_epoch = 0;
     } else {
-      to_epoch = start_time_ptp - ptp_time;
       epochs++; /* assign to next */
       start_time_ptp = pacing_start_time(pacing, epochs);
+      to_epoch = start_time_ptp - ptp_time;
 
       if (to_epoch < 0) {
         /* should never happen */