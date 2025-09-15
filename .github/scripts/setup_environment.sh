OpenVisualCloud/Media-Transport-Library

commit 12098baf5f94d18983954c265aa3783851c8869d

Add: Gstreamer plugin for ST30TX (#1027)
Create a plugin for GStreamer, allowing to send audio in
ST2110-30 standard by the use of MTL st30 pipeline mode.

---------

Signed-off-by: Kasiewicz, Marek <marek.kasiewicz@intel.com>

ecosystem/gstreamer_plugin/build.sh
@@ -1,0 +1,35 @@
+#!/bin/bash
+
+set -euo pipefail
+
+BUILD_DIR="builddir"
+DEBUG=false
+
+# Parse command-line arguments
+for arg in "$@"; do
+	case "$arg" in
+	--debug)
+		DEBUG=true
+		shift
+		;;
+	*)
+		shift
+		;;
+	esac
+done
+
+if [ -d "$BUILD_DIR" ]; then
+	echo "Removing existing build directory..."
+	rm -rf "$BUILD_DIR" || {
+		echo "Failed to remove existing build directory"
+		exit 1
+	}
+fi
+
+if [ "$DEBUG" = true ]; then
+	meson setup --buildtype=debug "$BUILD_DIR"
+else
+	meson setup "$BUILD_DIR"
+fi
+
+meson compile -C "$BUILD_DIR"

ecosystem/gstreamer_plugin/gst_mtl_st30p_tx.h
@@ -1,0 +1,115 @@
+/*
+ * GStreamer
+ * Copyright (C) 2005 Thomas Vander Stichele <thomas@apestaart.org>
+ * Copyright (C) 2005 Ronald S. Bultje <rbultje@ronald.bitfreak.net>
+ * Copyright (C) 2020 Niels De Graef <niels.degraef@gmail.com>
+ * Copyright (C) 2024 Intel Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *
+ * Alternatively, the contents of this file may be used under the
+ * GNU Lesser General Public License Version 2.1 (the "LGPL"), in
+ * which case the following provisions apply instead of the ones
+ * mentioned above:
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#ifndef __GST_MTL_ST30P_TX_H__
+#define __GST_MTL_ST30P_TX_H__
+
+#include <arpa/inet.h>
+#include <gst/audio/audio.h>
+#include <gst/gst.h>
+#include <mtl/mtl_api.h>
+#include <mtl/st30_pipeline_api.h>
+
+G_BEGIN_DECLS
+
+#ifndef NS_PER_MS
+#define NS_PER_MS (1000 * 1000)
+#endif
+
+#ifndef NS_PER_S
+#define NS_PER_S (1000 * NS_PER_MS)
+#endif
+
+#define GST_TYPE_MTL_ST30P_TX (gst_mtl_st30p_tx_get_type())
+G_DECLARE_FINAL_TYPE(Gst_Mtl_St30p_Tx, gst_mtl_st30p_tx, GST, MTL_ST30P_TX, GstAudioSink)
+
+typedef struct StDevArgs {
+  gchar port[MTL_PORT_MAX_LEN];
+  gchar local_ip_string[MTL_PORT_MAX_LEN];
+  gint tx_queues_cnt[MTL_PORT_MAX];
+  gint rx_queues_cnt[MTL_PORT_MAX];
+  gchar dma_dev[MTL_PORT_MAX_LEN];
+} StDevArgs;
+
+typedef struct StTxSessionPortArgs {
+  gchar tx_ip_string[MTL_PORT_MAX_LEN];
+  gchar port[MTL_PORT_MAX_LEN];
+  gint udp_port;
+  gint payload_type;
+} StTxSessionPortArgs;
+
+struct _Gst_Mtl_St30p_Tx {
+  GstAudioSink element;
+  gboolean silent;
+  mtl_handle mtl_lib_handle;
+  st30p_tx_handle tx_handle;
+
+  struct st30_frame* cur_frame;
+  guint cur_frame_available_size;
+
+  /* arguments for incomplete frame buffers */
+  guint retry_frame;
+  guint frame_size;
+
+  /* arguments for imtl initialization device */
+  StDevArgs devArgs;
+  /* arguments for imtl tx session */
+  StTxSessionPortArgs portArgs;
+
+  /* arguments for session */
+  guint framebuffer_num;
+  guint framerate;
+};
+
+enum gst_mtl_supported_audio_sampling {
+  GST_MTL_SUPPORTED_AUDIO_SAMPLING_44_1K = 44100,
+  GST_MTL_SUPPORTED_AUDIO_SAMPLING_48K = 48000,
+  GST_MTL_SUPPORTED_AUDIO_SAMPLING_96K = 96000
+};
+
+G_END_DECLS
+
+#endif /* __GST_MTL_ST30P_TX_H__ */

ecosystem/gstreamer_plugin/meson.build
@@ -22,14 +22,21 @@ plugin_c_args = [
   '-Wno-stringop-truncation'
 ]
 
+if run_command('test', '-d', '/usr/local/include/gstreamer-1.0').returncode() == 0
+    gstreamer_inc_dir = '/usr/local/include/gstreamer-1.0'
+else
+    gstreamer_inc_dir = '/usr/include/gstreamer-1.0'
+endif
+
 inc_dirs = include_directories(
     '/usr/include/glib-2.0',
-    '/usr/include/gstreamer-1.0',
     '/usr/lib/x86_64-linux-gnu/glib-2.0/include',
-    '/usr/local/include/mtl'
+    '/usr/local/include/mtl',
+    gstreamer_inc_dir
 )
 
 gstreamer_video_dep = dependency('gstreamer-video-1.0')
+gstreamer_audio_dep = dependency('gstreamer-audio-1.0')
 mtl_dep             = dependency('mtl')
 
 gst_mtl_common_sources = [
@@ -66,3 +73,16 @@ gst_mtl_st20p_rx = library('gstmtl_st20p_rx',
 )
 
 
+# The mtl_st30p_tx Plugin
+gstmtl_st30p_tx_sources = [
+    'gst_mtl_st30p_tx.c',
+]
+
+gstmtl_st30p_tx = library('gstmtl_st30p_tx',
+    gstmtl_st30p_tx_sources,
+    dependencies : [gst_dep, gstbase_dep, gstreamer_audio_dep, mtl_dep],
+    install : true,
+    install_dir : plugins_install_dir,
+    include_directories: inc_dirs,
+    c_args: plugin_c_args
+)

commit a71ab0837445ad93f844cce588b06bd02789abc4

Fix: GSstreamer build script path resolution
Using relative paths for build script made it
prone to fail and to produce unexpected results
when executed from different working directories.

Add a check to ensure the script always runs from
its own directory.

Fix the .github/scripts/setup_environment.sh
to install the gstreamer development package.

Fixes: 12098baf5f94d18983954c265aa3783851c8869d

.github/scripts/setup_environment.sh
@@ -182,6 +182,7 @@ function setup_ubuntu_install_dependencies() {
 		echo "Installing GStreamer dependencies"
 		apt install -y \
 			gstreamer1.0-plugins-base \
+			libgstreamer-plugins-base1.0-dev \
 			gstreamer1.0-plugins-good \
 			gstreamer1.0-tools \
 			gstreamer1.0-libav \

ecosystem/gstreamer_plugin/build.sh
@@ -5,6 +5,11 @@ set -euo pipefail
 BUILD_DIR="builddir"
 DEBUG=false
 
+SCRIPT_NAME=$(basename "${BASH_SOURCE[0]}")
+SCRIPT_PATH=$(readlink -qe "${BASH_SOURCE[0]}")
+SCRIPT_FOLDER=${SCRIPT_PATH/$SCRIPT_NAME/}
+cd "${SCRIPT_FOLDER}" || exit 1
+
 # Parse command-line arguments
 for arg in "$@"; do
 	case "$arg" in