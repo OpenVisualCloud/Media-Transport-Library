5af0ee5f59003334b4e43343b809877774ea8f9a
Wesierski, Dawid <dawid.wesierski@intel.com>
2025-09-19 10:03:12 -0400

    Test: unit st30p timestamp validation tests
    Improve the application of the timestamp
    validation tests for the st30p functional tests.
    
    Use the time calculated from 0 to allow for
    deterministic results.
    
    Add tests for default timestamp generation and
    for user pacing mode.

--- tests/unittest/noctx.hpp
+++ tests/unittest/noctx.hpp
@@ -1,5 +1,5 @@
 /* SPDX-License-Identifier: BSD-3-Clause
- * Copyright(c) 2022 Intel Corporation
+ * Copyright(c) 2025 Intel Corporation
  */
 
 #include <gtest/gtest.h>
@@ -9,29 +9,50 @@
 
 #include "tests.hpp"
 
-template <typename SessionOps>
-struct ManagedSession {
-  std::thread tx_thread;
-  std::thread rx_thread;
+class SessionManager;
+class NoCtxTest;
+class St30pHandler;
+class TransmissionThread;
+
+/* Common structure accessible by rx and tx session thread responsibility of the handlers is to make it thread safe */
+struct sessionInfo {
+  uint32_t idx_tx;
+
+  uint32_t idx_rx;
+  double expect_fps;
+};
+
+/* Session class represents a single media session */
+class TransmissionThread {
+  public:
+  int id;
+  std::thread thread;
   std::atomic<bool> stop_flag{false};
-  SessionOps ops;
+  std::function<void(std::atomic<bool>&)> func;
+  void* user_data;
 
-  template <typename... Args>
-  ManagedSession(Args&&... args) : ops(std::forward<Args>(args)...) {
+  TransmissionThread(int session_id, std::function<void(std::atomic<bool>&)> f,
+          void* data = nullptr)
+      : id(session_id), func(f), user_data(data) {
   }
+};
 
-  void start_tx(std::function<void(SessionOps&, std::atomic<bool>&)> tx_func) {
-    tx_thread = std::thread([this, tx_func]() { tx_func(ops, stop_flag); });
-  }
+/* Creates a map of TransmissionThread vectors keyed by session id
+   basically you can create as many connected TransmissionThreads as needed */
+class SessionManager {
+  std::unordered_map<int, std::vector<std::unique_ptr<TransmissionThread>>> sessionGroups;
 
-  void start_rx(std::function<void(SessionOps&, std::atomic<bool>&)> rx_func) {
-    rx_thread = std::thread([this, rx_func]() { rx_func(ops, stop_flag); });
-  }
+ public:
+  int startAsPartOfSession(int id, std::function<void(std::atomic<bool>&)> func,
+                   void* user_data = nullptr);
+  int stopSession(int id);
+  void stopAll();
+  void* getUserData(int id);
+  bool isRunning(int id);
+  size_t getSessionCount() const;
 
-  void stop_and_join() {
-    stop_flag = true;
-    if (tx_thread.joinable()) tx_thread.join();
-    if (rx_thread.joinable()) rx_thread.join();
+  ~SessionManager() {
+    stopAll();
   }
 };
 
@@ -42,35 +63,76 @@ class NoCtxTest : public ::testing::Test {
   void SetUp() override;
   void TearDown() override;
 
- public:
-  static uint64_t TestPtpSourceSinceEpoch(void* priv);
+  SessionManager session_manager;
 
-  std::vector<std::thread> tx_thread;
-  std::vector<std::atomic<bool>> stop_flag_tx;
-  std::vector<std::thread> rx_thread;
-  std::vector<std::atomic<bool>> stop_flag_rx;
+ public:
+  uint defaultTestDuration;
+  static uint64_t TestPtpSourceSinceEpoch(void* priv);
+  std::vector<std::unique_ptr<St30pHandler>> st30pHandlers;
+
+  void txDefaultCheck(int session_idx, void* frame, size_t frame_size);
+  void rxDefaultCheck(int session_idx, void* frame, size_t frame_size);
+
+  void addSessions(int index, std::function<void(std::atomic<bool>&)> func);
+
+  int startManagedSession(int index, std::function<void(std::atomic<bool>&)> func,
+                          void* user_data = nullptr);
+
+  int startManagedSession(int index, std::function<void(std::atomic<bool>&)> func_tx,
+                          std::function<void(std::atomic<bool>&)> func_rx,
+                          void* user_data = nullptr);
+
+  int stopManagedSession(int index) {
+    return session_manager.stopSession(index);
+  }
+
+  void stopAllManagedSessions() {
+    session_manager.stopAll();
+  }
 };
 
-class NoCtxTestSt30p : public NoCtxTest {
+class St30pHandler {
+ private:
+  uint sessionIdx;
+  st_tests_context* ctx = nullptr;
+  uint msPerFramebuffer;
+
  public:
+  uint nsPacketTime;
+  sessionInfo sessionsUserData;
+  std::function<void(int session_idx, void* frame, size_t frame_size)>
+      txTestFrameModifier;
+  std::function<void(int session_idx, void* frame, size_t frame_size)>
+      rxTestFrameModifier;
+  St30pHandler(uint session_idx, st_tests_context* ctx);
+  ~St30pHandler();
+  uint transmissionPortDefault;
+  uint payloadTypeDefault;
+  uint framebufferSizeDefault;
   std::vector<enum st30_sampling> samplingModesDefault;
   std::vector<enum st30_ptime> ptimeModesDefault;
   std::vector<uint16_t> channelCountsDefault;
   std::vector<enum st30_fmt> fmtModesDefault;
-  std::vector<struct st30p_tx_ops> sessionsOpsTx;
-  std::vector<struct st30p_rx_ops> sessionsOpsRx;
-  std::vector<std::unique_ptr<ManagedSession<st30p_tx_ops>>> tx_sessions;
-  std::vector<std::unique_ptr<ManagedSession<st30p_rx_ops>>> rx_sessions;
 
-  void SetUp() override;
+  struct st30p_tx_ops sessionsOpsTx;
+  struct st30p_rx_ops sessionsOpsRx;
+  st30p_tx_handle sessionsHandleTx;
+  st30p_rx_handle sessionsHandleRx;
 
-  void createSessions(st30p_tx_ops ops, st30p_rx_ops ops_rx) {
-    sessionsOpsTx.push_back(ops);
-    sessionsOpsRx.push_back(ops_rx);
-  }
+  st30p_tx_ops* fillDefaultSt30pTxOps(int defaultValuesIdx = 0);
+  st30p_rx_ops* fillDefaultSt30pRxOps(int defaultValuesIdx = 0);
 
-  size_t create_tx_session(const st30p_tx_ops& ops) {
-    tx_sessions.push_back(std::make_unique<ManagedSession<st30p_tx_ops>>(ops));
-    return tx_sessions.size() - 1;
-  }
+  void createSession(st30p_tx_ops ops_tx = {}, st30p_rx_ops ops_rx = {});
+  void startDefaultSession();
+  void createTxSession();
+  void createRxSession();
+
+  void st30pTxDefaultFunction(int session_idx, std::atomic<bool>& stop_flag);
+  void st30pRxDefaultFunction(int session_idx, std::atomic<bool>& stop_flag);
+
+  /* test modifier functions */
+  void rxSt30DefaultTimestampsCheck(int session_idx, void* frame, size_t frame_size);
+
+  void txSt30DefaultUserPacing(int session_idx, void* frame, size_t frame_size);
+  void rxSt30DefaultUserPacingCheck(int session_idx, void* frame, size_t frame_size);
 };
