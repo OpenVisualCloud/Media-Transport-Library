OpenVisualCloud/Media-Transport-Library

commit c43152d97a25b501fc68adc3e126ebde8e6e594b

Add support for new ST2110-41 (Fast Metadata Framework, "fmd") standard. (#951)
Add support for new ST2110-41 (Fast Metadata Framework) standard.

Add support for new ST2110-41 (Fast Metadata Framework, "fmd") standard.
As abbreviation in the code "fmd".

Support for st41 in /include/ directory.
Support for st41 in RxTxApp.
with added options in JSON file in RxTxApp (for RX and TX):
            "fastmetadata": [
with options for TX:
                    "fastmetadata_data_item_type": 3648364,
                    "fastmetadata_k_bit": 1,
                    "fastmetadata_url": "test_abc.txt", 
                    "fastmetadata_fps": "p59"

lib/src/st2110/st_rx_fastmetadata_session.h
@@ -1,0 +1,16 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * SPDX-FileCopyrightText: Copyright (c) 2024 Intel Corporation
+ */
+
+#ifndef _ST_LIB_RX_FASTMETADATA_SESSION_HEAD_H_
+#define _ST_LIB_RX_FASTMETADATA_SESSION_HEAD_H_
+
+#include "st_main.h"
+
+#define ST_RX_FASTMETADATA_BURST_SIZE (128)
+
+#define ST_RX_FASTMETADATA_PREFIX "RC_"
+
+int st_rx_fastmetadata_sessions_sch_uinit(struct mtl_sch_impl* sch);
+
+#endif /* _ST_LIB_RX_FASTMETADATA_SESSION_HEAD_H_ */

lib/src/st2110/st_tx_fastmetadata_session.h
@@ -1,0 +1,14 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * SPDX-FileCopyrightText: Copyright (c) 2024 Intel Corporation
+ */
+
+#ifndef _ST_LIB_TX_FASTMETADATA_SESSION_HEAD_H_
+#define _ST_LIB_TX_FASTMETADATA_SESSION_HEAD_H_
+
+#include "st_main.h"
+
+#define ST_TX_FASTMETADATA_PREFIX "TC_"
+
+int st_tx_fastmetadata_sessions_sch_uinit(struct mtl_sch_impl* sch);
+
+#endif /* _ST_LIB_TX_FASTMETADATA_SESSION_HEAD_H_ */

app/src/app_base.h
@@ -14,6 +14,7 @@
 #include <mtl/st30_api.h>
 #include <mtl/st30_pipeline_api.h>
 #include <mtl/st40_api.h>
+#include <mtl/st41_api.h>
 #include <mtl/st_pipeline_api.h>
 #include <pcap.h>
 #include <pthread.h>
@@ -40,6 +41,10 @@
 
 #define ST_APP_MAX_TX_ANC_SESSIONS (180)
 #define ST_APP_MAX_RX_ANC_SESSIONS (180)
+
+/* FMD = Fast Metadata (ST2110-41) */
+#define ST_APP_MAX_TX_FMD_SESSIONS (180)
+#define ST_APP_MAX_RX_FMD_SESSIONS (180)
 
 #define ST_APP_MAX_LCORES (32)
 
@@ -226,6 +231,37 @@ struct st_app_tx_anc_session {
   uint32_t st40_seq_id;
 };
 
+struct st_app_tx_fmd_session {
+  int idx;
+  st41_tx_handle handle;
+
+  uint16_t framebuff_cnt;
+
+  uint16_t framebuff_producer_idx;
+  uint16_t framebuff_consumer_idx;
+  struct st_tx_frame* framebuffs;
+
+  uint32_t st41_frame_done_cnt;
+  uint32_t st41_packet_done_cnt;
+
+  char st41_source_url[ST_APP_URL_MAX_LEN + 1];
+  int st41_source_fd;
+  pcap_t* st41_pcap;
+  bool st41_pcap_input;
+  uint32_t st41_dit;
+  uint32_t st41_k_bit;
+  bool st41_rtp_input;
+  uint8_t* st41_source_begin;
+  uint8_t* st41_source_end;
+  uint8_t* st41_frame_cursor; /* cursor to current frame */
+  pthread_t st41_app_thread;
+  bool st41_app_thread_stop;
+  pthread_cond_t st41_wake_cond;
+  pthread_mutex_t st41_wake_mutex;
+  uint32_t st41_rtp_tmstamp;
+  uint32_t st41_seq_id;
+};
+
 struct st_app_rx_video_session {
   int idx;
   mtl_handle st;
@@ -321,6 +357,19 @@ struct st_app_rx_anc_session {
   pthread_cond_t st40_wake_cond;
   pthread_mutex_t st40_wake_mutex;
   bool st40_app_thread_stop;
+
+  /* stat */
+  int stat_frame_total_received;
+  uint64_t stat_frame_first_rx_time;
+};
+
+struct st_app_rx_fmd_session {
+  int idx;
+  st41_rx_handle handle;
+  pthread_t st41_app_thread;
+  pthread_cond_t st41_wake_cond;
+  pthread_mutex_t st41_wake_mutex;
+  bool st41_app_thread_stop;
 
   /* stat */
   int stat_frame_total_received;
@@ -610,6 +659,12 @@ struct st_app_context {
   int tx_anc_rtp_ring_size; /* the ring size for tx anc rtp type */
   bool tx_anc_dedicate_queue;
 
+  struct st_app_tx_fmd_session* tx_fmd_sessions;
+  char tx_fmd_url[ST_APP_URL_MAX_LEN];
+  int tx_fmd_session_cnt;
+  int tx_fmd_rtp_ring_size; /* the ring size for tx fmd rtp type */
+  bool tx_fmd_dedicate_queue;
+
   char tx_st22p_url[ST_APP_URL_MAX_LEN]; /* send st22p content url*/
   struct st_app_tx_st22p_session* tx_st22p_sessions;
   int tx_st22p_session_cnt;
@@ -639,6 +694,9 @@ struct st_app_context {
 
   struct st_app_rx_anc_session* rx_anc_sessions;
   int rx_anc_session_cnt;
+
+  struct st_app_rx_fmd_session* rx_fmd_sessions;
+  int rx_fmd_session_cnt;
 
   struct st_app_rx_st22p_session* rx_st22p_sessions;
   int rx_st22p_session_cnt;

app/src/args.c
@@ -37,6 +37,9 @@ enum st_args_cmd {
   ST_ARG_TX_ANC_URL,
   ST_ARG_TX_ANC_SESSIONS_CNT,
   ST_ARG_TX_ANC_RTP_RING_SIZE,
+  ST_ARG_TX_FMD_URL,
+  ST_ARG_TX_FMD_SESSIONS_CNT,
+  ST_ARG_TX_FMD_RTP_RING_SIZE,
   ST22_ARG_TX_SESSIONS_CNT,
   ST22_ARG_TX_URL,
   ST_ARG_RX_VIDEO_SESSIONS_CNT,
@@ -48,6 +51,7 @@ enum st_args_cmd {
   ST_ARG_RX_AUDIO_RTP_RING_SIZE,
   ST_ARG_RX_AUDIO_DUMP_TIME_S,
   ST_ARG_RX_ANC_SESSIONS_CNT,
+  ST_ARG_RX_FMD_SESSIONS_CNT,
   ST22_ARG_RX_SESSIONS_CNT,
   ST_ARG_HDR_SPLIT,
   ST_ARG_PACING_WAY,
@@ -134,6 +138,7 @@ enum st_args_cmd {
   ST_ARG_AUDIO_RL_OFFSET_US,
   ST_ARG_AUDIO_FIFO_SIZE,
   ST_ARG_ANC_DEDICATE_QUEUE,
+  ST_ARG_FMD_DEDICATE_QUEUE,
   ST_ARG_TX_NO_BURST_CHECK,
   ST_ARG_DHCP,
   ST_ARG_IOVA_MODE,
@@ -183,6 +188,9 @@ static struct option st_app_args_options[] = {
     {"tx_anc_url", required_argument, 0, ST_ARG_TX_ANC_URL},
     {"tx_anc_sessions_count", required_argument, 0, ST_ARG_TX_ANC_SESSIONS_CNT},
     {"tx_anc_rtp_ring_size", required_argument, 0, ST_ARG_TX_ANC_RTP_RING_SIZE},
+    {"tx_fmd_url", required_argument, 0, ST_ARG_TX_FMD_URL},
+    {"tx_fmd_sessions_count", required_argument, 0, ST_ARG_TX_FMD_SESSIONS_CNT},
+    {"tx_fmd_rtp_ring_size", required_argument, 0, ST_ARG_TX_FMD_RTP_RING_SIZE},
     {"tx_st22_sessions_count", required_argument, 0, ST22_ARG_TX_SESSIONS_CNT},
     {"tx_st22_url", required_argument, 0, ST22_ARG_TX_URL},
 
@@ -195,6 +203,7 @@ static struct option st_app_args_options[] = {
     {"rx_audio_rtp_ring_size", required_argument, 0, ST_ARG_RX_AUDIO_RTP_RING_SIZE},
     {"rx_audio_dump_time_s", required_argument, 0, ST_ARG_RX_AUDIO_DUMP_TIME_S},
     {"rx_anc_sessions_count", required_argument, 0, ST_ARG_RX_ANC_SESSIONS_CNT},
+    {"rx_fmd_sessions_count", required_argument, 0, ST_ARG_RX_FMD_SESSIONS_CNT},
     {"rx_st22_sessions_count", required_argument, 0, ST22_ARG_RX_SESSIONS_CNT},
     {"hdr_split", no_argument, 0, ST_ARG_HDR_SPLIT},
     {"pacing_way", required_argument, 0, ST_ARG_PACING_WAY},
@@ -277,6 +286,7 @@ static struct option st_app_args_options[] = {
     {"audio_rl_offset", required_argument, 0, ST_ARG_AUDIO_RL_OFFSET_US},
     {"audio_fifo_size", required_argument, 0, ST_ARG_AUDIO_FIFO_SIZE},
     {"anc_dedicate_queue", no_argument, 0, ST_ARG_ANC_DEDICATE_QUEUE},
+    {"fmd_dedicate_queue", no_argument, 0, ST_ARG_FMD_DEDICATE_QUEUE},
     {"tx_no_burst_check", no_argument, 0, ST_ARG_TX_NO_BURST_CHECK},
     {"dhcp", no_argument, 0, ST_ARG_DHCP},
     {"iova_mode", required_argument, 0, ST_ARG_IOVA_MODE},
@@ -351,12 +361,14 @@ static int app_args_json(struct st_app_context* ctx, struct mtl_init_params* p,
   ctx->tx_video_session_cnt = ctx->json_ctx->tx_video_session_cnt;
   ctx->tx_audio_session_cnt = ctx->json_ctx->tx_audio_session_cnt;
   ctx->tx_anc_session_cnt = ctx->json_ctx->tx_anc_session_cnt;
+  ctx->tx_fmd_session_cnt = ctx->json_ctx->tx_fmd_session_cnt;
   ctx->tx_st22p_session_cnt = ctx->json_ctx->tx_st22p_session_cnt;
   ctx->tx_st20p_session_cnt = ctx->json_ctx->tx_st20p_session_cnt;
   ctx->tx_st30p_session_cnt = ctx->json_ctx->tx_st30p_session_cnt;
   ctx->rx_video_session_cnt = ctx->json_ctx->rx_video_session_cnt;
   ctx->rx_audio_session_cnt = ctx->json_ctx->rx_audio_session_cnt;
   ctx->rx_anc_session_cnt = ctx->json_ctx->rx_anc_session_cnt;
+  ctx->rx_fmd_session_cnt = ctx->json_ctx->rx_fmd_session_cnt;
   ctx->rx_st22p_session_cnt = ctx->json_ctx->rx_st22p_session_cnt;
   ctx->rx_st20p_session_cnt = ctx->json_ctx->rx_st20p_session_cnt;
   ctx->rx_st30p_session_cnt = ctx->json_ctx->rx_st30p_session_cnt;
@@ -513,6 +525,15 @@ int st_app_parse_args(struct st_app_context* ctx, struct mtl_init_params* p, int
       case ST_ARG_TX_ANC_SESSIONS_CNT:
         ctx->tx_anc_session_cnt = atoi(optarg);
         break;
+      case ST_ARG_TX_FMD_URL:
+        snprintf(ctx->tx_fmd_url, sizeof(ctx->tx_fmd_url), "%s", optarg);
+        break;
+      case ST_ARG_TX_FMD_RTP_RING_SIZE:
+        ctx->tx_fmd_rtp_ring_size = atoi(optarg);
+        break;
+      case ST_ARG_TX_FMD_SESSIONS_CNT:
+        ctx->tx_fmd_session_cnt = atoi(optarg);
+        break;
       case ST_ARG_RX_VIDEO_SESSIONS_CNT:
         ctx->rx_video_session_cnt = atoi(optarg);
         break;
@@ -539,6 +560,9 @@ int st_app_parse_args(struct st_app_context* ctx, struct mtl_init_params* p, int
         break;
       case ST_ARG_RX_ANC_SESSIONS_CNT:
         ctx->rx_anc_session_cnt = atoi(optarg);
+        break;
+      case ST_ARG_RX_FMD_SESSIONS_CNT:
+        ctx->rx_fmd_session_cnt = atoi(optarg);
         break;
       case ST22_ARG_TX_SESSIONS_CNT:
         ctx->tx_st22_session_cnt = atoi(optarg);
@@ -855,6 +879,9 @@ int st_app_parse_args(struct st_app_context* ctx, struct mtl_init_params* p, int
         break;
       case ST_ARG_ANC_DEDICATE_QUEUE:
         ctx->tx_anc_dedicate_queue = true;
+        break;
+      case ST_ARG_FMD_DEDICATE_QUEUE:
+        ctx->tx_fmd_dedicate_queue = true;
         break;
       case ST_ARG_DHCP:
         for (int port = 0; port < MTL_PORT_MAX; ++port)

app/src/fmt.h
@@ -13,6 +13,7 @@
 #define ST_APP_PAYLOAD_TYPE_AUDIO (111)
 #define ST_APP_PAYLOAD_TYPE_ANCILLARY (113)
 #define ST_APP_PAYLOAD_TYPE_ST22 (114)
+#define ST_APP_PAYLOAD_TYPE_FASTMETADATA (115)
 
 #define ARRAY_SIZE(arr) (sizeof(arr) / sizeof(arr[0]))
 

app/src/meson.build
@@ -5,6 +5,7 @@ sources = files('rxtx_app.c', 'args.c', 'parse_json.c', 'fmt.c',
 				'tx_st20p_app.c', 'rx_st20p_app.c',
 				'tx_st22p_app.c', 'rx_st22p_app.c',
 				'tx_ancillary_app.c', 'rx_ancillary_app.c',
+				'tx_fastmetadata_app.c', 'rx_fastmetadata_app.c',
 				'tx_st30p_app.c', 'rx_st30p_app.c',
 				)
 

app/src/parse_json.c
@@ -1219,6 +1219,134 @@ static int st_json_parse_rx_anc(int idx, json_object* anc_obj,
   return ST_JSON_SUCCESS;
 }
 
+static int st_json_parse_tx_fmd(int idx, json_object* fmd_obj,
+                                st_json_fastmetadata_session_t* fmd) {
+  if (fmd_obj == NULL || fmd == NULL) {
+    err("%s, can not parse tx fmd session\n", __func__);
+    return -ST_JSON_NULL;
+  }
+  int ret;
+
+  /* parse udp port  */
+  ret = parse_base_udp_port(fmd_obj, &fmd->base, idx);
+  if (ret < 0) return ret;
+
+  /* parse payload type */
+  ret = parse_base_payload_type(fmd_obj, &fmd->base);
+  if (ret < 0) {
+    err("%s, use default pt %u\n", __func__, ST_APP_PAYLOAD_TYPE_FASTMETADATA);
+    fmd->base.payload_type = ST_APP_PAYLOAD_TYPE_FASTMETADATA;
+  }
+
+  /* parse fmd type */
+  const char* type = json_object_get_string(st_json_object_object_get(fmd_obj, "type"));
+  REQUIRED_ITEM(type);
+  if (strcmp(type, "frame") == 0) {
+    fmd->info.type = ST41_TYPE_FRAME_LEVEL;
+  } else if (strcmp(type, "rtp") == 0) {
+    fmd->info.type = ST41_TYPE_RTP_LEVEL;
+  } else {
+    err("%s, invalid fmd type %s\n", __func__, type);
+    return -ST_JSON_NOT_VALID;
+  }
+
+  /* parse fmd data item type */
+  json_object* fmd_dit_obj =
+      st_json_object_object_get(fmd_obj, "fastmetadata_data_item_type");
+  if (fmd_dit_obj) {
+    uint32_t fmd_dit = json_object_get_int(fmd_dit_obj);
+    if (fmd_dit < 0 || fmd_dit > 0x3fffff) {
+      err("%s, invalid fastmetadata_data_item_type 0x%x\n", __func__, fmd_dit);
+      return -ST_JSON_NOT_VALID;
+    }
+    fmd->info.fmd_dit = fmd_dit;
+    info("%s, fastmetadata_data_item_type = 0x%x\n", __func__, fmd_dit);
+  } else {
+    err("%s, No fastmetadata_data_item_type !\n", __func__);
+    return -ST_JSON_NULL;
+  }
+
+  /* parse fmd data item K-bit */
+  json_object* fmd_k_bit_obj = st_json_object_object_get(fmd_obj, "fastmetadata_k_bit");
+  if (fmd_k_bit_obj) {
+    uint8_t fmd_k_bit = json_object_get_int(fmd_k_bit_obj);
+    if (fmd_k_bit < 0 || fmd_k_bit > 1) {
+      err("%s, invalid fastmetadata_k_bit 0x%x\n", __func__, fmd_k_bit);
+      return -ST_JSON_NOT_VALID;
+    }
+    fmd->info.fmd_k_bit = fmd_k_bit;
+    info("%s, fastmetadata_k_bit = 0x%x\n", __func__, fmd_k_bit);
+  } else {
+    err("%s, No fastmetadata_k_bit !\n", __func__);
+    return -ST_JSON_NULL;
+  }
+
+  /* parse fmd fps */
+  const char* fmd_fps =
+      json_object_get_string(st_json_object_object_get(fmd_obj, "fastmetadata_fps"));
+  REQUIRED_ITEM(fmd_fps);
+  if (strcmp(fmd_fps, "p59") == 0) {
+    fmd->info.fmd_fps = ST_FPS_P59_94;
+  } else if (strcmp(fmd_fps, "p50") == 0) {
+    fmd->info.fmd_fps = ST_FPS_P50;
+  } else if (strcmp(fmd_fps, "p25") == 0) {
+    fmd->info.fmd_fps = ST_FPS_P25;
+  } else if (strcmp(fmd_fps, "p29") == 0) {
+    fmd->info.fmd_fps = ST_FPS_P29_97;
+  } else {
+    err("%s, invalid fmd fps %s\n", __func__, fmd_fps);
+    return -ST_JSON_NOT_VALID;
+  }
+
+  /* parse fmd interlaced */
+  json_object* fmd_interlaced = st_json_object_object_get(fmd_obj, "interlaced");
+  if (fmd_interlaced) {
+    fmd->info.interlaced = json_object_get_boolean(fmd_interlaced);
+  }
+
+  /* parse fmd url */
+  ret = parse_url(fmd_obj, "fastmetadata_url", fmd->info.fmd_url);
+  if (ret < 0) return ret;
+
+  /* parse enable rtcp */
+  fmd->enable_rtcp =
+      json_object_get_boolean(st_json_object_object_get(fmd_obj, "enable_rtcp"));
+
+  return ST_JSON_SUCCESS;
+}
+
+static int st_json_parse_rx_fmd(int idx, json_object* fmd_obj,
+                                st_json_fastmetadata_session_t* fmd) {
+  if (fmd_obj == NULL || fmd == NULL) {
+    err("%s, can not parse rx fmd session\n", __func__);
+    return -ST_JSON_NULL;
+  }
+  int ret;
+
+  /* parse udp port  */
+  ret = parse_base_udp_port(fmd_obj, &fmd->base, idx);
+  if (ret < 0) return ret;
+
+  /* parse payload type */
+  ret = parse_base_payload_type(fmd_obj, &fmd->base);
+  if (ret < 0) {
+    err("%s, use default pt %u\n", __func__, ST_APP_PAYLOAD_TYPE_FASTMETADATA);
+    fmd->base.payload_type = ST_APP_PAYLOAD_TYPE_FASTMETADATA;
+  }
+
+  /* parse fmd interlaced */
+  json_object* fmd_interlaced = st_json_object_object_get(fmd_obj, "interlaced");
+  if (fmd_interlaced) {
+    fmd->info.interlaced = json_object_get_boolean(fmd_interlaced);
+  }
+
+  /* parse enable rtcp */
+  fmd->enable_rtcp =
+      json_object_get_boolean(st_json_object_object_get(fmd_obj, "enable_rtcp"));
+
+  return ST_JSON_SUCCESS;
+}
+
 static int parse_st22p_width(json_object* st22p_obj, st_json_st22p_session_t* st22p) {
   int width = json_object_get_int(st_json_object_object_get(st22p_obj, "width"));
   if (width <= 0) {
@@ -1265,7 +1393,7 @@ static int parse_st22p_fps(json_object* st22p_obj, st_json_st22p_session_t* st22
   } else if (strcmp(fps, "p23") == 0) {
     st22p->info.fps = ST_FPS_P23_98;
   } else {
-    err("%s, invalid anc fps %s\n", __func__, fps);
+    err("%s, invalid st22 fps %s\n", __func__, fps);
     return -ST_JSON_NOT_VALID;
   }
   return ST_JSON_SUCCESS;
@@ -1611,7 +1739,7 @@ static int parse_st20p_fps(json_object* st20p_obj, st_json_st20p_session_t* st20
   } else if (strcmp(fps, "p23") == 0) {
     st20p->info.fps = ST_FPS_P23_98;
   } else {
-    err("%s, invalid anc fps %s\n", __func__, fps);
+    err("%s, invalid st20 fps %s\n", __func__, fps);
     return -ST_JSON_NOT_VALID;
   }
   return ST_JSON_SUCCESS;
@@ -2001,6 +2129,10 @@ void st_app_free_json(st_json_context_t* ctx) {
     st_app_free(ctx->tx_anc_sessions);
     ctx->tx_anc_sessions = NULL;
   }
+  if (ctx->tx_fmd_sessions) {
+    st_app_free(ctx->tx_fmd_sessions);
+    ctx->tx_fmd_sessions = NULL;
+  }
   if (ctx->tx_st22p_sessions) {
     st_app_free(ctx->tx_st22p_sessions);
     ctx->tx_st22p_sessions = NULL;
@@ -2024,6 +2156,10 @@ void st_app_free_json(st_json_context_t* ctx) {
   if (ctx->rx_anc_sessions) {
     st_app_free(ctx->rx_anc_sessions);
     ctx->rx_anc_sessions = NULL;
+  }
+  if (ctx->rx_fmd_sessions) {
+    st_app_free(ctx->rx_fmd_sessions);
+    ctx->rx_fmd_sessions = NULL;
   }
   if (ctx->rx_st22p_sessions) {
     st_app_free(ctx->rx_st22p_sessions);
@@ -2176,6 +2312,10 @@ int st_app_parse_json(st_json_context_t* ctx, const char* filename) {
       num = parse_session_num(tx_group, "ancillary");
       if (num < 0) goto error;
       ctx->tx_anc_session_cnt += num;
+      /* parse tx fastmetadata sessions */
+      num = parse_session_num(tx_group, "fastmetadata");
+      if (num < 0) goto error;
+      ctx->tx_fmd_session_cnt += num;
       /* parse tx st22p sessions */
       num = parse_session_num(tx_group, "st22p");
       if (num < 0) goto error;
@@ -2212,6 +2352,13 @@ int st_app_parse_json(st_json_context_t* ctx, const char* filename) {
       ret = -ST_JSON_NULL;
       goto error;
     }
+    ctx->tx_fmd_sessions = (st_json_fastmetadata_session_t*)st_app_zmalloc(
+        ctx->tx_fmd_session_cnt * sizeof(st_json_fastmetadata_session_t));
+    if (!ctx->tx_fmd_sessions) {
+      err("%s, failed to allocate tx_fmd_sessions\n", __func__);
+      ret = -ST_JSON_NULL;
+      goto error;
+    }
     ctx->tx_st22p_sessions = (st_json_st22p_session_t*)st_app_zmalloc(
         ctx->tx_st22p_session_cnt * sizeof(st_json_st22p_session_t));
     if (!ctx->tx_st22p_sessions) {
@@ -2238,6 +2385,7 @@ int st_app_parse_json(st_json_context_t* ctx, const char* filename) {
     int num_video = 0;
     int num_audio = 0;
     int num_anc = 0;
+    int num_fmd = 0;
     int num_st22p = 0;
     int num_st20p = 0;
     int num_st30p = 0;
@@ -2401,6 +2549,37 @@ int st_app_parse_json(st_json_context_t* ctx, const char* filename) {
         }
       }
 
+      /* parse tx fastmetadata sessions */
+      json_object* fmd_array = st_json_object_object_get(tx_group, "fastmetadata");
+      if (fmd_array != NULL && json_object_get_type(fmd_array) == json_type_array) {
+        for (int j = 0; j < json_object_array_length(fmd_array); ++j) {
+          json_object* fmd_session = json_object_array_get_idx(fmd_array, j);
+          int replicas =
+              json_object_get_int(st_json_object_object_get(fmd_session, "replicas"));
+          if (replicas < 0) {
+            err("%s, invalid replicas number: %d\n", __func__, replicas);
+            ret = -ST_JSON_NOT_VALID;
+            goto error;
+          }
+          for (int k = 0; k < replicas; ++k) {
+            parse_session_ip(json_object_get_string(dip_p),
+                             &ctx->tx_fmd_sessions[num_fmd].base, MTL_SESSION_PORT_P);
+            ctx->tx_fmd_sessions[num_fmd].base.inf[0] = &ctx->interfaces[inf_p];
+            ctx->interfaces[inf_p].tx_fmd_sessions_cnt++;
+            if (num_inf == 2) {
+              parse_session_ip(json_object_get_string(dip_r),
+                               &ctx->tx_fmd_sessions[num_fmd].base, MTL_SESSION_PORT_R);
+              ctx->tx_fmd_sessions[num_fmd].base.inf[1] = &ctx->interfaces[inf_r];
+              ctx->interfaces[inf_r].tx_fmd_sessions_cnt++;
+            }
+            ctx->tx_fmd_sessions[num_fmd].base.num_inf = num_inf;
+            ret = st_json_parse_tx_fmd(k, fmd_session, &ctx->tx_fmd_sessions[num_fmd]);
+            if (ret) goto error;
+            num_fmd++;
+          }
+        }
+      }
+
       /* parse tx st22p sessions */
       json_object* st22p_array = st_json_object_object_get(tx_group, "st22p");
       if (st22p_array != NULL && json_object_get_type(st22p_array) == json_type_array) {
@@ -2528,6 +2707,10 @@ int st_app_parse_json(st_json_context_t* ctx, const char* filename) {
       num = parse_session_num(rx_group, "ancillary");
       if (num < 0) goto error;
       ctx->rx_anc_session_cnt += num;
+      /* parse rx fastmetadata sessions */
+      num = parse_session_num(rx_group, "fastmetadata");
+      if (num < 0) goto error;
+      ctx->rx_fmd_session_cnt += num;
       /* parse rx st22p sessions */
       num = parse_session_num(rx_group, "st22p");
       if (num < 0) goto error;
@@ -2568,6 +2751,13 @@ int st_app_parse_json(st_json_context_t* ctx, const char* filename) {
       ret = -ST_JSON_NULL;
       goto error;
     }
+    ctx->rx_fmd_sessions = (st_json_fastmetadata_session_t*)st_app_zmalloc(
+        ctx->rx_fmd_session_cnt * sizeof(st_json_fastmetadata_session_t));
+    if (!ctx->rx_fmd_sessions) {
+      err("%s, failed to allocate rx_fmd_sessions\n", __func__);
+      ret = -ST_JSON_NULL;
+      goto error;
+    }
     ctx->rx_st22p_sessions = (st_json_st22p_session_t*)st_app_zmalloc(
         ctx->rx_st22p_session_cnt * sizeof(st_json_st22p_session_t));
     if (!ctx->rx_st22p_sessions) {
@@ -2601,6 +2791,7 @@ int st_app_parse_json(st_json_context_t* ctx, const char* filename) {
     int num_video = 0;
     int num_audio = 0;
     int num_anc = 0;
+    int num_fmd = 0;
     int num_st22p = 0;
     int num_st20p = 0;
     int num_st20r = 0;
@@ -2801,6 +2992,44 @@ int st_app_parse_json(st_json_context_t* ctx, const char* filename) {
             ret = st_json_parse_rx_anc(k, anc_session, &ctx->rx_anc_sessions[num_anc]);
             if (ret) goto error;
             num_anc++;
+          }
+        }
+      }
+
+      /* parse rx fastmetadata sessions */
+      json_object* fmd_array = st_json_object_object_get(rx_group, "fastmetadata");
+      if (fmd_array != NULL && json_object_get_type(fmd_array) == json_type_array) {
+        for (int j = 0; j < json_object_array_length(fmd_array); ++j) {
+          json_object* fmd_session = json_object_array_get_idx(fmd_array, j);
+          int replicas =
+              json_object_get_int(st_json_object_object_get(fmd_session, "replicas"));
+          if (replicas < 0) {
+            err("%s, invalid replicas number: %d\n", __func__, replicas);
+            ret = -ST_JSON_NOT_VALID;
+            goto error;
+          }
+          for (int k = 0; k < replicas; ++k) {
+            parse_session_ip(json_object_get_string(ip_p),
+                             &ctx->rx_fmd_sessions[num_fmd].base, MTL_SESSION_PORT_P);
+            if (mcast_src_ip_p)
+              parse_mcast_src_ip(json_object_get_string(mcast_src_ip_p),
+                                 &ctx->rx_fmd_sessions[num_fmd].base, MTL_SESSION_PORT_P);
+            ctx->rx_fmd_sessions[num_fmd].base.inf[0] = &ctx->interfaces[inf_p];
+            ctx->interfaces[inf_p].rx_fmd_sessions_cnt++;
+            if (num_inf == 2) {
+              parse_session_ip(json_object_get_string(ip_r),
+                               &ctx->rx_fmd_sessions[num_fmd].base, MTL_SESSION_PORT_R);
+              if (mcast_src_ip_r)
+                parse_mcast_src_ip(json_object_get_string(mcast_src_ip_r),
+                                   &ctx->rx_fmd_sessions[num_fmd].base,
+                                   MTL_SESSION_PORT_R);
+              ctx->rx_fmd_sessions[num_fmd].base.inf[1] = &ctx->interfaces[inf_r];
+              ctx->interfaces[inf_r].rx_fmd_sessions_cnt++;
+            }
+            ctx->rx_fmd_sessions[num_fmd].base.num_inf = num_inf;
+            ret = st_json_parse_rx_fmd(k, fmd_session, &ctx->rx_fmd_sessions[num_fmd]);
+            if (ret) goto error;
+            num_fmd++;
           }
         }
       }

app/src/parse_json.h
@@ -6,6 +6,7 @@
 #include <math.h>
 #include <mtl/st30_api.h>
 #include <mtl/st40_api.h>
+#include <mtl/st41_api.h>
 #include <mtl/st_pipeline_api.h>
 #include <stdbool.h>
 #include <stdio.h>
@@ -126,6 +127,8 @@ typedef struct st_json_interface {
   int rx_audio_sessions_cnt; /* st30 on interface level */
   int tx_anc_sessions_cnt;   /* st40 on interface level */
   int rx_anc_sessions_cnt;   /* st40 on interface level */
+  int tx_fmd_sessions_cnt;   /* st41 on interface level */
+  int rx_fmd_sessions_cnt;   /* st41 on interface level */
 } st_json_interface_t;
 
 enum st_json_ip_type {
@@ -176,6 +179,16 @@ typedef struct st_json_ancillary_info {
 
   char anc_url[ST_APP_URL_MAX_LEN];
 } st_json_ancillary_info_t;
+
+typedef struct st_json_fastmetadata_info {
+  enum st41_type type;
+  uint32_t fmd_dit;
+  uint8_t fmd_k_bit;
+  enum st_fps fmd_fps;
+  bool interlaced;
+
+  char fmd_url[ST_APP_URL_MAX_LEN];
+} st_json_fastmetadata_info_t;
 
 typedef struct st_json_st22p_info {
   enum st_frame_fmt format;
@@ -238,6 +251,13 @@ typedef struct st_json_ancillary_session {
   bool enable_rtcp;
 } st_json_ancillary_session_t;
 
+typedef struct st_json_fastmetadata_session {
+  st_json_session_base_t base;
+  st_json_fastmetadata_info_t info;
+
+  bool enable_rtcp;
+} st_json_fastmetadata_session_t;
+
 typedef struct st_json_st22p_session {
   st_json_session_base_t base;
   st_json_st22p_info_t info;
@@ -275,6 +295,8 @@ typedef struct st_json_context {
   int tx_audio_session_cnt;
   st_json_ancillary_session_t* tx_anc_sessions;
   int tx_anc_session_cnt;
+  st_json_fastmetadata_session_t* tx_fmd_sessions;
+  int tx_fmd_session_cnt;
   st_json_st22p_session_t* tx_st22p_sessions;
   int tx_st22p_session_cnt;
   st_json_st20p_session_t* tx_st20p_sessions;
@@ -288,6 +310,8 @@ typedef struct st_json_context {
   int rx_audio_session_cnt;
   st_json_ancillary_session_t* rx_anc_sessions;
   int rx_anc_session_cnt;
+  st_json_fastmetadata_session_t* rx_fmd_sessions;
+  int rx_fmd_session_cnt;
   st_json_st22p_session_t* rx_st22p_sessions;
   int rx_st22p_session_cnt;
   st_json_st20p_session_t* rx_st20p_sessions;

commit 58f28167a3cba5b00ba0303cf434d4ca58f2cbbc

Fix: ST41 ring name conflicts when using ancillary prefixes
ST41 sessions were using ancillary prefixes for ring names,
causing naming conflicts when both are initialized simultaneously.

Fixes: c43152d97a25b501fc68adc3e126ebde8e6e594b

lib/src/st2110/st_rx_fastmetadata_session.h
@@ -9,7 +9,7 @@
 
 #define ST_RX_FASTMETADATA_BURST_SIZE (128)
 
-#define ST_RX_FASTMETADATA_PREFIX "RC_"
+#define ST_RX_FASTMETADATA_PREFIX "RF_"
 
 int st_rx_fastmetadata_sessions_sch_uinit(struct mtl_sch_impl* sch);
 

lib/src/st2110/st_tx_fastmetadata_session.h
@@ -7,7 +7,7 @@
 
 #include "st_main.h"
 
-#define ST_TX_FASTMETADATA_PREFIX "TC_"
+#define ST_TX_FASTMETADATA_PREFIX "TF_"
 
 int st_tx_fastmetadata_sessions_sch_uinit(struct mtl_sch_impl* sch);
 