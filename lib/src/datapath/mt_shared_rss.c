d21e2eb66a76f722e4a421b4dccd3b3f0bf3426d
Swirydczuk, Michal <michal.swirydczuk@intel.com>
2025-10-23 17:53:39 +0200

    Fixed Linter check again2

--- lib/src/datapath/mt_shared_rss.c
+++ lib/src/datapath/mt_shared_rss.c
@@ -418,7 +418,7 @@ int mt_srss_init(struct mtl_main_impl* impl) {
     srss->queue_mode =
         mt_pmd_is_native_af_xdp(impl, port) ? MT_QUEUE_MODE_XDP : MT_QUEUE_MODE_DPDK;
     srss->nb_rx_q = mt_if(impl, port)->nb_rx_q;
-        
+
     srss->lists_sz = 64 - 1; /* use odd count for better distribution */
     srss->lists = mt_rte_zmalloc_socket(sizeof(*srss->lists) * srss->lists_sz,
                                         mt_socket_id(impl, port));
