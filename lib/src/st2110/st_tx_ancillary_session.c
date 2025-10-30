OpenVisualCloud/Media-Transport-Library

commit be5563dd70af95475446c4cefe6d55e93f1543e9

Fix: ST40/ST41 first packet timestamp
Packet timestamps were only calculated when
the RTP time differed from the previous frame,
which failed for the initial packet. This caused
the first TX packet to be transmitted with
timestamp 0, resulting in DDoS-like behavior
on the RX side.

During initalization make the session calculate
the timestamp to avoid such issues.

To test set the date into problematic clock area
sudo date -s "2025-10-29 17:28:30"

lib/src/st2110/st_tx_ancillary_session.c
@@ -1574,6 +1574,7 @@ static int tx_ancillary_session_attach(struct mtl_main_impl* impl,
     return ret;
   }
 
+  s->calculate_time_cursor = true;
   ret = tx_ancillary_session_init_pacing(s);
   if (ret < 0) {
     err("%s(%d), init pacing fail %d\n", __func__, idx, ret);

lib/src/st2110/st_tx_fastmetadata_session.c
@@ -1495,6 +1495,7 @@ static int tx_fastmetadata_session_attach(struct mtl_main_impl* impl,
     return ret;
   }
 
+  s->calculate_time_cursor = true;
   ret = tx_fastmetadata_session_init_pacing(s);
   if (ret < 0) {
     err("%s(%d), init pacing fail %d\n", __func__, idx, ret);

lib/src/st2110/st_header.h
@@ -1190,7 +1190,7 @@ struct st_rx_ancillary_session_impl {
 
   struct mt_rtcp_rx* rtcp_rx[MTL_SESSION_PORT_MAX];
 
-  uint32_t tmstamp;
+  int64_t tmstamp;
   /* status */
   rte_atomic32_t stat_frames_received;
   int stat_pkts_dropped;
@@ -1363,7 +1363,8 @@ struct st_rx_fastmetadata_session_impl {
 
   struct mt_rtcp_rx* rtcp_rx[MTL_SESSION_PORT_MAX];
 
-  uint32_t tmstamp;
+  /* the timestamp */
+  int64_t tmstamp;
   /* status */
   rte_atomic32_t stat_frames_received;
   int stat_pkts_redundant;

lib/src/st2110/st_rx_ancillary_session.c
@@ -152,7 +152,7 @@ static int rx_ancillary_session_handle_pkt(struct mtl_main_impl* impl,
       dbg("%s(%d,%d), redundant seq now %u session last %d\n", __func__, s->idx, s_port,
           seq_id, s->session_seq_id);
     } else {
-      dbg("%s(%d,%d), redundant tmstamp now %u session last %u\n", __func__, s->idx,
+      dbg("%s(%d,%d), redundant tmstamp now %u session last %ld\n", __func__, s->idx,
           s_port, tmstamp, s->tmstamp);
     }
 

lib/src/st2110/st_rx_fastmetadata_session.c
@@ -135,7 +135,7 @@ static int rx_fastmetadata_session_handle_pkt(struct mtl_main_impl* impl,
       dbg("%s(%d,%d), redundant seq now %u session last %d\n", __func__, s->idx, s_port,
           seq_id, s->session_seq_id);
     } else {
-      dbg("%s(%d,%d), redundant tmstamp now %u session last %u\n", __func__, s->idx,
+      dbg("%s(%d,%d), redundant tmstamp now %u session last %ld\n", __func__, s->idx,
           s_port, tmstamp, s->tmstamp);
     }
 

commit f2da9ce2c58893adae8541e378aa9ad453d9fdc7

Fix: First timestamp initialized to 0 in RTP mode
In RTP mode, our API incorrectly initializes the first
packet timestamp to 0 for ST40, ST41, and ST30 streams.

Fixes: be5563dd70af95475446c4cefe6d55e93f1543e9

lib/src/st2110/st_tx_ancillary_session.c
@@ -189,6 +189,7 @@ static int tx_ancillary_session_init_hdr(struct mtl_main_impl* impl,
   rtp->base.ssrc = htonl(ssrc);
   s->st40_seq_id = 0;
   s->st40_ext_seq_id = 0;
+  s->st40_rtp_time = -1;
 
   info("%s(%d,%d), ip %u.%u.%u.%u port %u:%u\n", __func__, idx, s_port, dip[0], dip[1],
        dip[2], dip[3], s->st40_src_port[s_port], s->st40_dst_port[s_port]);

lib/src/st2110/st_tx_audio_session.c
@@ -188,6 +188,7 @@ static int tx_audio_session_init_hdr(struct mtl_main_impl* impl,
   rtp->ssrc = htonl(ssrc);
 
   s->st30_seq_id = 0;
+  s->st30_rtp_time = -1;
 
   info("%s(%d,%d), ip %u.%u.%u.%u port %u:%u payload_type %u\n", __func__, idx, s_port,
        dip[0], dip[1], dip[2], dip[3], s->st30_src_port[s_port], s->st30_dst_port[s_port],

lib/src/st2110/st_tx_fastmetadata_session.c
@@ -188,6 +188,7 @@ static int tx_fastmetadata_session_init_hdr(struct mtl_main_impl* impl,
   uint32_t ssrc = ops->ssrc ? ops->ssrc : s->idx + 0x323450;
   rtp->base.ssrc = htonl(ssrc);
   s->st41_seq_id = 0;
+  s->st41_rtp_time = -1;
 
   info("%s(%d,%d), ip %u.%u.%u.%u port %u:%u\n", __func__, idx, s_port, dip[0], dip[1],
        dip[2], dip[3], s->st41_src_port[s_port], s->st41_dst_port[s_port]);