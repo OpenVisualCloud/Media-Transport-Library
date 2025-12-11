dbf1d3e51d0f60e2b4a67ec9c4cf01b88c34fb14
Wesierski, Dawid <dawid.wesierski@intel.com>
2025-12-11 07:57:42 -0500

    Ci: nightly tests not working

--- script/build_ebpf_xdp.sh
+++ script/build_ebpf_xdp.sh
@@ -8,23 +8,33 @@ set -e
 script_name=$(basename "${BASH_SOURCE[0]}")
 script_path=$(readlink -qe "${BASH_SOURCE[0]}")
 script_folder=${script_path/$script_name/}
-cd "${script_folder}"
+
+archive_name="xdp-tools.tar.gz"
+repo_dir="${script_folder}/xdp-tools"
 
 (return 0 2>/dev/null) && sourced=1 || sourced=0
 
 if [ "$sourced" -eq 0 ]; then
-	cd "${script_folder}"
+	pushd "${script_folder}" >/dev/null || exit 1
 
-	if [ ! -d "xdp-tools" ]; then
+	if [ ! -d "${repo_dir}" ]; then
 		echo "Clone XDP source code"
-		git clone --recurse-submodules https://github.com/xdp-project/xdp-tools.git
+		git clone --recurse-submodules https://github.com/xdp-project/xdp-tools.git "${repo_dir}"
 	fi
 
-	cd xdp-tools
+	pushd "${repo_dir}" >/dev/null || exit 1
 	./configure
 	make
 	sudo make install
-	cd lib/libbpf/src
+	pushd lib/libbpf/src >/dev/null || exit 1
 	make
 	sudo make install
+	popd >/dev/null
+	popd >/dev/null
+
+	echo "Removing downloaded XDP sources"
+	rm -rf "${repo_dir}"
+	rm -f "${script_folder}/${archive_name}"
+
+	popd >/dev/null
 fi
