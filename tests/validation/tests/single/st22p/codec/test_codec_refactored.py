416c67723f904bbdc7d98ef1c9de4738ff90f39e
Wilczynski, Andrzej <andrzej.wilczynski@intel.com>
2025-12-16 18:35:06 +0000

    Addresed review comments, adjusted to the latest changes in pytests on main, cleanup
    Signed-off-by: Wilczynski, Andrzej <andrzej.wilczynski@intel.com>

--- tests/validation/tests/single/st22p/codec/test_codec_refactored.py
+++ tests/validation/tests/single/st22p/codec/test_codec_refactored.py
@@ -3,12 +3,13 @@
 import os
 
 import pytest
+from common.nicctl import InterfaceSetup
 from mtl_engine.media_files import yuv_files_422p10le
 from mtl_engine.rxtxapp import RxTxApp
+from pathlib import Path
 
 
 @pytest.mark.nightly
-@pytest.mark.refactored
 @pytest.mark.parametrize(
     "media_file",
     [yuv_files_422p10le["Penguin_1080p"]],
@@ -19,34 +20,33 @@ def test_codec_refactored(
     hosts,
     build,
     media,
-    nic_port_list,
+    setup_interfaces: InterfaceSetup,
     test_time,
     codec,
     test_config,
     prepare_ramdisk,
+    pcap_capture,
     media_file,
 ):
-    media_file_info = media_file
+    media_file_info, media_file_path = media_file
     host = list(hosts.values())[0]
+    interfaces_list = setup_interfaces.get_interfaces_list_single(
+        test_config.get("interface_type", "VF")
+    )
 
     app = RxTxApp(app_path="./tests/tools/RxTxApp/build")
-    input_path = (
-        os.path.join(media, media_file_info["filename"])
-        if media
-        else media_file_info["filename"]
-    )
     app.create_command(
         session_type="st22p",
         test_mode="multicast",
-        nic_port_list=host.vfs,
+        nic_port_list=interfaces_list,
         width=media_file_info["width"],
         height=media_file_info["height"],
         framerate=f"p{media_file_info['fps']}",
         codec=codec,
         quality="speed",
         pixel_format=media_file_info["file_format"],
-        input_file=input_path,
+        input_file=media_file_path,
         codec_threads=2,
         test_time=test_time,
     )
-    app.execute_test(build=build, test_time=test_time, host=host)
+    app.execute_test(build=build, test_time=test_time, host=host, netsniff=pcap_capture)
