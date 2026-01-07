13a816261e649ce0ac9cd5349631fa42709955ce
Pawel Staszczuk <41273017+staszczuk@users.noreply.github.com>
2026-01-07 17:17:28 +0000

    Test: Remove unsupported test cases

--- tests/validation/tests/xfail.py
+++ tests/validation/tests/xfail.py
@@ -12,14 +12,6 @@ def add_issue(issue: str, request):
     request.node.add_marker(pytest.mark.xfail)
 
 
-def SDBQ1001_audio_channel_check(audio_channel: str, audio_format: str, request):
-    if audio_channel == "222" and (audio_format == "PCM16" or audio_format == "PCM24"):
-        add_issue(
-            "XFAIL: SDBQ-1001 - Audio/St30p, PCM-16 with 222 audio channel tx_audio_session_attach error",
-            request,
-        )
-
-
 def SDBQ1002_pg_format_error_check(video_format: str, pg_format: str, request):
     if video_format == "i720p50" and pg_format == "V210":
         add_issue(
