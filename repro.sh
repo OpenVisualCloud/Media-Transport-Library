2480c5a7d28ff4f7707ae16d2fea636afd4c49f3
Dawid Wesierski <dawid.wesierski@intel.com>
2026-01-27 11:51:11 +0100

    QUEUE_FAIL

---
+++ repro.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+
+# Check if script is being run with ./repro.sh
+if [[ "$0" != "./repro.sh" ]]; then
+    echo "Error: This script must be run from the correct directory"
+    exit 1
+fi
+
+
+export INPUT_CONFIG_FILE=./tests/tools/RxTxApp/build/input.json
+
+
+./script/build_ice_driver.sh
+./script/build_dpdk.sh
+./build.sh
+
+# Check if ramdisk mount exists, create if it doesn't
+if ! mountpoint -q /mnt/ramdisk; then
+    mkdir -p /mnt/ramdisk
+    mount -t tmpfs -o size=20G tmpfs /mnt/ramdisk
+fi
+
+
+FILE=/mnt/ramdisk/input.v210
+if [ -f "$FILE" ]; then
+    echo "$FILE exists."
+else 
+    echo "$FILE does not exist. Creating..."
+    dd if=/dev/zero of=$FILE bs=1M count=1977 seek=0 2>/dev/null
+fi
+
+
+
+dpdk-devbind.py -u 0000:4b:00.0
+./script/nicctl.sh create_vf 0000:4b:00.0
+sleep 4
+
+
+./tests/tools/RxTxApp/build/RxTxApp --config_file $INPUT_CONFIG_FILE 
