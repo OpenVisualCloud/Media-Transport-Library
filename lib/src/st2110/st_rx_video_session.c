OpenVisualCloud/Media-Transport-Library

commit c17883b4b68b78a0f7ac2346798d2c33b6444296

Fix: slots allocation mechanism for redundant streams
Fix slots allocation falling into wrong state when redundant streams are old.
This caused primary streams to set 2 slots while redundant streams allocated
new slots, resulting in all frames being incomplete.

Issue occurs when the latency of redundnat stream is greater than
slot number * frame time.

This would not adress an issue where 2 streams with slightly diffrent
timestamps would be sent to rx but that case is not worth fixing
as this would be incompliat with st2110

To test:
./build/tests/KahawaiTest \
    --auto_start_stop \
    --port_list=0000:xx:xx.x,0000:xx:xx.x,0000:xx:xx.x,0000:xx:xx.x \
    --gtest_filter=noctx.st20p_redundant_latency

lib/src/st2110/st_rx_video_session.c
@@ -619,7 +619,7 @@ void rv_slot_dump(struct st_rx_video_session_impl* s) {
 
   for (int i = 0; i < ST_VIDEO_RX_REC_NUM_OFO; i++) {
     slot = &s->slots[i];
-    info("%s(%d), tmstamp %u recv_size %" PRIu64 " pkts_received %u\n", __func__, i,
+    info("%s(%d), tmstamp %ld recv_size %" PRIu64 " pkts_received %u\n", __func__, i,
          slot->tmstamp, rv_slot_get_frame_size(slot), slot->pkts_received);
   }
 }
@@ -671,7 +671,7 @@ static int rv_init_slot(struct st_rx_video_session_impl* s) {
     slot->frame = NULL;
     rv_slot_init_frame_size(slot);
     slot->pkts_received = 0;
-    slot->tmstamp = 0;
+    slot->tmstamp = -1;
     slot->seq_id_got = false;
     frame_bitmap = mt_rte_zmalloc_socket(bitmap_size, soc_id);
     if (!frame_bitmap) {
@@ -889,7 +889,7 @@ static void rv_frame_notify(struct st_rx_video_session_impl* s,
     }
 
     /* notify frame */
-    dbg("%s(%d): tmstamp %u\n", __func__, s->idx, slot->tmstamp);
+    dbg("%s(%d): tmstamp %ld\n", __func__, s->idx, slot->tmstamp);
     int ret = rv_notify_frame_ready(s, frame->addr, meta);
     if (ret < 0) {
       err("%s(%d), notify_frame_ready fail %d\n", __func__, s->idx, ret);
@@ -901,7 +901,7 @@ static void rv_frame_notify(struct st_rx_video_session_impl* s,
     double reactive = 1080.0 / 1125.0;
     s->trs = s->frame_time * reactive / meta->pkts_total;
   } else {
-    dbg("%s(%d): frame_recv_size %" PRIu64 ", frame_total_size %" PRIu64 ", tmstamp %u\n",
+    dbg("%s(%d): frame_recv_size %" PRIu64 ", frame_total_size %" PRIu64 ", tmstamp %ld\n",
         __func__, s->idx, meta->frame_recv_size, meta->frame_total_size, slot->tmstamp);
     MT_USDT_ST20_RX_FRAME_INCOMPLETE(s->parent->idx, s->idx, frame->idx, slot->tmstamp,
                                      meta->frame_recv_size, s->st20_frame_size);
@@ -1109,7 +1109,26 @@ static struct st_rx_video_slot_impl* rv_slot_by_tmstamp(
     }
   }
 
+
+  /* if the slot timestamp is in the past just drop it */
+  bool timestamp_is_in_the_past = true;
+  for (i = 0; i < s->slot_max; i++) {
+    slot = &s->slots[i];
+
+    if (slot->tmstamp == -1 || mt_seq32_greater(tmstamp, slot->tmstamp)) {
+      timestamp_is_in_the_past = false;
+      break;
+    }
+  }
+
+  if (timestamp_is_in_the_past) {
+    dbg("%s(%d): tmstamp %u is in the past, drop it\n", __func__, s->idx, tmstamp);
+    return NULL;
+  }
+
   dbg("%s(%d): new tmstamp %u\n", __func__, s->idx, tmstamp);
+
+
   if (s->dma_dev && !mt_dma_empty(s->dma_dev)) {
     /* still in progress of previous frame, drop current pkt */
     rte_atomic32_inc(&s->dma_previous_busy_cnt);
@@ -1735,7 +1754,7 @@ static int rv_handle_frame_pkt(struct st_rx_video_session_impl* s, struct rte_mb
   if (end_frame) {
     dbg("%s(%d,%d): full frame on %p(%" PRIu64 ")\n", __func__, s->idx, s_port,
         slot->frame->addr, frame_recv_size);
-    dbg("%s(%d,%d): tmstamp %u slot %d\n", __func__, s->idx, s_port, slot->tmstamp,
+    dbg("%s(%d,%d): tmstamp %ld slot %d\n", __func__, s->idx, s_port, slot->tmstamp,
         slot->idx);
     /* end of frame */
     rv_slot_full_frame(s, slot);
@@ -2254,7 +2273,7 @@ static int rv_handle_hdr_split_pkt(struct st_rx_video_session_impl* s,
   if (frame_recv_size >= s->st20_frame_size) {
     dbg("%s(%d,%d): full frame on %p(%zu)\n", __func__, s->idx, s_port, slot->frame->addr,
         frame_recv_size);
-    dbg("%s(%d,%d): tmstamp %u slot %d\n", __func__, s->idx, s_port, slot->tmstamp,
+    dbg("%s(%d,%d): tmstamp %ld slot %d\n", __func__, s->idx, s_port, slot->tmstamp,
         slot->idx);
     rv_slot_full_frame(s, slot);
   }

lib/src/st2110/st_header.h
@@ -449,7 +449,7 @@ struct st_rx_video_slot_slice_info {
 
 struct st_rx_video_slot_impl {
   int idx;
-  uint32_t tmstamp;
+  int64_t tmstamp;
   uint16_t seq_id_base;     /* seq id for the first packet */
   uint32_t seq_id_base_u32; /* seq id for the first packet with u32 */
   bool seq_id_got;

commit 0864d0ecfe8e46c4edf3e2f31d37099708747a84

Fix: video sessions using one slot for redundant
Fix the redundancy in video sessions, that were
only utilizing one slot for redundant streams.
Wchich causesd that the mechanism that handles
dropping the redundant packets was not working
as intended.

Fixes: c17883b4b68b78a0f7ac2346798d2c33b6444296

lib/src/st2110/st_rx_video_session.c
@@ -690,10 +690,7 @@ static int rv_init_slot(struct st_rx_video_session_impl* s) {
     }
   }
   s->slot_idx = -1;
-  if (s->ops.flags & ST20_RX_FLAG_ENABLE_RTCP)
-    s->slot_max = 2; /* use 2 slots for rtcp */
-  else
-    s->slot_max = 1; /* default only one slot */
+  s->slot_max = ST_VIDEO_RX_REC_NUM_OFO;
 
   dbg("%s(%d), succ\n", __func__, idx);
   return 0;