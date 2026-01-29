OpenVisualCloud/Media-Transport-Library

commit dc7a4778c2d1929b5dbbc6151da6acad158d8023

Fix: User pacing test
- Split noctx code into more files
- Simplify testcases by adding HandlerBundle
- Use pacing info to better check receive time

Signed-off-by: Kasiewicz, Marek <marek.kasiewicz@intel.com>

lib/src/st2110/st_video_transmitter.c
@@ -17,7 +17,7 @@
  * This adds a superficial difference between the RTP timestamp and the transmission
  * time, which makes it look as if the packets have a slight latency immediately after
  * entering the wire. This prevents negative latency values. */
-#define LATENCY_COMPENSATION 1
+#define LATENCY_COMPENSATION 2
 
 static int video_trs_tasklet_start(void* priv) {
   struct st_video_transmitter_impl* trs = priv;

.gitignore
@@ -70,7 +70,6 @@ tests/validation/logs
 html/
 test.*
 build/
-core*
 out*
 assets/
 *.pkg

include/st20_api.h
@@ -480,6 +480,8 @@ struct st20_rx_tp_meta {
   enum st_rx_tp_compliant compliant;
   /** the failed cause if compliant is not ST_RX_TP_COMPLIANT_NARROW */
   char failed_cause[64];
+  /** TAI timestamp measured right after first packet of the frame was received */
+  uint64_t receive_timestamp;
 
   /* packets count in current report meta */
   uint32_t pkts_cnt;
@@ -1875,6 +1877,24 @@ int st20_tx_put_mbuf(st20_tx_handle handle, void* mbuf, uint16_t len);
  *   - <0: Error code.
  */
 int st20_tx_get_sch_idx(st20_tx_handle handle);
+
+/**
+ * Retrieve pacing parameters for a tx st2110-20 session.
+ *
+ * @param handle
+ *   The handle to the tx st2110-20(video) session.
+ * @param tr_offset_ns
+ *   Optional output for the tr offset value in nanoseconds.
+ * @param trs_ns
+ *   Optional output for the packet spacing (TRS) value in nanoseconds.
+ * @param vrx_pkts
+ *   Optional output for the VRX packet count.
+ *
+ * @return
+ *    0 on success, negative value otherwise.
+ */
+int st20_tx_get_pacing_params(st20_tx_handle handle, double* tr_offset_ns, double* trs_ns,
+                              uint32_t* vrx_pkts);
 
 /**
  * Retrieve the general statistics(I/O) for one tx st2110-20(video) session.

include/st_pipeline_api.h
@@ -319,6 +319,8 @@ struct st_frame {
   /** timing parser meta for st20p_rx_get_frame, only active if
    * ST20P_RX_FLAG_TIMING_PARSER_META */
   struct st20_rx_tp_meta* tp[MTL_SESSION_PORT_MAX];
+  /** TAI timestamp measured right after first packet of the frame was received */
+  uint64_t receive_timestamp;
 };
 
 /** Device type of st plugin */
@@ -1761,6 +1763,23 @@ size_t st20p_tx_frame_size(st20p_tx_handle handle);
  */
 int st20p_tx_get_sch_idx(st20p_tx_handle handle);
 
+/**
+ * Retrieve pacing parameters for a tx st2110-20(pipeline) session.
+ *
+ * @param handle
+ *   The handle to the tx st2110-20(video) session.
+ * @param tr_offset_ns
+ *   Optional output for the tr offset value in nanoseconds.
+ * @param trs_ns
+ *   Optional output for the packet spacing (TRS) value in nanoseconds.
+ * @param vrx_pkts
+ *   Optional output for the VRX packet count.
+ *
+ * @return
+ *    0 on success, negative value otherwise.
+ */
+int st20p_tx_get_pacing_params(st20p_tx_handle handle, double* tr_offset_ns,
+                               double* trs_ns, uint32_t* vrx_pkts);
 /**
  * Retrieve the general statistics(I/O) for one tx st2110-20(pipeline) session.
  *

lib/src/st2110/pipeline/st20_pipeline_rx.c
@@ -218,6 +218,8 @@ static int rx_st20p_frame_ready(void* priv, void* frame,
   framebuff->src.timestamp = framebuff->dst.timestamp = meta->timestamp;
   framebuff->src.rtp_timestamp = framebuff->dst.rtp_timestamp = meta->rtp_timestamp;
   framebuff->src.status = framebuff->dst.status = meta->status;
+  framebuff->src.receive_timestamp = framebuff->dst.receive_timestamp =
+      meta->timestamp_first_pkt;
 
   framebuff->src.pkts_total = framebuff->dst.pkts_total = meta->pkts_total;
   for (enum mtl_session_port s_port = 0; s_port < MTL_SESSION_PORT_MAX; s_port++) {

lib/src/st2110/pipeline/st20_pipeline_tx.c
@@ -984,6 +984,26 @@ int st20p_tx_get_sch_idx(st20p_tx_handle handle) {
   return st20_tx_get_sch_idx(ctx->transport);
 }
 
+int st20p_tx_get_pacing_params(st20p_tx_handle handle, double* tr_offset_ns,
+                               double* trs_ns, uint32_t* vrx_pkts) {
+  struct st20p_tx_ctx* ctx = handle;
+  int cidx;
+
+  if (!handle) {
+    err("%s, invalid handle %p\n", __func__, handle);
+    return -EINVAL;
+  }
+  ctx = handle;
+  cidx = ctx->idx;
+
+  if (ctx->type != MT_ST20_HANDLE_PIPELINE_TX) {
+    err("%s(%d), invalid type %d\n", __func__, cidx, ctx->type);
+    return -EINVAL;
+  }
+
+  return st20_tx_get_pacing_params(ctx->transport, tr_offset_ns, trs_ns, vrx_pkts);
+}
+
 int st20p_tx_get_session_stats(st20p_tx_handle handle, struct st20_tx_user_stats* stats) {
   struct st20p_tx_ctx* ctx = handle;
   int cidx;

lib/src/st2110/st_tx_video_session.c
@@ -4314,6 +4314,27 @@ int st20_tx_get_sch_idx(st20_tx_handle handle) {
   return s_impl->sch->idx;
 }
 
+int st20_tx_get_pacing_params(st20_tx_handle handle, double* tr_offset_ns, double* trs_ns,
+                              uint32_t* vrx_pkts) {
+  struct st_tx_video_session_handle_impl* s_impl = handle;
+
+  if (!handle) {
+    err("%s, invalid handle %p\n", __func__, handle);
+    return -EINVAL;
+  }
+
+  if (s_impl->type != MT_HANDLE_TX_VIDEO) {
+    err("%s, invalid type %d\n", __func__, s_impl->type);
+    return -EINVAL;
+  }
+
+  struct st_tx_video_session_impl* s = s_impl->impl;
+  if (tr_offset_ns) *tr_offset_ns = s->pacing.tr_offset;
+  if (trs_ns) *trs_ns = s->pacing.trs;
+  if (vrx_pkts) *vrx_pkts = s->pacing.vrx;
+  return 0;
+}
+
 int st20_tx_get_session_stats(st20_tx_handle handle, struct st20_tx_user_stats* stats) {
   struct st_tx_video_session_handle_impl* s_impl = handle;
 

tests/integration_tests/meson.build
@@ -8,6 +8,7 @@ sources = files('tests.cpp', 'st_test.cpp', 'st20_test.cpp', 'st22_test.cpp',
 
 subdir('noctx')
 sources += noctx_sources
+test_inc = include_directories('.', 'noctx')
 
 ufd_sources = files('ufd_test.cpp', 'ufd_loop_test.cpp', 'test_util.cpp')
 

commit 956f28403c047571c63637c5d9581f8a0c7e130e

Fix: video_trs_rl_warm_up, recalc warm-up packets (#1414)
video_trs_rl_warm_up function could add too many packets to
trs_pad_inflight_num, which caused unrecoverable queue error.

bug introduced in: dc7a4778

Signed-off-by: Kasiewicz, Marek <marek.kasiewicz@intel.com>

lib/src/st2110/st_video_transmitter.c
@@ -17,7 +17,7 @@
  * This adds a superficial difference between the RTP timestamp and the transmission
  * time, which makes it look as if the packets have a slight latency immediately after
  * entering the wire. This prevents negative latency values. */
-#define LATENCY_COMPENSATION 2
+#define LATENCY_COMPENSATION 3
 
 static int video_trs_tasklet_start(void* priv) {
   struct st_video_transmitter_impl* trs = priv;
@@ -125,12 +125,9 @@ static void video_trs_rl_warm_up(struct mtl_main_impl* impl,
   pads[0] = s->pad[s_port][ST20_PKT_TYPE_NORMAL];
   for (int i = 0; i < warm_pkts + LATENCY_COMPENSATION; i++) {
     rte_mbuf_refcnt_update(pads[0], 1);
-    tx = video_trs_burst_pad(impl, s, s_port, &pads[0], 1);
-    if (tx < 1) {
-      dbg("%s(%d), warm_pkts fail at %d\n", __func__, s->idx, i);
-      s->trs_pad_inflight_num[s_port] += (warm_pkts - i);
-      return;
-    }
+    tx = video_trs_burst_pad(impl, s, s_port, &s->pad[s_port][ST20_PKT_TYPE_NORMAL], 1);
+    if (tx < 1) s->trs_pad_inflight_num[s_port]++;
+
     /* re-calculate the delta */
     cur_tsc = mt_get_tsc(impl);
     delta_pkts = (target_tsc - cur_tsc + pacing->trs - 1) / pacing->trs;