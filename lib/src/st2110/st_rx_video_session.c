OpenVisualCloud/Media-Transport-Library

commit fcde0c8b30980f3138126ab5a4423e8cc853d2c4

Fix: video sessions using one slot for redundant (#1413)
Fix the redundancy in video sessions, that were
only utilizing one slot for redundant streams.
Wchich causesd the mechanism that handles
dropping the redundant packets was not working
as intended.

Fixes: c17883b4b68b78a0f7ac2346798d2c33b6444296

lib/src/st2110/st_rx_video_session.c
@@ -690,10 +690,7 @@ static int rv_init_slot(struct st_rx_video_session_impl* s) {
     }
   }
   s->slot_idx = -1;
-  if (s->ops.flags & ST20_RX_FLAG_ENABLE_RTCP)
-    s->slot_max = 2; /* use 2 slots for rtcp */
-  else
-    s->slot_max = 1; /* default only one slot */
+  s->slot_max = ST_VIDEO_RX_REC_NUM_OFO;
 
   dbg("%s(%d), succ\n", __func__, idx);
   return 0;

commit 57d26c2a989da13f03093d66fae15917f4cf54e1

Fix: slice not working with bigger than 1 slot
Fix the slice feature not working due to increased
number of slots for out-of-order packet recovery.

Fixes: fcde0c8b30980f3138126ab5a4423e8cc853d2c4

Co-authored-by: Marek Kasiewicz <30420796+Sakoram@users.noreply.github.com>

lib/src/st2110/st_rx_video_session.c
@@ -690,7 +690,19 @@ static int rv_init_slot(struct st_rx_video_session_impl* s) {
     }
   }
   s->slot_idx = -1;
-  s->slot_max = ST_VIDEO_RX_REC_NUM_OFO;
+
+  if (s->ops.flags & ST20_RX_FLAG_ENABLE_RTCP)
+    s->slot_max = ST_RX_VIDEO_RTCP_SLOT_NUM;
+  else if (s->ops.num_port > 1)
+    s->slot_max = ST_RX_VIDEO_REDUNDANT_SLOT_NUM;
+  else
+    s->slot_max = 1;
+
+  if (s->slot_max > ST_VIDEO_RX_REC_NUM_OFO) {
+    err("%s(%d), slot_max %d exceed max %d\n", __func__, idx, s->slot_max,
+        ST_VIDEO_RX_REC_NUM_OFO);
+    return -EINVAL;
+  }
 
   dbg("%s(%d), succ\n", __func__, idx);
   return 0;

lib/src/st2110/st_rx_video_session.h
@@ -8,6 +8,12 @@
 #include "st_main.h"
 
 #define ST_RX_VIDEO_DMA_MIN_SIZE (1024)
+/* Number of slots for out of order packet recovery for RTCP retransmission
+   cannot be bigger than ST_VIDEO_RX_REC_NUM_OFO*/
+#define ST_RX_VIDEO_RTCP_SLOT_NUM 2
+
+/* Number of slots for redundant support */
+#define ST_RX_VIDEO_REDUNDANT_SLOT_NUM 2
 
 #define ST_RV_TP_TSC_SYNC_MS (100) /* sync tsc with ptp period(ms) */
 #define ST_RV_TP_TSC_SYNC_NS (ST_RV_TP_TSC_SYNC_MS * 1000 * 1000)