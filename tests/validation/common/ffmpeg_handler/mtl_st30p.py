ba0cbdfe49b10ebe22978ef2cdf502473020786c
Kasiewicz, Marek <marek.kasiewicz@intel.com>
2026-02-05 12:38:23 +0000

    Test: Remove val framework for realease v26.01
    Validation framework and tests are not a part of the realease

--- tests/validation/common/ffmpeg_handler/mtl_st30p.py
+++
@@ -1,3 +0,0 @@
-# SPDX-License-Identifier: BSD-3-Clause
-# Copyright 2025 Intel Corporation
-# Media Communications Mesh
--- tests/validation/mtl_engine/const.py
+++
@@ -1,6 +0,0 @@
-# SPDX-License-Identifier: BSD-3-Clause
-# Copyright(c) 2024-2025 Intel Corporation
-
-LOG_FOLDER = "logs"
-TESTCMD_LVL = 24  # Custom logging level for test commands
-FRAMES_CAPTURE = 4  # Number of frames to capture for compliance tests
