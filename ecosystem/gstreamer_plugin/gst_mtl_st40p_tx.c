OpenVisualCloud/Media-Transport-Library

commit 9e23a22254e2ec1a11119d92d918929962230f61

Add: GStreamer testing for ancillary (#1251)
We process the meta_num incorrectly in
st40_pipeline_tx.c, leading to potential issues
with frame metadata handling.
Fix it by overwriting the anc_frame meta_num and
treating the framebuff from pipeline anc size
as the only source of truth.

Fix the improper handling of the CAPS negotiation
signal, for now, just ignore consecutive signals

TODO: Checking and reinitialization of the session.

Add the ability for the ST40 GStreamer plugin to
parse multiple ancillary packages, including
support for different payload types and
dynamic reconfiguration.

Add the ability for st20 video plugin to work
without metadata with an assumed bit configuration.

Add the st40p meta insertion into the metadata
element

Test the changes by adding the ability for the
pytest framework to generate data from both the
Rx simplified rfc8331 and the Tx rfc8331
custom formats (ancgenerator)
For more advanced anc frames testing.

Add the ability for the testing framework in
GStreamer to run user pacing scenarios
TODO: Add tests for user pacing
TODO: Add lipsync for GStreamer
TODO: Add redundant scenarios

Fix the way we handle killing and signal
handling in the GStreamer engine.

Fix unlocked the st40 pipeline abilility to use anc_count == 0

To test: the pr the GStreamer tools and plugins need
to be compiled
1. Build tests/tools/gstreamer_tools
$ meson setup builddir
$ ninja -C build

Fixes: dd611004d2fdce5f149c820dd350c2f77222d1e4

---------

Co-authored-by: Magda-Pytel <magdalena.pytel@intel.com>
Co-authored-by: Marek Kasiewicz <marokastg@gmail.com>

ecosystem/gstreamer_plugin/gst_mtl_st40p_tx.c
@@ -200,12 +200,13 @@ static void gst_mtl_st40p_tx_class_init(Gst_Mtl_St40p_TxClass* klass) {
 
   g_object_class_install_property(
       gobject_class, PROP_ST40P_TX_USE_PTS_FOR_PACING,
-      g_param_spec_boolean(
-          "use-pts-for-pacing", "Use PTS for packet pacing",
-          "This property modifies the default behavior where MTL handles packet pacing. "
-          "Instead, it uses the buffer's PTS (Presentation Timestamp) to determine the "
-          "precise time for sending packets.",
-          FALSE, G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS));
+      g_param_spec_boolean("use-pts-for-pacing", "Use PTS for packet pacing",
+                           "This property modifies the default behavior where "
+                           "MTL handles packet pacing. "
+                           "Instead, it uses the buffer's PTS (Presentation "
+                           "Timestamp) to determine the "
+                           "precise time for sending packets.",
+                           FALSE, G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS));
 
   g_object_class_install_property(
       gobject_class, PROP_ST40P_TX_PTS_PACING_OFFSET,
@@ -409,8 +410,8 @@ static gboolean gst_mtl_st40p_tx_session_create(Gst_Mtl_St40p_Tx* sink) {
   }
 
   ops_tx.interlaced = false;
-  /* Only single ANC data packet is possible when metadata is not being parsed from
-   * parse_rfc8331_input mode per frame. anc_count = 1 TODO: allow more */
+  /* Only single ANC data packet is possible when metadata is not being parsed
+   * from parse_rfc8331_input mode per frame. anc_count = 1 TODO: allow more */
   sink->frame_size = MAX_UDW_SIZE;
 
   if (sink->max_combined_udw_size)
@@ -469,13 +470,14 @@ static gboolean gst_mtl_st40p_tx_sink_event(GstPad* pad, GstObject* parent,
 /**
  * @brief Fills the metadata for a st40 frame.
  *
- * This function initializes the metadata fields of a given st40 frame with the provided
- * data and metadata values. It sets the first metadata entry with the specified DID,
- * SDID, and data size, and assigns the data pointer to the frame.
- * Note: Most of the fields are hardcoded, and only first metadata block is filled as
- * only one ANC Data packet per frame is allowed.
+ * This function initializes the metadata fields of a given st40 frame with the
+ * provided data and metadata values. It sets the first metadata entry with the
+ * specified DID, SDID, and data size, and assigns the data pointer to the
+ * frame. Note: Most of the fields are hardcoded, and only first metadata block
+ * is filled as only one ANC Data packet per frame is allowed.
  *
- * @param frame Pointer to the st40_frame structure to be filled. Cannot be NULL.
+ * @param frame Pointer to the st40_frame structure to be filled. Cannot be
+ * NULL.
  * @param data Pointer to the data to be associated with the frame.
  * @param data_size Size of the data in bytes.
  * @param did Data Identifier (DID) to be set in the metadata.
@@ -496,7 +498,8 @@ static void gst_mtl_st40p_tx_fill_meta(struct st40_frame_info* frame_info, void*
   frame_info->meta_num = 1;
 }
 
-/* we dont' really check the data here we let the st40 ancillary data to do so */
+/* we dont' really check the data here we let the st40 ancillary data to do so
+ */
 static GstFlowReturn gst_mtl_st40p_tx_parse_8331_meta(
     struct st40_frame_info* frame_info, struct st40_rfc8331_payload_hdr payload_header,
     guint anc_idx, guint udw_offset) {
@@ -546,6 +549,8 @@ static GstFlowReturn gst_mtl_st40p_tx_parse_8331_anc_words(
     return GST_FLOW_ERROR;
   }
 
+  frame_info->meta_num = 0;
+
   for (int i = 0; i < anc_count; i++) {
     /* Processing of the input 8331 header */
     if (bytes_left_to_process < sizeof(struct st40_rfc8331_payload_hdr)) {
@@ -563,18 +568,29 @@ static GstFlowReturn gst_mtl_st40p_tx_parse_8331_anc_words(
         ntohl(rfc8331_meta.headers[i]->swapped_second_hdr_chunk);
 
     payload_cursor = (uint8_t*)&rfc8331_meta.headers[i]->swapped_second_hdr_chunk;
+    /*
+     * In RFC 8331, the header struct occupies only 30 bits, not 32.
+     * The layout is:
+     *   |C|   Line_Number=9     |   Horizontal_Offset   |S| StreamNum=0 |
+     *   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+     *   |         DID       |        SDID       |  Data_Count=0x84  |
+     * We are currently skipping 2 more bits than needed here.
+     * This will be accommodated a little bit later in the parsing logic with
+     * RFC_8331_PAYLOAD_HEADER_LOST_BITS define.
+     */
     bytes_left_to_process -= sizeof(struct st40_rfc8331_payload_hdr);
     data_count = payload_header.second_hdr_chunk.data_count & 0xff;
 
-    /* data count * 10 bits + 10 bit checksum*/
-    udw_byte_size = (data_count * UDW_WORD_BIT_SIZE) + UDW_WORD_BIT_SIZE;
+    /* data count * 10 bits + 10 bit checksum - 2 lost bit from the
+     * st40_rfc8331_payload_hdr */
+    udw_byte_size = (data_count * UDW_WORD_BIT_SIZE) + UDW_WORD_BIT_SIZE -
+                    RFC_8331_PAYLOAD_HEADER_LOST_BITS;
     /* round up to the nearest byte */
     udw_byte_size = (udw_byte_size + 7) / 8;
 
     if (bytes_left_to_process < udw_byte_size) {
       GST_ERROR("Buffer size (%u) is too small for data count (%d)",
                 bytes_left_to_process, udw_byte_size);
-      return GST_FLOW_ERROR;
     }
 
     /* Use data_size as the offset for the next UDW block.
@@ -609,9 +625,9 @@ static GstFlowReturn gst_mtl_st40p_tx_parse_8331_anc_words(
     }
 
     bytes_left_to_process -= udw_byte_size;
-
     /* Get checksum and promptly ignore it */
     udw = st40_get_udw((data_count + 3), payload_cursor);
+    printf("Checksum UDW: 0x%04x\n", udw);
 
     if (sink->use_pts_for_pacing) {
       frame_info->timestamp = GST_BUFFER_PTS(buf) += sink->pts_for_pacing_offset;
@@ -679,12 +695,11 @@ static GstFlowReturn gst_mtl_st40p_tx_parse_8331_memory_block(Gst_Mtl_St40p_Tx* 
 /**
  * @brief Parses a GstBuffer and prepares a frame for transmission.
  *
- * This function retrieves a frame from the ST40p transmitter handle, copies the relevant
- * data from the provided GstBuffer into the frame buffer, fills in ancillary metadata,
- * and submits the frame for transmission. It also handles timestamping if pacing is
- * enabled.
- * No memory management is done here, the caller is responsible for managing the
- * GstBuffer and its associated memory.
+ * This function retrieves a frame from the ST40p transmitter handle, copies the
+ * relevant data from the provided GstBuffer into the frame buffer, fills in
+ * ancillary metadata, and submits the frame for transmission. It also handles
+ * timestamping if pacing is enabled. No memory management is done here, the
+ * caller is responsible for managing the GstBuffer and its associated memory.
  *
  * @param sink Pointer to the Gst_Mtl_St40p_Tx sink element.
  * @param gst_buffer_memory GstMemory associated with the GstBuffer.

ecosystem/gstreamer_plugin/gst_mtl_common.c
@@ -407,15 +407,16 @@ void gst_mtl_common_get_general_arguments(GObject* object, guint prop_id,
 }
 
 /**
- * Copies general initialization port values to session-specific port arguments if not
- * specified. If the primary port is not specified, the redundant port argument will be
- * copied from the general initialization ports regardless of its specification. If the
- * UDP port for the redundant port is not specified, it will be incremented by one over
- * the primary port.
+ * Copies general initialization port values to session-specific port arguments
+ * if not specified. If the primary port is not specified, the redundant port
+ * argument will be copied from the general initialization ports regardless of
+ * its specification. If the UDP port for the redundant port is not specified,
+ * it will be incremented by one over the primary port.
  *
- * @param general_args Pointer to the structure containing general initialization
- * parameters.
- * @param port_args Pointer to the structure containing session-specific port arguments.
+ * @param general_args Pointer to the structure containing general
+ * initialization parameters.
+ * @param port_args Pointer to the structure containing session-specific port
+ * arguments.
  */
 void gst_mtl_common_copy_general_to_session_args(GeneralArgs* general_args,
                                                  SessionPortArgs* port_args) {
@@ -437,8 +438,9 @@ void gst_mtl_common_copy_general_to_session_args(GeneralArgs* general_args,
 }
 
 /**
- * Parses the transmission port arguments and initializes the tranmission port structure.
- * Validates and sets the destination IP address, port number, UDP port, and payload type.
+ * Parses the transmission port arguments and initializes the tranmission port
+ * structure. Validates and sets the destination IP address, port number, UDP
+ * port, and payload type.
  *
  * @param port Pointer to the transmission port structure to be initialized.
  * @param port_args Pointer to the structure containing the port arguments.
@@ -485,8 +487,9 @@ guint gst_mtl_common_parse_tx_port_arguments(struct st_tx_port* port,
 }
 
 /**
- * Parses the transmission port arguments and initializes the receive port structure.
- * Validates and sets the destination IP address, port number, UDP port, and payload type.
+ * Parses the transmission port arguments and initializes the receive port
+ * structure. Validates and sets the destination IP address, port number, UDP
+ * port, and payload type.
  *
  * @param port Pointer to the transmission port structure to be initialized.
  * @param port_args Pointer to the structure containing the port arguments.

ecosystem/gstreamer_plugin/gst_mtl_st20p_rx.c
@@ -149,10 +149,10 @@ static void gst_mtl_st20p_rx_class_init(Gst_Mtl_St20p_RxClass* klass) {
   gstelement_class = GST_ELEMENT_CLASS(klass);
   gstbasesrc_class = GST_BASE_SRC_CLASS(klass);
 
-  gst_element_class_set_metadata(
-      gstelement_class, "MtlRxSt20Src", "Src/Video",
-      "MTL transmission plugin for SMPTE ST 2110-20 standard (uncompressed video)",
-      "Dawid Wesierski <dawid.wesierski@intel.com>");
+  gst_element_class_set_metadata(gstelement_class, "MtlRxSt20Src", "Src/Video",
+                                 "MTL transmission plugin for SMPTE ST 2110-20 "
+                                 "standard (uncompressed video)",
+                                 "Dawid Wesierski <dawid.wesierski@intel.com>");
 
   gst_element_class_add_static_pad_template(gstelement_class,
                                             &gst_mtl_st20p_rx_src_pad_template);

ecosystem/gstreamer_plugin/gst_mtl_st20p_tx.c
@@ -163,10 +163,10 @@ static void gst_mtl_st20p_tx_class_init(Gst_Mtl_St20p_TxClass* klass) {
   gstelement_class = GST_ELEMENT_CLASS(klass);
   gstvideosinkelement_class = GST_VIDEO_SINK_CLASS(klass);
 
-  gst_element_class_set_metadata(
-      gstelement_class, "MtlTxSt20Sink", "Sink/Video",
-      "MTL transmission plugin for SMPTE ST 2110-20 standard (uncompressed video)",
-      "Dawid Wesierski <dawid.wesierski@intel.com>");
+  gst_element_class_set_metadata(gstelement_class, "MtlTxSt20Sink", "Sink/Video",
+                                 "MTL transmission plugin for SMPTE ST 2110-20 "
+                                 "standard (uncompressed video)",
+                                 "Dawid Wesierski <dawid.wesierski@intel.com>");
 
   gst_element_class_add_static_pad_template(gstelement_class,
                                             &gst_mtl_st20p_tx_sink_pad_template);
@@ -199,12 +199,13 @@ static void gst_mtl_st20p_tx_class_init(Gst_Mtl_St20p_TxClass* klass) {
 
   g_object_class_install_property(
       gobject_class, PROP_ST20P_TX_USE_PTS_FOR_PACING,
-      g_param_spec_boolean(
-          "use-pts-for-pacing", "Use PTS for packet pacing",
-          "This property modifies the default behavior where MTL handles packet pacing. "
-          "Instead, it uses the buffer's PTS (Presentation Timestamp) to determine the "
-          "precise time for sending packets.",
-          FALSE, G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS));
+      g_param_spec_boolean("use-pts-for-pacing", "Use PTS for packet pacing",
+                           "This property modifies the default behavior where "
+                           "MTL handles packet pacing. "
+                           "Instead, it uses the buffer's PTS (Presentation "
+                           "Timestamp) to determine the "
+                           "precise time for sending packets.",
+                           FALSE, G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS));
 
   g_object_class_install_property(
       gobject_class, PROP_ST20P_TX_PTS_PACING_OFFSET,
@@ -446,6 +447,11 @@ static gboolean gst_mtl_st20p_tx_sink_event(GstPad* pad, GstObject* parent,
 
   switch (GST_EVENT_TYPE(event)) {
     case GST_EVENT_CAPS:
+      if (sink->session_capabilites_set) {
+        GST_WARNING("Capabilities already set, ignoring");
+        return gst_pad_event_default(pad, parent, event);
+      }
+
       gst_event_parse_caps(event, &caps);
       if (sink->async_session_create) {
         thread_data = malloc(sizeof(GstMtlSt20pTxThreadData));
@@ -460,6 +466,7 @@ static gboolean gst_mtl_st20p_tx_sink_event(GstPad* pad, GstObject* parent,
           return FALSE;
         }
       }
+      sink->session_capabilites_set = TRUE;
       ret = gst_pad_event_default(pad, parent, event);
       break;
     case GST_EVENT_EOS:
@@ -562,12 +569,8 @@ static GstFlowReturn gst_mtl_st20p_tx_zero_copy(Gst_Mtl_St20p_Tx* sink, GstBuffe
   GstSt20pTxExternalDataParent* parent;
   struct st_frame* frame;
   struct st_ext_frame ext_frame;
-  GstVideoMeta* video_meta = gst_buffer_get_video_meta(buf);
+  GstVideoMeta* video_meta = NULL;
   gint buffer_n = gst_buffer_n_memory(buf);
-  if (!video_meta) {
-    g_print("Failed to get video meta from buffer\n");
-    return GST_FLOW_ERROR;
-  }
 
   parent = malloc(sizeof(GstSt20pTxExternalDataParent));
   if (!parent) {
@@ -615,11 +618,30 @@ static GstFlowReturn gst_mtl_st20p_tx_zero_copy(Gst_Mtl_St20p_Tx* sink, GstBuffe
       frame->tfmt = ST10_TIMESTAMP_FMT_TAI;
     }
 
-    for (int i = 0; i < video_meta->n_planes; i++) {
-      ext_frame.addr[i] = child->map_info.data + video_meta->offset[i];
-      ext_frame.linesize[i] = video_meta->stride[i];
-      ext_frame.iova[i] = 0;
+    video_meta = gst_buffer_get_video_meta(buf);
+    if (video_meta) {
+      for (int i = 0; i < video_meta->n_planes; i++) {
+        ext_frame.addr[i] = child->map_info.data + video_meta->offset[i];
+        ext_frame.linesize[i] = video_meta->stride[i];
+        ext_frame.iova[i] = 0;
+      }
+
+    } else {
+      ext_frame.addr[0] = child->map_info.data;
+      ext_frame.iova[0] = 0;
+      ext_frame.linesize[0] = st_frame_least_linesize(frame->fmt, frame->width, 0);
+      guint8 planes = st_frame_fmt_planes(frame->fmt);
+
+      /* Assume video planes are stored contiguously in memory */
+      for (gint plane = 1; plane < planes; plane++) {
+        ext_frame.linesize[plane] =
+            st_frame_least_linesize(frame->fmt, frame->width, plane);
+        ext_frame.addr[plane] = (guint8*)ext_frame.addr[plane - 1] +
+                                ext_frame.linesize[plane - 1] * frame->height;
+        ext_frame.iova[plane] = 0;
+      }
     }
+
     ext_frame.size = child->map_info.size;
     ext_frame.opaque = child;
     frame->opaque = NULL;

ecosystem/gstreamer_plugin/gst_mtl_st20p_tx.h
@@ -62,6 +62,7 @@ struct _Gst_Mtl_St20p_Tx {
   gboolean zero_copy;
 
   gboolean session_ready;
+  gboolean session_capabilites_set;
   pthread_mutex_t session_mutex;
   pthread_t session_thread;
 

ecosystem/gstreamer_plugin/gst_mtl_st30p_rx.c
@@ -135,10 +135,10 @@ static void gst_mtl_st30p_rx_class_init(Gst_Mtl_St30p_RxClass* klass) {
   gstelement_class = GST_ELEMENT_CLASS(klass);
   gstbasesrc_class = GST_BASE_SRC_CLASS(klass);
 
-  gst_element_class_set_metadata(
-      gstelement_class, "MtlRxSt30Src", "Src/Audio",
-      "MTL transmission plugin for SMPTE ST 2110-30 standard (uncompressed audio)",
-      "Dawid Wesierski <dawid.wesierski@intel.com>");
+  gst_element_class_set_metadata(gstelement_class, "MtlRxSt30Src", "Src/Audio",
+                                 "MTL transmission plugin for SMPTE ST 2110-30 "
+                                 "standard (uncompressed audio)",
+                                 "Dawid Wesierski <dawid.wesierski@intel.com>");
 
   gst_element_class_add_static_pad_template(gstelement_class,
                                             &gst_mtl_st30p_rx_src_pad_template);

ecosystem/gstreamer_plugin/gst_mtl_st30p_tx.c
@@ -182,12 +182,13 @@ static void gst_mtl_st30p_tx_class_init(Gst_Mtl_St30p_TxClass* klass) {
 
   g_object_class_install_property(
       gobject_class, PROP_ST30P_TX_USE_PTS_FOR_PACING,
-      g_param_spec_boolean(
-          "use-pts-for-pacing", "Use PTS for packet pacing",
-          "This property modifies the default behavior where MTL handles packet pacing. "
-          "Instead, it uses the buffer's PTS (Presentation Timestamp) to determine the "
-          "precise time for sending packets.",
-          FALSE, G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS));
+      g_param_spec_boolean("use-pts-for-pacing", "Use PTS for packet pacing",
+                           "This property modifies the default behavior where "
+                           "MTL handles packet pacing. "
+                           "Instead, it uses the buffer's PTS (Presentation "
+                           "Timestamp) to determine the "
+                           "precise time for sending packets.",
+                           FALSE, G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS));
 
   g_object_class_install_property(
       gobject_class, PROP_ST30P_TX_PTS_PACING_OFFSET,
@@ -455,6 +456,11 @@ static gboolean gst_mtl_st30p_tx_sink_event(GstPad* pad, GstObject* parent,
 
   switch (GST_EVENT_TYPE(event)) {
     case GST_EVENT_CAPS:
+      if (sink->session_capabilites_set) {
+        GST_WARNING("Capabilities already set, ignoring");
+        return gst_pad_event_default(pad, parent, event);
+      }
+
       gst_event_parse_caps(event, &caps);
       if (sink->async_session_create) {
         thread_data = malloc(sizeof(GstMtlSt30pTxThreadData));
@@ -470,6 +476,7 @@ static gboolean gst_mtl_st30p_tx_sink_event(GstPad* pad, GstObject* parent,
         }
       }
 
+      sink->session_capabilites_set = TRUE;
       ret = gst_pad_event_default(pad, parent, event);
       break;
     case GST_EVENT_EOS:

ecosystem/gstreamer_plugin/gst_mtl_st30p_tx.h
@@ -61,11 +61,13 @@ struct _Gst_Mtl_St30p_Tx {
   guint frame_size;
 
   gboolean session_ready;
+  gboolean session_capabilites_set;
   pthread_mutex_t session_mutex;
   pthread_t session_thread;
 
   /*
-   * Handles incomplete frame buffers when their size does not match the expected size.
+   * Handles incomplete frame buffers when their size does not match the
+   * expected size.
    */
   struct st30_frame* cur_frame;
   guint cur_frame_available_size;

commit f53a386f20ed47f5f377cb006dee96397f9dc82a

Fix: Remove debug printf in gst_mtl_st40p_tx.c (#1438)
Fixes: 9e23a22254e2ec1a11119d92d918929962230f61

ecosystem/gstreamer_plugin/gst_mtl_st40p_tx.c
@@ -870,7 +870,6 @@ static GstFlowReturn gst_mtl_st40p_tx_parse_8331_anc_words(
     bytes_left_to_process -= udw_byte_size;
     /* Get checksum and promptly ignore it */
     udw = st40_get_udw((data_count + 3), payload_cursor);
-    printf("Checksum UDW: 0x%04x\n", udw);
 
     if (sink->use_pts_for_pacing) {
       frame_info->timestamp = GST_BUFFER_PTS(buf) + sink->pts_for_pacing_offset;