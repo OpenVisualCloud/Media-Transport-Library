bf7ede79423884c8049366eabfb4e540839470e9
Wesierski, Dawid <dawid.wesierski@intel.com>
2026-03-09 09:43:27 -0400

    Fix: correct audio TX epoch drop count reporting
    Use -to_epoch/pkt_time instead of epochs-next_epochs for the
    stat_epoch_drop counter and notify_frame_late callback argument
    in tx_audio_session_sync_pacing.

--- lib/src/st2110/st_tx_audio_session.c
+++ lib/src/st2110/st_tx_audio_session.c
@@ -323,10 +323,10 @@ static int tx_audio_session_sync_pacing(struct mtl_main_impl* impl,
 
   if (epochs > next_epochs) {
     ST_SESSION_STAT_ADD(s, port_user_stats.common, stat_epoch_drop,
-                        (epochs - next_epochs));
+                        -to_epoch / pkt_time);
 
     if (s->ops.notify_frame_late) {
-      s->ops.notify_frame_late(s->ops.priv, epochs - next_epochs);
+      s->ops.notify_frame_late(s->ops.priv, -to_epoch / pkt_time);
     }
   }
 
