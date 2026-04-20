d96c7e035f203341e275419999185db5760964e3
Wilczynski, Andrzej <andrzej.wilczynski@intel.com>
2026-04-20 12:32:10 +0000

    Test: add refactored ancillary tests using rxtxapp fixture
    Refactored variants of ancillary/type_mode and ancillary/multicast_with_compliance
    to the unified rxtxapp.create_command(session_type='ancillary') /
    rxtxapp.execute_test() API.
    
    Pass criterion: process rc==0 and app_rx_anc_result(N), OK line for every
    replica.
    
    Signed-off-by: Wilczynski, Andrzej <andrzej.wilczynski@intel.com>

---
+++ tests/validation/tests/single/ancillary/type_mode/test_type_mode_refactored.py
@@ -0,0 +1,57 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright(c) 2024-2025 Intel Corporation
+
+import pytest
+from common.nicctl import InterfaceSetup
+from mtl_engine.media_files import anc_files
+
+
+@pytest.mark.nightly
+@pytest.mark.parametrize(
+    "media_file",
+    [
+        anc_files["text_p29"],
+        anc_files["text_p50"],
+        anc_files["text_p59"],
+    ],
+    indirect=["media_file"],
+    ids=[
+        "text_p29",
+        "text_p50",
+        "text_p59",
+    ],
+)
+@pytest.mark.parametrize("type_mode", ["rtp", "frame"])
+def test_type_mode_refactored(
+    hosts,
+    mtl_path,
+    setup_interfaces: InterfaceSetup,
+    test_time,
+    test_config,
+    prepare_ramdisk,
+    media_file,
+    type_mode,
+    rxtxapp,
+):
+    media_file_info, media_file_path = media_file
+    host = list(hosts.values())[0]
+    interfaces_list = setup_interfaces.get_interfaces_list_single(
+        test_config.get("interface_type", "VF")
+    )
+
+    rxtxapp.create_command(
+        session_type="ancillary",
+        nic_port_list=interfaces_list,
+        test_mode="unicast",
+        type_mode=type_mode,
+        ancillary_format="closed_caption",
+        ancillary_fps=media_file_info["fps"],
+        ancillary_url=media_file_path,
+        test_time=test_time,
+    )
+
+    rxtxapp.execute_test(
+        build=mtl_path,
+        test_time=test_time,
+        host=host,
+    )
