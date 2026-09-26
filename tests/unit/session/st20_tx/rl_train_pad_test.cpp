OpenVisualCloud/Media-Transport-Library

commit a6fdd20b743bd15e7b2b2beb5d29df1d100abf99

tx/video: add build-in static pad interval for rl pacing (#338)
also add user customized by "--pad_interval <value>".

Signed-off-by: Frank Du <frank.du@intel.com>

lib/src/st2110/st_tx_video_session.c
@@ -251,9 +251,9 @@ static int tv_poll_vsync(struct mtl_main_impl* impl, struct st_tx_video_session_
   return 0;
 }
 
-static int double_cmp(const void* a, const void* b) {
-  const double* ai = a;
-  const double* bi = b;
+static int uint64_t_cmp(const void* a, const void* b) {
+  const uint64_t* ai = a;
+  const uint64_t* bi = b;
 
   if (*ai < *bi) {
     return -1;
@@ -269,15 +269,29 @@ static int tv_train_pacing(struct mtl_main_impl* impl, struct st_tx_video_sessio
   struct rte_mbuf* pad = s->pad[s_port][ST20_PKT_TYPE_NORMAL];
   int idx = s->idx;
   struct mt_txq_entry* queue = s->queue[s_port];
-  unsigned int bulk = s->bulk;
   int pad_pkts, ret;
-  int loop_cnt = 30;
-  int trim = 5;
-  double array[loop_cnt];
-  double pkts_per_sec_sum = 0;
+  int up_trim = 5;
+  int low_trim = 10;
+  int loop_frame = 60 * 2 + up_trim + low_trim; /* the frames to be trained */
+  uint64_t frame_times_ns[loop_frame];
   float pad_interval;
   uint64_t rl_bps = tv_rl_bps(s);
   uint64_t train_start_time, train_end_time;
+
+  uint16_t resolved = s->ops.pad_interval;
+  if (resolved) {
+    s->pacing.pad_interval = resolved;
+    info("%s(%d), user customized pad_interval %u\n", __func__, idx, resolved);
+    return 0;
+  }
+  if (!(s->ops.flags & ST20_TX_FLAG_DISABLE_STATIC_PAD_P)) {
+    resolved = st20_pacing_static_profiling(s);
+    if (resolved) {
+      s->pacing.pad_interval = resolved;
+      info("%s(%d), user static pad_interval %u\n", __func__, idx, resolved);
+      return 0;
+    }
+  }
 
   ret = mt_pacing_train_result_search(impl, port, rl_bps, &pad_interval);
   if (ret >= 0) {
@@ -291,46 +305,58 @@ static int tv_train_pacing(struct mtl_main_impl* impl, struct st_tx_video_sessio
 
   train_start_time = mt_get_tsc(impl);
 
-  /* warm stage to consume all nix tx buf */
-  pad_pkts = s->st20_total_pkts * 100;
+  /* warm-up stage to consume all nix tx buf */
+  pad_pkts = mt_if_nb_tx_desc(impl, port) * 1;
   for (int i = 0; i < pad_pkts; i++) {
     rte_mbuf_refcnt_update(pad, 1);
     mt_txq_burst_busy(queue, &pad, 1, 10);
   }
 
   /* training stage */
-  pad_pkts = s->st20_total_pkts * 2;
-  for (int loop = 0; loop < loop_cnt; loop++) {
+  for (int loop = 0; loop < loop_frame; loop++) {
     uint64_t start = mt_get_tsc(impl);
-    for (int i = 0; i < ST20_PKT_TYPE_MAX; i++) {
-      pad = s->pad[s_port][i];
-      int pkts = s->st20_pkt_info[i].number * 2;
+    for (int i = 0; i < s->st20_total_pkts; i++) {
+      enum st20_packet_type type;
 
-      struct rte_mbuf* bulk_pad[bulk];
-      for (int j = 0; j < bulk; j++) {
-        bulk_pad[j] = pad;
+      if ((s->ops.type == ST20_TYPE_RTP_LEVEL) ||
+          (s->s_type == MT_ST22_HANDLE_TX_VIDEO) ||
+          (s->ops.packing == ST20_PACKING_GPM_SL)) {
+        type = ST20_PKT_TYPE_NORMAL;
+      } else { /* frame type */
+        uint32_t offset = s->st20_pkt_len * i;
+        uint16_t line1_number = offset / s->st20_bytes_in_line;
+        /* last pkt should be treated as normal pkt also */
+        if ((offset + s->st20_pkt_len) < (line1_number + 1) * s->st20_bytes_in_line) {
+          type = ST20_PKT_TYPE_NORMAL;
+        } else {
+          type = ST20_PKT_TYPE_EXTRA;
+        }
       }
-      int bulk_batch = pkts / bulk;
-      for (int j = 0; j < bulk_batch; j++) {
-        rte_mbuf_refcnt_update(pad, bulk);
-        mt_txq_burst_busy(queue, bulk_pad, bulk, 10);
-      }
-      int remaining = pkts % bulk;
-      for (int j = 0; j < remaining; j++) {
-        rte_mbuf_refcnt_update(pad, 1);
-        mt_txq_burst_busy(queue, &pad, 1, 10);
-      }
+
+      pad = s->pad[s_port][type];
+      rte_mbuf_refcnt_update(pad, 1);
+      mt_txq_burst_busy(queue, &pad, 1, 10);
     }
     uint64_t end = mt_get_tsc(impl);
-    double time_sec = (double)(end - start) / NS_PER_S;
-    array[loop] = pad_pkts / time_sec;
+    frame_times_ns[loop] = end - start;
   }
 
-  qsort(array, loop_cnt, sizeof(double), double_cmp);
-  for (int i = trim; i < loop_cnt - trim; i++) {
-    pkts_per_sec_sum += array[i];
+  for (int loop = 0; loop < loop_frame; loop++) {
+    dbg("%s(%d), frame_time_ns %" PRIu64 "\n", __func__, idx, frame_times_ns[loop]);
   }
-  double pkts_per_sec = pkts_per_sec_sum / (loop_cnt - trim * 2);
+  qsort(frame_times_ns, loop_frame, sizeof(uint64_t), uint64_t_cmp);
+  for (int loop = 0; loop < loop_frame; loop++) {
+    dbg("%s(%d), sorted frame_time_ns %" PRIu64 "\n", __func__, idx,
+        frame_times_ns[loop]);
+  }
+  uint64_t frame_times_ns_sum = 0;
+  int entry_in_sum = 0;
+  for (int i = up_trim; i < loop_frame - low_trim; i++) {
+    frame_times_ns_sum += frame_times_ns[i];
+    entry_in_sum++;
+  }
+  double frame_avg_time_sec = (double)frame_times_ns_sum / entry_in_sum / NS_PER_S;
+  double pkts_per_sec = s->st20_total_pkts / frame_avg_time_sec;
 
   /* parse the pad interval */
   double pkts_per_frame = pkts_per_sec * s->fps_tm.den / s->fps_tm.mul;
@@ -457,13 +483,13 @@ static int tv_init_pacing(struct mtl_main_impl* impl,
     pacing->vrx = 0;
     pacing->warm_pkts = 0;
   }
-  if (s->ops.vrx) {
-    if (s->ops.vrx >= pkts_in_tr_offset) {
-      err("%s[%02d], use vrx %u larger than pkts in tr offset %u\n", __func__, idx,
-          s->ops.vrx, pkts_in_tr_offset);
+  if (s->ops.start_vrx) {
+    if (s->ops.start_vrx >= pkts_in_tr_offset) {
+      err("%s[%02d], use start_vrx %u larger than pkts in tr offset %u\n", __func__, idx,
+          s->ops.start_vrx, pkts_in_tr_offset);
     } else {
-      info("%s[%02d], use vrx %u from user\n", __func__, idx, s->ops.vrx);
-      pacing->vrx = s->ops.vrx;
+      info("%s[%02d], use start_vrx %u from user\n", __func__, idx, s->ops.start_vrx);
+      pacing->vrx = s->ops.start_vrx;
     }
   } else if (s->ops.pacing == ST21_PACING_WIDE) {
     uint32_t wide_vrx = pkts_in_tr_offset * 8 / 10;
@@ -2186,6 +2212,8 @@ static int tv_init_hw(struct mtl_main_impl* impl, struct st_tx_video_sessions_mg
     }
     for (int j = 0; j < ST20_PKT_TYPE_MAX; j++) {
       if (!s->st20_pkt_info[j].number) continue;
+      info("%s(%d), type %d number %u size %u\n", __func__, idx, j,
+           s->st20_pkt_info[j].number, s->st20_pkt_info[j].size);
       pad = mt_build_pad(impl, pad_mempool, port_id, RTE_ETHER_TYPE_IPV4,
                          s->st20_pkt_info[j].size);
       if (!pad) {
@@ -2509,7 +2537,7 @@ static int tv_init_pkt(struct mtl_main_impl* impl, struct st_tx_video_session_im
     s->st20_pkt_info[ST20_PKT_TYPE_NORMAL].size = s->st20_pkt_size;
     s->st20_pkt_info[ST20_PKT_TYPE_NORMAL].number =
         s->st20_total_pkts - s->st20_pkt_info[ST20_PKT_TYPE_LINE_TAIL].number;
-    info("%s(%d),  line_last_len: %d\n", __func__, idx, line_last_len);
+    dbg("%s(%d),  line_last_len: %d\n", __func__, idx, line_last_len);
   } else if (ops->packing == ST20_PACKING_BPM) {
     s->st20_pkt_len = ST_VIDEO_BPM_SIZE;
     int last_pkt_len = s->st20_frame_size % s->st20_pkt_len;
@@ -2536,7 +2564,7 @@ static int tv_init_pkt(struct mtl_main_impl* impl, struct st_tx_video_session_im
     s->st20_pkt_info[ST20_PKT_TYPE_NORMAL].number =
         s->st20_total_pkts - s->st20_pkt_info[ST20_PKT_TYPE_FRAME_TAIL].number -
         s->st20_pkt_info[ST20_PKT_TYPE_EXTRA].number;
-    info("%s(%d),  extra_pkts: %d\n", __func__, idx, extra_pkts);
+    dbg("%s(%d),  extra_pkts: %d\n", __func__, idx, extra_pkts);
   } else if (ops->packing == ST20_PACKING_GPM) {
     int max_data_len = impl->pkt_udp_suggest_max_size -
                        sizeof(struct st20_rfc4175_rtp_hdr) -
@@ -2570,7 +2598,7 @@ static int tv_init_pkt(struct mtl_main_impl* impl, struct st_tx_video_session_im
     s->st20_pkt_info[ST20_PKT_TYPE_NORMAL].number =
         s->st20_total_pkts - s->st20_pkt_info[ST20_PKT_TYPE_FRAME_TAIL].number -
         s->st20_pkt_info[ST20_PKT_TYPE_EXTRA].number;
-    info("%s(%d),  extra_pkts: %d\n", __func__, idx, extra_pkts);
+    dbg("%s(%d),  extra_pkts: %d\n", __func__, idx, extra_pkts);
   } else {
     err("%s(%d), invalid packing mode %d\n", __func__, idx, ops->packing);
     return -EIO;

app/src/app_base.h
@@ -487,7 +487,9 @@ struct st_app_context {
   struct st_app_tx_video_session* tx_video_sessions;
   int tx_video_session_cnt;
   int tx_video_rtp_ring_size; /* the ring size for tx video rtp type */
-  uint16_t tx_vrx;
+  uint16_t tx_start_vrx;
+  uint16_t tx_pad_interval;
+  bool tx_no_static_pad;
   enum st21_pacing tx_pacing_type;
 
   struct st_app_tx_audio_session* tx_audio_sessions;

app/src/args.c
@@ -46,7 +46,9 @@ enum st_args_cmd {
   ST22_ARG_RX_SESSIONS_CNT,
   ST_ARG_HDR_SPLIT,
   ST_ARG_PACING_WAY,
-  ST_ARG_VRX,
+  ST_ARG_START_VRX,
+  ST_ARG_PAD_INTERVAL,
+  ST_ARG_NO_PAD_STATIC,
   ST_ARG_SHAPING,
 
   ST_ARG_CONFIG_FILE = 0x300,
@@ -148,7 +150,9 @@ static struct option st_app_args_options[] = {
     {"rx_st22_sessions_count", required_argument, 0, ST22_ARG_RX_SESSIONS_CNT},
     {"hdr_split", no_argument, 0, ST_ARG_HDR_SPLIT},
     {"pacing_way", required_argument, 0, ST_ARG_PACING_WAY},
-    {"vrx", required_argument, 0, ST_ARG_VRX},
+    {"start_vrx", required_argument, 0, ST_ARG_START_VRX},
+    {"pad_interval", required_argument, 0, ST_ARG_PAD_INTERVAL},
+    {"no_static_pad", no_argument, 0, ST_ARG_NO_PAD_STATIC},
     {"shaping", required_argument, 0, ST_ARG_SHAPING},
 
     {"config_file", required_argument, 0, ST_ARG_CONFIG_FILE},
@@ -415,8 +419,14 @@ int st_app_parse_args(struct st_app_context* ctx, struct mtl_init_params* p, int
         else
           err("%s, unknow pacing way %s\n", __func__, optarg);
         break;
-      case ST_ARG_VRX:
-        ctx->tx_vrx = atoi(optarg);
+      case ST_ARG_START_VRX:
+        ctx->tx_start_vrx = atoi(optarg);
+        break;
+      case ST_ARG_PAD_INTERVAL:
+        ctx->tx_pad_interval = atoi(optarg);
+        break;
+      case ST_ARG_NO_PAD_STATIC:
+        ctx->tx_no_static_pad = true;
         break;
       case ST_ARG_SHAPING:
         if (!strcmp(optarg, "narrow"))

app/src/tx_st20p_app.c
@@ -238,6 +238,9 @@ static int app_tx_st20p_init(struct st_app_context* ctx, st_json_st20p_session_t
   ops.device = st20p ? st20p->info.device : ST_PLUGIN_DEVICE_AUTO;
   ops.framebuff_cnt = 2;
   ops.notify_frame_available = app_tx_st20p_frame_available;
+  ops.start_vrx = ctx->tx_start_vrx;
+  ops.pad_interval = ctx->tx_pad_interval;
+  if (ctx->tx_no_static_pad) ops.flags |= ST20P_TX_FLAG_DISABLE_STATIC_PAD_P;
 
   s->width = ops.width;
   s->height = ops.height;

app/src/tx_video_app.c
@@ -724,8 +724,10 @@ static int app_tx_video_init(struct st_app_context* ctx, st_json_video_session_t
   ops.notify_event = app_tx_notify_event;
   ops.framebuff_cnt = 2;
   ops.payload_type = video ? video->base.payload_type : ST_APP_PAYLOAD_TYPE_VIDEO;
-  ops.vrx = ctx->tx_vrx;
+  ops.start_vrx = ctx->tx_start_vrx;
+  ops.pad_interval = ctx->tx_pad_interval;
   if (s->enable_vsync) ops.flags |= ST20_TX_FLAG_ENABLE_VSYNC;
+  if (ctx->tx_no_static_pad) ops.flags |= ST20_TX_FLAG_DISABLE_STATIC_PAD_P;
 
   ret = st20_get_pgroup(ops.fmt, &s->st20_pg);
   if (ret < 0) return ret;

include/st20_api.h
@@ -60,6 +60,11 @@ extern "C" {
  * If enabled, lib will pass ST_EVENT_VSYNC by the notify_event on every epoch start.
  */
 #define ST20_TX_FLAG_ENABLE_VSYNC (MTL_BIT32(5))
+/**
+ * Flag bit in flags of struct st20_tx_ops.
+ * If disable the static RL pad interval profiling.
+ */
+#define ST20_TX_FLAG_DISABLE_STATIC_PAD_P (MTL_BIT32(6))
 
 /**
  * Flag bit in flags of struct st22_tx_ops.
@@ -899,13 +904,17 @@ struct st20_tx_ops {
    */
   uint8_t tx_dst_mac[MTL_SESSION_PORT_MAX][MTL_MAC_ADDR_LEN];
   /**
-   * vrx buffer for tx.
-   * Leave to zero if not know detail, lib will assign vrx(narrow) based on resolution and
-   * timing.
-   * Refer to st21 spec for the possible vrx value, lib will follow this VRX if it's not a
-   * zero value, and also fine tune is required since network setup difference.
+   * The start vrx buffer.
+   * Leave to zero if not know detail, lib will assign a start value of vrx(narrow) based
+   * on resolution and timing. Refer to st21 spec for the possible vrx value and also fine
+   * tune is required since network setup difference and RL burst.
    */
-  uint16_t vrx;
+  uint16_t start_vrx;
+  /**
+   * Manually assigned padding pkt interval(pkts level) for RL pacing.
+   * Leave to zero if not know detail, lib will train the interval in the initial routine.
+   */
+  uint16_t pad_interval;
 
   /**
    * the frame buffer count requested for one st20 tx session,

include/st_pipeline_api.h
@@ -383,10 +383,15 @@ enum st22_quality_mode {
  * If enabled, lib will pass ST_EVENT_VSYNC by the notify_event on every epoch start.
  */
 #define ST20P_TX_FLAG_ENABLE_VSYNC (MTL_BIT32(5))
+/**
+ * Flag bit in flags of struct st20p_tx_ops.
+ * If disable the static RL pad interval profiling.
+ */
+#define ST20P_TX_FLAG_DISABLE_STATIC_PAD_P (MTL_BIT32(6))
 
 /**
  * Flag bit in flags of struct st22p_rx_ops, for non MTL_PMD_DPDK_USER.
- * If set, it's application duty to set the rx flow(queue) and muticast join/drop.
+ * If set, it's application duty to set the rx flow(queue) and multicast join/drop.
  * Use st22p_rx_get_queue_meta to get the queue meta(queue number etc) info.
  */
 #define ST22P_RX_FLAG_DATA_PATH_ONLY (MTL_BIT32(0))
@@ -655,6 +660,18 @@ struct st20p_tx_ops {
    * Valid if ST20P_TX_FLAG_USER_P(R)_MAC is enabled
    */
   uint8_t tx_dst_mac[MTL_SESSION_PORT_MAX][MTL_MAC_ADDR_LEN];
+  /**
+   * The start vrx buffer.
+   * Leave to zero if not know detail, lib will assign a start value of vrx(narrow) based
+   * on resolution and timing. Refer to st21 spec for the possible vrx value and also fine
+   * tune is required since network setup difference and RL burst.
+   */
+  uint16_t start_vrx;
+  /**
+   * Manually assigned padding pkt interval(pkts level) for RL pacing.
+   * Leave to zero if not know detail, lib will train the interval in the initial routine.
+   */
+  uint16_t pad_interval;
   /** Session resolution width */
   uint32_t width;
   /** Session resolution height */

lib/src/st2110/pipeline/st20_pipeline_tx.c
@@ -246,6 +246,8 @@ static int tx_st20p_create_transport(struct mtl_main_impl* impl, struct st20p_tx
     ops_tx.flags |= ST20_TX_FLAG_USER_R_MAC;
   }
   ops_tx.pacing = ST21_PACING_NARROW;
+  ops_tx.start_vrx = ops->start_vrx;
+  ops_tx.pad_interval = ops->pad_interval;
   ops_tx.width = ops->width;
   ops_tx.height = ops->height;
   ops_tx.fps = ops->fps;
@@ -264,6 +266,8 @@ static int tx_st20p_create_transport(struct mtl_main_impl* impl, struct st20p_tx
   if (ops->flags & ST20P_TX_FLAG_USER_TIMESTAMP)
     ops_tx.flags |= ST20_TX_FLAG_USER_TIMESTAMP;
   if (ops->flags & ST20P_TX_FLAG_ENABLE_VSYNC) ops_tx.flags |= ST20_TX_FLAG_ENABLE_VSYNC;
+  if (ops->flags & ST20P_TX_FLAG_DISABLE_STATIC_PAD_P)
+    ops_tx.flags |= ST20_TX_FLAG_DISABLE_STATIC_PAD_P;
 
   transport = st20_tx_create(impl, &ops_tx);
   if (!transport) {

commit 1754a45133e1a5088b3d26ec7456b25327d03b73

Fix: Use LINE_TAIL pads for GPM_SL in tv_train_pacing
With rate-limit pacing, GPM_SL sessions whose lines do not split evenly
into packets, such as 720p and 2160p YUV 4:2:2 10-bit, finished each
frame early and pushed VRX past the ST 2110-21 narrow-sender limit.

tv_train_pacing() timed a full-size NORMAL pad in place of each line's
shorter LINE_TAIL packet, so it measured more bytes than a frame sends:
7,200 per frame at 720p and 54,000 at 2160p. The longer time understated
the packet rate and made the pad interval too large, leaving too few
pads to stretch each frame across its active period.

Train each line's last packet on the LINE_TAIL pad tv_init_hw() already
builds, so the trained bytes match the frame. The choice moves into
tv_train_pad_type() so a unit test can pin it. Other packing modes and
RTP-level and ST22 sessions train on the same pads as before.
St20TxRlTrainPadTest fails at 720p and 2160p when GPM_SL trains on
NORMAL pads only.

Fixes: a6fdd20b743b ("tx/video: add build-in static pad interval for rl pacing (#338)")
Signed-off-by: Wilczynski, Andrzej <andrzej.wilczynski@intel.com>

lib/src/st2110/st_tx_video_session.c
@@ -318,6 +318,31 @@ static int tv_poll_vsync(struct mtl_main_impl* impl, struct st_tx_video_session_
   return 0;
 }
 
+static enum st20_packet_type tv_train_pad_type(struct st_tx_video_session_impl* s,
+                                               int pkt_idx) {
+  enum st20_packet_type type;
+
+  if ((s->ops.type == ST20_TYPE_RTP_LEVEL) || (s->s_type == MT_ST22_HANDLE_TX_VIDEO)) {
+    type = ST20_PKT_TYPE_NORMAL;
+  } else if (s->ops.packing == ST20_PACKING_GPM_SL) {
+    type = (s->st20_pkt_info[ST20_PKT_TYPE_LINE_TAIL].number &&
+            pkt_idx % s->st20_pkts_in_line == s->st20_pkts_in_line - 1)
+               ? ST20_PKT_TYPE_LINE_TAIL
+               : ST20_PKT_TYPE_NORMAL;
+  } else { /* frame type */
+    uint32_t offset = s->st20_pkt_len * pkt_idx;
+    uint16_t line1_number = offset / s->st20_bytes_in_line;
+    /* last pkt should be treated as normal pkt also */
+    if ((offset + s->st20_pkt_len) < (line1_number + 1) * s->st20_bytes_in_line) {
+      type = ST20_PKT_TYPE_NORMAL;
+    } else {
+      type = ST20_PKT_TYPE_EXTRA;
+    }
+  }
+
+  return type;
+}
+
 static int uint64_t_cmp(const void* a, const void* b) {
   const uint64_t* ai = a;
   const uint64_t* bi = b;
@@ -400,24 +425,7 @@ static int tv_train_pacing(struct mtl_main_impl* impl, struct st_tx_video_sessio
   for (int loop = 0; loop < loop_frame; loop++) {
     uint64_t start = mt_get_ptp_time(impl, MTL_PORT_P);
     for (int i = 0; i < total; i++) {
-      enum st20_packet_type type;
-
-      if ((s->ops.type == ST20_TYPE_RTP_LEVEL) ||
-          (s->s_type == MT_ST22_HANDLE_TX_VIDEO) ||
-          (s->ops.packing == ST20_PACKING_GPM_SL)) {
-        type = ST20_PKT_TYPE_NORMAL;
-      } else { /* frame type */
-        uint32_t offset = s->st20_pkt_len * i;
-        uint16_t line1_number = offset / s->st20_bytes_in_line;
-        /* last pkt should be treated as normal pkt also */
-        if ((offset + s->st20_pkt_len) < (line1_number + 1) * s->st20_bytes_in_line) {
-          type = ST20_PKT_TYPE_NORMAL;
-        } else {
-          type = ST20_PKT_TYPE_EXTRA;
-        }
-      }
-
-      pad = s->pad[s_port][type];
+      pad = s->pad[s_port][tv_train_pad_type(s, i)];
       rte_mbuf_refcnt_update(pad, 1);
       mt_txq_burst_busy(queue, &pad, 1, 10);
     }

tests/unit/meson.build
@@ -108,6 +108,7 @@ unit_sources = [
   'session/st20_tx/rtp_timestamp_rounding_test.cpp',
   'session/st20_tx/rtp_gate_snap_test.cpp',
   'session/st20_tx/mempool_release_test.cpp',
+  'session/st20_tx/rl_train_pad_test.cpp',
   'session/multi_essence_sync_test.cpp',
   'session/rtcp_tx_harness.c',
   'session/rtcp/parse_nack_bounds_test.cpp',

tests/unit/session/st20_tx/rl_train_pad_test.cpp
@@ -1,0 +1,57 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright(c) 2026 Intel Corporation
+ *
+ * Pins that rate-limiter pacing trains on the bytes a real frame puts on the
+ * wire. tv_train_pacing() times one frame's worth of pads and derives the pad
+ * interval from that time, so pads heavier than the frame yield an interval that
+ * is too large: frames then finish early and VRX climbs. GPM_SL ends every line
+ * with a shorter LINE_TAIL packet unless the line splits evenly into packets.
+ *
+ * Build: meson setup build_unit -Denable_unit_tests=true && ninja -C build_unit
+ * Run:   ./build_unit/tests/unit/UnitTest --gtest_filter='St20TxRlTrainPadTest.*'
+ */
+
+#include <gtest/gtest.h>
+
+#include "session/st20_tx_harness.h"
+
+namespace {
+
+class St20TxRlTrainPadTest : public ::testing::Test {
+ protected:
+  void SetUp() override {
+    ASSERT_EQ(ut_txv_init(), 0);
+    ctx_ = ut_txv_create();
+    ASSERT_NE(ctx_, nullptr);
+  }
+  void TearDown() override {
+    ut_txv_destroy(ctx_);
+  }
+  ut_txv_ctx* ctx_ = nullptr;
+};
+
+TEST_F(St20TxRlTrainPadTest, GpmSl720pPadsMatchFrameBytes) {
+  ASSERT_EQ(ut_txv_init_gpm_sl_pkt(ctx_, 1280, 720), 0);
+  ASSERT_EQ(ut_txv_line_tail_pkts(ctx_), 720u);
+
+  EXPECT_EQ(ut_txv_train_pad_bytes(ctx_), ut_txv_frame_bytes(ctx_))
+      << "each line's short tail must be trained as a LINE_TAIL pad";
+}
+
+TEST_F(St20TxRlTrainPadTest, GpmSl2160pPadsMatchFrameBytes) {
+  ASSERT_EQ(ut_txv_init_gpm_sl_pkt(ctx_, 3840, 2160), 0);
+  ASSERT_EQ(ut_txv_line_tail_pkts(ctx_), 2160u);
+
+  EXPECT_EQ(ut_txv_train_pad_bytes(ctx_), ut_txv_frame_bytes(ctx_))
+      << "each line's short tail must be trained as a LINE_TAIL pad";
+}
+
+TEST_F(St20TxRlTrainPadTest, GpmSl1080pWithoutLineTailPadsMatchFrameBytes) {
+  ASSERT_EQ(ut_txv_init_gpm_sl_pkt(ctx_, 1920, 1080), 0);
+  ASSERT_EQ(ut_txv_line_tail_pkts(ctx_), 0u);
+
+  EXPECT_EQ(ut_txv_train_pad_bytes(ctx_), ut_txv_frame_bytes(ctx_))
+      << "1080p lines split evenly, so no LINE_TAIL pad exists to be trained";
+}
+
+}  // namespace

tests/unit/session/st20_tx_harness.c
@@ -641,6 +641,39 @@ bool ut_txv_hdr_mempool_installed(const ut_txv_ctx* ctx) {
   return ctx->session.mbuf_mempool_hdr[MTL_SESSION_PORT_P] != NULL;
 }
 
+/* ── RL pacing training pads (tv_train_pad_type) ──────────────────────── */
+
+int ut_txv_init_gpm_sl_pkt(ut_txv_ctx* ctx, uint32_t width, uint32_t height) {
+  struct st_tx_video_session_impl* s = &ctx->session;
+  struct st20_tx_ops* ops = &s->ops;
+
+  s->s_type = MT_HANDLE_TX_VIDEO;
+  ops->type = ST20_TYPE_FRAME_LEVEL;
+  ops->packing = ST20_PACKING_GPM_SL;
+  ops->fmt = ST20_FMT_YUV_422_10BIT;
+  ops->width = width;
+  ops->height = height;
+  if (st20_get_pgroup(ops->fmt, &s->st20_pg) < 0) return -EINVAL;
+  return tv_init_pkt(&ctx->impl, s, ops, NULL);
+}
+
+uint64_t ut_txv_train_pad_bytes(ut_txv_ctx* ctx) {
+  struct st_tx_video_session_impl* s = &ctx->session;
+  uint64_t bytes = 0;
+
+  /* tv_init_hw() builds each pad at its type's st20_pkt_info[].size */
+  for (int i = 0; i < s->st20_total_pkts; i++)
+    bytes += s->st20_pkt_info[tv_train_pad_type(s, i)].size;
+  return bytes;
+}
+
+uint64_t ut_txv_frame_bytes(const ut_txv_ctx* ctx) {
+  const struct st_tx_video_session_impl* s = &ctx->session;
+
+  return (uint64_t)s->ops.height * s->st20_bytes_in_line +
+         (uint64_t)s->st20_total_pkts * sizeof(struct st_rfc4175_video_hdr);
+}
+
 /* ── accessors ─────────────────────────────────────────────────────────── */
 
 uint64_t ut_txv_cur_epochs(const ut_txv_ctx* ctx) {
@@ -737,4 +770,8 @@ uint64_t ut_txv_stat_exceed_frame_time(const ut_txv_ctx* ctx) {
 
 uint32_t ut_txv_rtp_time_stamp(const ut_txv_ctx* ctx) {
   return ctx->session.pacing.rtp_time_stamp;
+}
+
+uint32_t ut_txv_line_tail_pkts(const ut_txv_ctx* ctx) {
+  return ctx->session.st20_pkt_info[ST20_PKT_TYPE_LINE_TAIL].number;
 }

tests/unit/session/st20_tx_harness.h
@@ -157,6 +157,16 @@ bool ut_txv_hdr_mempool_installed(const ut_txv_ctx* ctx);
 /* Whether the pool itself still exists, independent of the session's pointer. */
 bool ut_txv_hdr_mempool_alive(const ut_txv_ctx* ctx);
 
+/* ── RL pacing training pads (tv_train_pad_type) ──────────────────────── */
+/* Lays out a progressive YUV 4:2:2 10-bit GPM_SL session through the real
+ * tv_init_pkt(). Returns its status. */
+int ut_txv_init_gpm_sl_pkt(ut_txv_ctx* ctx, uint32_t width, uint32_t height);
+/* Bytes of the st20_total_pkts pads tv_train_pacing() times as one frame. */
+uint64_t ut_txv_train_pad_bytes(ut_txv_ctx* ctx);
+/* Bytes one progressive GPM_SL frame puts on the wire: every line's pixels plus
+ * one st_rfc4175_video_hdr per packet, independent of st20_pkt_info[]. */
+uint64_t ut_txv_frame_bytes(const ut_txv_ctx* ctx);
+
 /* ── accessors ─────────────────────────────────────────────────────────── */
 uint64_t ut_txv_cur_epochs(const ut_txv_ctx* ctx);
 long double ut_txv_pacing_tr_offset(const ut_txv_ctx* ctx);
@@ -187,6 +197,7 @@ uint64_t ut_txv_stat_port_frames(const ut_txv_ctx* ctx);
 uint64_t ut_txv_stat_exceed_frame_time(const ut_txv_ctx* ctx);
 /* Result of the last ut_txv_update_rtp_time_stamp() call. */
 uint32_t ut_txv_rtp_time_stamp(const ut_txv_ctx* ctx);
+uint32_t ut_txv_line_tail_pkts(const ut_txv_ctx* ctx);
 
 #ifdef __cplusplus
 }