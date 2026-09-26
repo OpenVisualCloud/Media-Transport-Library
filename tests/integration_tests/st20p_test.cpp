OpenVisualCloud/Media-Transport-Library

commit c0fdad3106dc31805a0899d3e3c476d656f2d613

srss: add mt_srss_burst support by ring (#342)
mt_srss_burst dequeue from ring, srss tasklet enqueue to ring.
Also delete the rss code in rv video as the rss behavior is the same rxq.

Signed-off-by: Frank Du <frank.du@intel.com>

lib/src/mt_shared_rss.c
@@ -6,17 +6,44 @@
 
 #include "mt_log.h"
 #include "mt_sch.h"
+#include "mt_stat.h"
 #include "mt_util.h"
 
 #define MT_SRSS_BURST_SIZE (128)
+#define MT_SRSS_RING_PREFIX "SR_"
 
-#define UPDATE_ENTRY()                                                       \
-  do {                                                                       \
-    if (matched_pkts_nb)                                                     \
-      last_srss_entry->flow.cb(last_srss_entry->flow.priv, &matched_pkts[0], \
-                               matched_pkts_nb);                             \
-    last_srss_entry = srss_entry;                                            \
-    matched_pkts_nb = 0;                                                     \
+static inline void srss_lock(struct mt_srss_impl* srss) {
+  mt_pthread_mutex_lock(&srss->mutex);
+}
+
+/* return true if try lock succ */
+static inline bool srss_try_lock(struct mt_srss_impl* srss) {
+  int ret = mt_pthread_mutex_try_lock(&srss->mutex);
+  return ret == 0 ? true : false;
+}
+
+static inline void srss_unlock(struct mt_srss_impl* srss) {
+  mt_pthread_mutex_unlock(&srss->mutex);
+}
+
+static inline void srss_entry_pkts_enqueue(struct mt_srss_entry* entry,
+                                           struct rte_mbuf** pkts,
+                                           const uint16_t nb_pkts) {
+  /* use bulk version */
+  unsigned int n = rte_ring_sp_enqueue_bulk(entry->ring, (void**)pkts, nb_pkts, NULL);
+  entry->stat_enqueue_cnt += n;
+  if (n == 0) {
+    rte_pktmbuf_free_bulk(pkts, nb_pkts);
+    entry->stat_enqueue_fail_cnt += nb_pkts;
+  }
+}
+
+#define UPDATE_ENTRY()                                                             \
+  do {                                                                             \
+    if (matched_pkts_nb)                                                           \
+      srss_entry_pkts_enqueue(last_srss_entry, &matched_pkts[0], matched_pkts_nb); \
+    last_srss_entry = srss_entry;                                                  \
+    matched_pkts_nb = 0;                                                           \
   } while (0)
 
 static int srss_tasklet_handler(void* priv) {
@@ -29,9 +56,10 @@ static int srss_tasklet_handler(void* priv) {
   struct rte_ipv4_hdr* ipv4;
   struct rte_udp_hdr* udp;
 
+  srss_lock(srss);
   for (uint16_t queue = 0; queue < inf->max_rx_queues; queue++) {
     uint16_t matched_pkts_nb = 0;
-    pthread_mutex_lock(&srss->mutex);
+
     uint16_t rx =
         rte_eth_rx_burst(mt_port_id(impl, srss->port), queue, pkts, MT_SRSS_BURST_SIZE);
     if (rx) {
@@ -42,15 +70,13 @@ static int srss_tasklet_handler(void* priv) {
         if (hdr->eth.ether_type !=
             htons(RTE_ETHER_TYPE_IPV4)) { /* non ip, redirect to cni */
           UPDATE_ENTRY();
-          if (srss->cni_entry)
-            srss->cni_entry->flow.cb(srss->cni_entry->flow.priv, &pkts[i], 1);
+          srss_entry_pkts_enqueue(srss->cni_entry, &pkts[i], 1);
           continue;
         }
         ipv4 = &hdr->ipv4;
         if (ipv4->next_proto_id != IPPROTO_UDP) { /* non udp, redirect to cni */
           UPDATE_ENTRY();
-          if (srss->cni_entry)
-            srss->cni_entry->flow.cb(srss->cni_entry->flow.priv, &pkts[i], 1);
+          srss_entry_pkts_enqueue(srss->cni_entry, &pkts[i], 1);
           continue;
         }
         udp = &hdr->udp;
@@ -77,17 +103,14 @@ static int srss_tasklet_handler(void* priv) {
         }
         if (!srss_entry) { /* no match, redirect to cni */
           UPDATE_ENTRY();
-          if (srss->cni_entry)
-            srss->cni_entry->flow.cb(srss->cni_entry->flow.priv, &pkts[i], 1);
+          srss_entry_pkts_enqueue(srss->cni_entry, &pkts[i], 1);
         }
       }
       if (matched_pkts_nb)
-        last_srss_entry->flow.cb(last_srss_entry->flow.priv, &matched_pkts[0],
-                                 matched_pkts_nb);
-      rte_pktmbuf_free_bulk(&pkts[0], rx);
+        srss_entry_pkts_enqueue(last_srss_entry, &matched_pkts[0], matched_pkts_nb);
     }
-    pthread_mutex_unlock(&srss->mutex);
   }
+  srss_unlock(srss);
 
   return 0;
 }
@@ -141,6 +164,7 @@ static int srss_tasklet_start(void* priv) {
 
   return 0;
 }
+
 static int srss_tasklet_stop(void* priv) {
   struct mt_srss_impl* srss = priv;
 
@@ -149,56 +173,102 @@ static int srss_tasklet_stop(void* priv) {
   return 0;
 }
 
+static int srss_stat(void* priv) {
+  struct mt_srss_impl* srss = priv;
+  enum mtl_port port = srss->port;
+  struct mt_srss_entry* entry;
+  int idx;
+
+  if (!srss_try_lock(srss)) return 0;
+  MT_TAILQ_FOREACH(entry, &srss->head, next) {
+    idx = entry->idx;
+    notice("%s(%d,%d), enqueue %u dequeue %u\n", __func__, port, idx,
+           entry->stat_enqueue_cnt, entry->stat_dequeue_cnt);
+    entry->stat_enqueue_cnt = 0;
+    entry->stat_dequeue_cnt = 0;
+    if (entry->stat_enqueue_fail_cnt) {
+      warn("%s(%d,%d), enqueue fail %u\n", __func__, port, idx,
+           entry->stat_enqueue_fail_cnt);
+      entry->stat_enqueue_fail_cnt = 0;
+    }
+  }
+  srss_unlock(srss);
+
+  return 0;
+}
+
 struct mt_srss_entry* mt_srss_get(struct mtl_main_impl* impl, enum mtl_port port,
                                   struct mt_rxq_flow* flow) {
   struct mt_srss_impl* srss = impl->srss[port];
+  int idx = srss->entry_idx;
   struct mt_srss_entry* entry;
-  static uint16_t srss_queue_id;
 
   if (!mt_has_srss(impl, port)) {
-    err("%s(%d), shared rss not enabled\n", __func__, port);
+    err("%s(%d,%d), shared rss not enabled\n", __func__, port, idx);
     return NULL;
   }
   if (!flow->cb) {
-    err("%s(%d), no cb in the flow\n", __func__, port);
+    err("%s(%d,%d), no cb in the flow\n", __func__, port, idx);
     return NULL;
   }
 
   MT_TAILQ_FOREACH(entry, &srss->head, next) {
     if (entry->flow.dst_port == flow->dst_port &&
         *(uint32_t*)entry->flow.dip_addr == *(uint32_t*)flow->dip_addr) {
-      err("%s(%d), already has entry %u.%u.%u.%u:%u\n", __func__, port, flow->dip_addr[0],
-          flow->dip_addr[1], flow->dip_addr[2], flow->dip_addr[3], flow->dst_port);
+      err("%s(%d,%d), already has entry %u.%u.%u.%u:%u\n", __func__, port, idx,
+          flow->dip_addr[0], flow->dip_addr[1], flow->dip_addr[2], flow->dip_addr[3],
+          flow->dst_port);
       return NULL;
     }
   }
 
   entry = mt_rte_zmalloc_socket(sizeof(*entry), mt_socket_id(impl, port));
   if (!entry) {
-    err("%s(%d), malloc fail\n", __func__, port);
+    err("%s(%d,%d), malloc fail\n", __func__, port, idx);
     return NULL;
   }
+
+  /* ring create */
+  char ring_name[32];
+  snprintf(ring_name, 32, "%sP%d_%d", MT_SRSS_RING_PREFIX, port, idx);
+  entry->ring = rte_ring_create(ring_name, 512, mt_socket_id(impl, MTL_PORT_P),
+                                RING_F_SP_ENQ | RING_F_SC_DEQ);
+  if (!entry->ring) {
+    err("%s(%d,%d), ring create fail\n", __func__, port, idx);
+    mt_rte_free(entry);
+    return NULL;
+  }
+
   entry->flow = *flow;
   entry->srss = srss;
-  entry->queue_id = srss_queue_id; /* use a dummy queue id */
-  srss_queue_id++;
-  pthread_mutex_lock(&srss->mutex);
+  entry->idx = idx;
+
+  srss->entry_idx++;
+  srss_lock(srss);
   MT_TAILQ_INSERT_TAIL(&srss->head, entry, next);
   if (flow->sys_queue) srss->cni_entry = entry;
-  pthread_mutex_unlock(&srss->mutex);
+  srss_unlock(srss);
 
-  info("%s(%d), entry %u.%u.%u.%u:(dst)%u succ\n", __func__, port, flow->dip_addr[0],
-       flow->dip_addr[1], flow->dip_addr[2], flow->dip_addr[3], flow->dst_port);
+  info("%s(%d), entry %u.%u.%u.%u:(dst)%u on %d\n", __func__, port, flow->dip_addr[0],
+       flow->dip_addr[1], flow->dip_addr[2], flow->dip_addr[3], flow->dst_port, idx);
   return entry;
 }
 
 int mt_srss_put(struct mt_srss_entry* entry) {
   struct mt_srss_impl* srss = entry->srss;
+  enum mtl_port port = srss->port;
 
-  pthread_mutex_lock(&srss->mutex);
+  srss_lock(srss);
   MT_TAILQ_REMOVE(&srss->head, entry, next);
-  pthread_mutex_unlock(&srss->mutex);
+  srss_unlock(srss);
 
+  if (entry->ring) {
+    mt_ring_dequeue_clean(entry->ring);
+    rte_ring_free(entry->ring);
+    entry->ring = NULL;
+  }
+
+  notice("%s(%d), succ on %d\n", __func__, port, entry->idx);
   mt_rte_free(entry);
   return 0;
 }
@@ -209,6 +279,7 @@ int mt_srss_init(struct mtl_main_impl* impl) {
 
   for (int i = 0; i < num_ports; i++) {
     if (!mt_has_srss(impl, i)) continue;
+
     impl->srss[i] = mt_rte_zmalloc_socket(sizeof(*impl->srss[i]), mt_socket_id(impl, i));
     if (!impl->srss[i]) {
       err("%s(%d), srss malloc fail\n", __func__, i);
@@ -217,15 +288,14 @@ int mt_srss_init(struct mtl_main_impl* impl) {
     }
     struct mt_srss_impl* srss = impl->srss[i];
 
-    ret = pthread_mutex_init(&srss->mutex, NULL);
+    ret = mt_pthread_mutex_init(&srss->mutex, NULL);
     if (ret < 0) {
       err("%s(%d), mutex init fail\n", __func__, i);
       mt_srss_uinit(impl);
       return ret;
     }
 
-    struct mt_sch_impl* sch = mt_sch_get(impl, mt_if(impl, i)->link_speed,
-                                         MT_SCH_TYPE_DEFAULT, MT_SCH_MASK_ALL);
+    struct mt_sch_impl* sch = mt_sch_get(impl, 0, MT_SCH_TYPE_DEFAULT, MT_SCH_MASK_ALL);
     if (!sch) {
       err("%s(%d), get sch fail\n", __func__, i);
       mt_srss_uinit(impl);
@@ -260,6 +330,8 @@ int mt_srss_init(struct mtl_main_impl* impl) {
       return ret;
     }
 
+    mt_stat_register(impl, srss_stat, srss, "srss");
+
     info("%s(%d), succ with shared rss mode\n", __func__, i);
   }
 
@@ -271,26 +343,28 @@ int mt_srss_uinit(struct mtl_main_impl* impl) {
 
   for (int i = 0; i < num_ports; i++) {
     struct mt_srss_impl* srss = impl->srss[i];
-    if (srss) {
-      srss_traffic_thread_stop(srss);
-      if (srss->tasklet) {
-        mt_sch_unregister_tasklet(srss->tasklet);
-        srss->tasklet = NULL;
-      }
-      if (srss->sch) {
-        mt_sch_put(srss->sch, mt_if(impl, i)->link_speed);
-        srss->sch = NULL;
-      }
-      struct mt_srss_entry* entry;
-      while ((entry = MT_TAILQ_FIRST(&srss->head))) {
-        warn("%s, still has entry %p\n", __func__, entry);
-        MT_TAILQ_REMOVE(&srss->head, entry, next);
-        mt_rte_free(entry);
-      }
-      pthread_mutex_destroy(&srss->mutex);
-      mt_rte_free(srss);
-      impl->srss[i] = NULL;
+    if (!srss) continue;
+
+    mt_stat_unregister(impl, srss_stat, srss);
+    srss_traffic_thread_stop(srss);
+    if (srss->tasklet) {
+      mt_sch_unregister_tasklet(srss->tasklet);
+      srss->tasklet = NULL;
     }
+    if (srss->sch) {
+      mt_sch_put(srss->sch, 0);
+      srss->sch = NULL;
+    }
+    struct mt_srss_entry* entry;
+    while ((entry = MT_TAILQ_FIRST(&srss->head))) {
+      warn("%s, still has entry %p\n", __func__, entry);
+      MT_TAILQ_REMOVE(&srss->head, entry, next);
+      mt_rte_free(entry);
+    }
+    mt_pthread_mutex_destroy(&srss->mutex);
+    mt_rte_free(srss);
+    impl->srss[i] = NULL;
   }
+
   return 0;
 }

lib/src/mt_cni.c
@@ -340,10 +340,6 @@ int mt_cni_init(struct mtl_main_impl* impl) {
   ret = mt_tap_init(impl);
   if (ret < 0) return ret;
 
-  if (mt_has_srss(impl, MTL_PORT_P)) {
-    goto exit_with_stat_register;
-  }
-
   if (cni->lcore_tasklet) {
     struct mt_sch_tasklet_ops ops;
 
@@ -369,9 +365,7 @@ int mt_cni_init(struct mtl_main_impl* impl) {
     return ret;
   }
 
-exit_with_stat_register:
   mt_stat_register(impl, cni_stat, cni, "cni");
-
   return 0;
 }
 

lib/src/mt_main.h
@@ -757,7 +757,11 @@ struct mt_tsq_impl {
 struct mt_srss_entry {
   struct mt_rxq_flow flow;
   struct mt_srss_impl* srss;
-  uint16_t queue_id;
+  int idx;
+  struct rte_ring* ring;
+  uint32_t stat_enqueue_cnt;
+  uint32_t stat_dequeue_cnt;
+  uint32_t stat_enqueue_fail_cnt;
   /* linked list */
   MT_TAILQ_ENTRY(mt_srss_entry) next;
 };
@@ -773,6 +777,7 @@ struct mt_srss_impl {
   struct mt_sch_tasklet_impl* tasklet;
   struct mt_sch_impl* sch;
   struct mt_srss_entry* cni_entry;
+  int entry_idx;
 };
 
 struct mtl_main_impl {

lib/src/mt_queue.c
@@ -58,7 +58,7 @@ uint16_t mt_rxq_burst(struct mt_rxq_entry* entry, struct rte_mbuf** rx_pkts,
                       const uint16_t nb_pkts) {
   uint16_t rx;
   if (entry->srss) {
-    rx = 0; /* srss rx on the srss tasklet */
+    rx = mt_srss_burst(entry->srss, rx_pkts, nb_pkts);
   } else if (entry->rsq) {
     mt_rsq_burst(entry->rsq, nb_pkts);
     rx = 0; /* the buf is handled in the callback */

lib/src/mt_shared_rss.h
@@ -14,7 +14,13 @@ int mt_srss_uinit(struct mtl_main_impl* impl);
 struct mt_srss_entry* mt_srss_get(struct mtl_main_impl* impl, enum mtl_port port,
                                   struct mt_rxq_flow* flow);
 static inline uint16_t mt_srss_queue_id(struct mt_srss_entry* entry) {
-  return entry->queue_id;
+  return entry->idx; /* use dummy id*/
+}
+static inline uint16_t mt_srss_burst(struct mt_srss_entry* entry,
+                                     struct rte_mbuf** rx_pkts, const uint16_t nb_pkts) {
+  uint16_t n = rte_ring_sc_dequeue_burst(entry->ring, (void**)rx_pkts, nb_pkts, NULL);
+  entry->stat_dequeue_cnt += n;
+  return n;
 }
 int mt_srss_put(struct mt_srss_entry* entry);
 

lib/src/mt_util.h
@@ -50,6 +50,7 @@ static inline void mt_u32_to_ip(uint32_t group, uint8_t ip[MTL_IP_ADDR_LEN]) {
 bool mt_bitmap_test_and_set(uint8_t* bitmap, int idx);
 bool mt_bitmap_test(uint8_t* bitmap, int idx);
 
+/* only for mbuf ring with RING_F_SP_ENQ | RING_F_SC_DEQ */
 int mt_ring_dequeue_clean(struct rte_ring* ring);
 
 void mt_mbuf_sanity_check(struct rte_mbuf** mbufs, uint16_t nb, char* tag);

lib/src/st2110/st_rx_ancillary_session.c
@@ -475,19 +475,17 @@ static int rx_ancillary_sessions_mgr_init(struct mtl_main_impl* impl,
     rte_spinlock_init(&mgr->mutex[i]);
   }
 
-  if (!mt_has_srss(impl, MTL_PORT_P)) {
-    memset(&ops, 0x0, sizeof(ops));
-    ops.priv = mgr;
-    ops.name = "rx_anc_sessions_mgr";
-    ops.start = rx_ancillary_sessions_tasklet_start;
-    ops.stop = rx_ancillary_sessions_tasklet_stop;
-    ops.handler = rx_ancillary_sessions_tasklet_handler;
+  memset(&ops, 0x0, sizeof(ops));
+  ops.priv = mgr;
+  ops.name = "rx_anc_sessions_mgr";
+  ops.start = rx_ancillary_sessions_tasklet_start;
+  ops.stop = rx_ancillary_sessions_tasklet_stop;
+  ops.handler = rx_ancillary_sessions_tasklet_handler;
 
-    mgr->tasklet = mt_sch_register_tasklet(sch, &ops);
-    if (!mgr->tasklet) {
-      err("%s(%d), mt_sch_register_tasklet fail\n", __func__, idx);
-      return -EIO;
-    }
+  mgr->tasklet = mt_sch_register_tasklet(sch, &ops);
+  if (!mgr->tasklet) {
+    err("%s(%d), mt_sch_register_tasklet fail\n", __func__, idx);
+    return -EIO;
   }
 
   mt_stat_register(mgr->parent, st_rx_ancillary_sessions_stat, mgr, "rx_anc");

lib/src/st2110/st_rx_audio_session.c
@@ -900,19 +900,17 @@ static int rx_audio_sessions_mgr_init(struct mtl_main_impl* impl, struct mt_sch_
     rte_spinlock_init(&mgr->mutex[i]);
   }
 
-  if (!mt_has_srss(impl, MTL_PORT_P)) {
-    memset(&ops, 0x0, sizeof(ops));
-    ops.priv = mgr;
-    ops.name = "rx_audio_sessions_mgr";
-    ops.start = rx_audio_sessions_tasklet_start;
-    ops.stop = rx_audio_sessions_tasklet_stop;
-    ops.handler = rx_audio_sessions_tasklet_handler;
+  memset(&ops, 0x0, sizeof(ops));
+  ops.priv = mgr;
+  ops.name = "rx_audio_sessions_mgr";
+  ops.start = rx_audio_sessions_tasklet_start;
+  ops.stop = rx_audio_sessions_tasklet_stop;
+  ops.handler = rx_audio_sessions_tasklet_handler;
 
-    mgr->tasklet = mt_sch_register_tasklet(sch, &ops);
-    if (!mgr->tasklet) {
-      err("%s(%d), mt_sch_register_tasklet fail\n", __func__, idx);
-      return -EIO;
-    }
+  mgr->tasklet = mt_sch_register_tasklet(sch, &ops);
+  if (!mgr->tasklet) {
+    err("%s(%d), mt_sch_register_tasklet fail\n", __func__, idx);
+    return -EIO;
   }
 
   mt_stat_register(mgr->parent, st_rx_audio_sessions_stat, mgr, "rx_audio");

commit af9092e484155f36d248df2809e426039c97e380

Fix: Give shared RSS dispatchers a full sch quota
With shared RSS on, the first of four 2160p119 st20p TX sessions ran
at 54-79 fps while the other three held 120.

mt_srss_init() has requested zero quota for the dispatchers since
c0fdad3106dc. They landed on main_sch and left its quota total at
zero, which admits any request, so the first video session joined
them. Video TX builds a fixed burst per scheduler loop, and the
dispatchers' RX polling made each loop too long to reach the frame
rate.

Request the full data_quota_mbs_limit, so no quota-bearing session
shares a dispatcher's scheduler; zero-quota ancillary and fast-metadata
sessions still can. This needs the two commits before it: alone on a
scheduler under MTL_FLAG_TASKLET_SLEEP, a dispatcher without their
pending report and sleep advice sleeps 1 ms per poll.

Each dispatcher now has a scheduler to itself. Port 0's first one
fills main_sch, which keeps the CNI and PTP tasklets, so the sessions
need one scheduler more; under MTL_FLAG_DEDICATED_SYS_LCORE that
dispatcher leaves the system lcore instead. Each further port adds
rss_sch_nb more, as its dispatchers no longer share port 0's. In lcore
mode each costs an lcore, on ENA and ixgbe too, where shared RSS is on
by default. A host short of lcores fails with "no available lcore" at
mtl_start() or session create, and one past MT_MAX_SCH_NUM schedulers
with "no free sch".

Under MTL_FLAG_TASKLET_SLEEP an RX session no longer shares the
dispatcher's lcore, so the 512-entry ring between them has to cover
that lcore's wake-up delay: a 2160p59 RX-only process here fell from
59.9 to 58.1-58.8 fps.

St20p.tx_sch_separate_from_shared_rss fails without this under
--rss_mode l3_l4: the TX session adds no scheduler.
MtSrssInitTest.RequestsFullSchQuota fails on the old request of 0.

Fixes: c0fdad3106dc ("srss: add mt_srss_burst support by ring (#342)")
Signed-off-by: Wilczynski, Andrzej <andrzej.wilczynski@intel.com>

lib/src/datapath/mt_shared_rss.c
@@ -478,7 +478,7 @@ int mt_srss_init(struct mtl_main_impl* impl) {
       struct mt_srss_sch* srss_sch = &srss->schs[s_idx];
       srss_sch->parent = srss;
       srss_sch->idx = s_idx;
-      srss_sch->quota_mps = 0;
+      srss_sch->quota_mps = impl->main_sch->data_quota_mbs_limit;
       srss_sch->q_start = q_idx;
       uint16_t q_end = srss_sch->q_start + q_per_sch;
       if (s_idx < q_remaining) q_end++;

tests/integration_tests/st20p_test.cpp
@@ -2432,3 +2432,27 @@ TEST(St20p, tx_no_epoch_drop) {
 TEST(St20p, tx_user_pacing_no_epoch_drop) {
   st20p_tx_epoch_drop_test(true);
 }
+
+TEST(St20p, tx_sch_separate_from_shared_rss) {
+  auto ctx = (struct st_tests_context*)st_test_ctx();
+  if (ctx->rss_mode == MTL_RSS_MODE_NONE)
+    GTEST_SKIP() << "shared RSS is off, run with --rss_mode l3_l4";
+
+  auto test_ctx = new tests_context();
+  ASSERT_TRUE(test_ctx != NULL);
+  test_ctx->idx = 0;
+  test_ctx->ctx = ctx;
+  test_ctx->fb_cnt = 3;
+  struct st20p_tx_ops ops_tx;
+  st20p_tx_ops_init(test_ctx, &ops_tx);
+
+  int sch_cnt = st_test_sch_cnt(ctx);
+  auto tx_handle = st20p_tx_create(ctx->handle, &ops_tx);
+  ASSERT_TRUE(tx_handle != NULL);
+  EXPECT_EQ(st_test_sch_cnt(ctx), sch_cnt + 1)
+      << "tx session joined an existing scheduler";
+
+  int ret = st20p_tx_free(tx_handle);
+  EXPECT_GE(ret, 0);
+  delete test_ctx;
+}

tests/unit/datapath/mt_srss_harness.c
@@ -49,6 +49,7 @@ struct ut_srss_ctx {
   struct mt_srss_impl srss;
   struct mt_srss_sch srss_sch;
   struct mt_sch_tasklet_impl tasklet;
+  int requested_quota_mbs;
   uint16_t nb_pkts;
 };
 
@@ -70,9 +71,9 @@ static uint16_t ut_rte_eth_rx_burst(uint16_t port_id, uint16_t queue_id,
 
 static struct mtl_sch_impl* ut_mt_sch_get(struct mtl_main_impl* impl, int quota_mbs,
                                           enum mt_sch_type type, mt_sch_mask_t mask) {
-  (void)quota_mbs;
   (void)type;
   (void)mask;
+  ut_active_ctx->requested_quota_mbs = quota_mbs;
   return impl->main_sch;
 }
 
@@ -144,6 +145,8 @@ ut_srss_ctx* ut_srss_init_port(uint32_t link_speed_mbps) {
 
   ctx->impl.user_para.num_ports = 1;
   ctx->impl.main_sch = mt_sch_instance(&ctx->impl, 0);
+  ctx->impl.main_sch->data_quota_mbs_limit =
+      ST_QUOTA_TX1080P_PER_SCH * st20_1080p59_yuv422_10bit_bandwidth_mps();
   struct mt_interface* inf = mt_if(&ctx->impl, MTL_PORT_P);
   inf->rss_mode = MTL_RSS_MODE_L3_L4;
   inf->nb_rx_q = 1;
@@ -170,6 +173,14 @@ int ut_srss_tasklet_handler(ut_srss_ctx* ctx) {
 
 uint64_t ut_srss_registered_advice_sleep_us(const ut_srss_ctx* ctx) {
   return ctx->tasklet.ops.advice_sleep_us;
+}
+
+int ut_srss_requested_quota_mbs(const ut_srss_ctx* ctx) {
+  return ctx->requested_quota_mbs;
+}
+
+int ut_srss_main_sch_quota_limit_mbs(const ut_srss_ctx* ctx) {
+  return ctx->impl.main_sch->data_quota_mbs_limit;
 }
 
 uint16_t ut_srss_burst_size(void) {

tests/unit/datapath/mt_srss_harness.h
@@ -27,6 +27,10 @@ int ut_srss_tasklet_handler(ut_srss_ctx* ctx);
 /* advice_sleep_us of the tasklet mt_srss_init() registered. */
 uint64_t ut_srss_registered_advice_sleep_us(const ut_srss_ctx* ctx);
 
+/* The quota mt_srss_init() asked mt_sch_get() for, and the main sch's quota limit. */
+int ut_srss_requested_quota_mbs(const ut_srss_ctx* ctx);
+int ut_srss_main_sch_quota_limit_mbs(const ut_srss_ctx* ctx);
+
 /* MT_SRSS_BURST_SIZE, the most packets one rx burst takes from a queue. */
 uint16_t ut_srss_burst_size(void);
 

tests/unit/datapath/mt_srss_tasklet_test.cpp
@@ -71,3 +71,10 @@ TEST_F(MtSrssInitTest, UnreportedSpeedAdvisesAs100G) {
   EXPECT_EQ(RegisteredAdviceUs(RTE_ETH_SPEED_NUM_NONE), advice_100g_us);
   EXPECT_EQ(RegisteredAdviceUs(RTE_ETH_SPEED_NUM_UNKNOWN), advice_100g_us);
 }
+
+TEST_F(MtSrssInitTest, RequestsFullSchQuota) {
+  ctx_ = ut_srss_init_port(RTE_ETH_SPEED_NUM_100G);
+  ASSERT_NE(ctx_, nullptr);
+  ASSERT_GT(ut_srss_main_sch_quota_limit_mbs(ctx_), 0);
+  EXPECT_EQ(ut_srss_requested_quota_mbs(ctx_), ut_srss_main_sch_quota_limit_mbs(ctx_));
+}