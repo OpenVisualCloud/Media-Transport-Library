OpenVisualCloud/Media-Transport-Library

commit 5bbfe980a0994d3628656f3cbd62b23546ef9011

Fix: handling case where RL is lower then expected (#1205)
1. Fix handling case where RL is lower then expected
2. Update ice driver to version 2.2.8

---------

Signed-off-by: Szumski, Tomasz<tomasz.szumski@intel.com>
Co-authored-by: Kasiewicz, Marek <marek.kasiewicz@intel.com>

lib/src/st2110/st_tx_video_session.c
@@ -322,6 +322,8 @@ static int tv_train_pacing(struct mtl_main_impl* impl, struct st_tx_video_sessio
   float pad_interval;
   uint64_t rl_bps = tv_rl_bps(s);
   uint64_t train_start_time, train_end_time;
+  double measured_bps;
+  uint64_t bps_to_set;
 
   uint16_t resolved = s->ops.pad_interval;
   if (resolved) {
@@ -329,7 +331,7 @@ static int tv_train_pacing(struct mtl_main_impl* impl, struct st_tx_video_sessio
     info("%s(%d), user customized pad_interval %u\n", __func__, idx, resolved);
     return 0;
   }
-  if (!(s->ops.flags & ST20_TX_FLAG_DISABLE_STATIC_PAD_P)) {
+  if ((s->ops.flags & ST20_TX_FLAG_ENABLE_STATIC_PAD_P)) {
     resolved = st20_pacing_static_profiling(impl, s, s_port);
     if (resolved) {
       s->pacing.pad_interval = resolved;
@@ -338,7 +340,7 @@ static int tv_train_pacing(struct mtl_main_impl* impl, struct st_tx_video_sessio
     }
   }
 
-  ret = mt_pacing_train_result_search(impl, port, rl_bps, &pad_interval);
+  ret = mt_pacing_train_pad_result_search(impl, port, rl_bps, &pad_interval);
   if (ret >= 0) {
     s->pacing.pad_interval = pad_interval;
     info("%s(%d), use pre-train pad_interval %f\n", __func__, idx, pad_interval);
@@ -423,10 +425,30 @@ static int tv_train_pacing(struct mtl_main_impl* impl, struct st_tx_video_sessio
     reactive = (s->ops.height == 480) ? 487.0 / 525.0 : 576.0 / 625.0;
   }
   pkts_per_frame = pkts_per_frame * reactive;
-  if (pkts_per_frame < s->st20_total_pkts) {
-    err("%s(%d), error pkts_per_frame %f, st20_total_pkts %d\n", __func__, idx,
-        pkts_per_frame, s->st20_total_pkts);
-    return -EINVAL;
+  measured_bps = s->st20_pkt_size * pkts_per_sec * reactive;
+
+  /* If the measured speed is lower than expected. Set higher bps and retrain to add
+   * padding */
+  if (measured_bps < rl_bps) {
+    info("%s(%d), measured bps %" PRIu64 " is lower then set bps %" PRIu64 "\n", __func__,
+         idx, (uint64_t)measured_bps, rl_bps);
+
+    if (!mt_pacing_train_bps_result_search(impl, port, rl_bps, &bps_to_set)) {
+      err("%s(%d), measured speed is too low on already trained bps\n", __func__, idx);
+      return -EINVAL;
+    }
+
+/* Slightly increase the target bitrate to compensate for measurement inaccuracies,
+ * rounding errors, and system overhead. This helps ensure the actual transmission bitrate
+ * meets or exceeds the required rate
+ */
+#define INCREASE_BPS_FACTOR 1.005
+    bps_to_set = INCREASE_BPS_FACTOR * (rl_bps * rl_bps) / measured_bps;
+    info("%s(%d), increase bps to %" PRIu64 "\n", __func__, idx, bps_to_set);
+    mt_pacing_train_bps_result_add(impl, port, rl_bps, bps_to_set);
+    mt_txq_set_tx_bps(queue, bps_to_set);
+    ret = tv_train_pacing(impl, s, s_port);
+    return ret;
   }
 
   pad_interval = (float)s->st20_total_pkts / (pkts_per_frame - s->st20_total_pkts);
@@ -437,7 +459,7 @@ static int tv_train_pacing(struct mtl_main_impl* impl, struct st_tx_video_sessio
   }
 
   s->pacing.pad_interval = pad_interval;
-  mt_pacing_train_result_add(impl, port, rl_bps, pad_interval);
+  mt_pacing_train_pad_result_add(impl, port, rl_bps, pad_interval);
   train_end_time = mt_get_tsc(impl);
   info("%s(%d,%d), trained pad_interval %f pkts_per_frame %f with time %fs\n", __func__,
        idx, s_port, pad_interval, pkts_per_frame,
@@ -2526,6 +2548,7 @@ static int tv_init_hw(struct mtl_main_impl* impl, struct st_tx_video_sessions_mg
     struct mt_txq_flow flow;
     memset(&flow, 0, sizeof(flow));
     flow.bytes_per_sec = tv_rl_bps(s);
+    mt_pacing_train_bps_result_search(impl, i, flow.bytes_per_sec, &flow.bytes_per_sec);
     mtl_memcpy(&flow.dip_addr, &s->ops.dip_addr[i], MTL_IP_ADDR_LEN);
     flow.dst_port = s->ops.udp_port[i];
     if (ST21_TX_PACING_WAY_TSN == s->pacing_way[i])
@@ -3858,6 +3881,8 @@ int st20_tx_queue_fatal_error(struct mtl_main_impl* impl,
   struct mt_txq_flow flow;
   memset(&flow, 0, sizeof(flow));
   flow.bytes_per_sec = tv_rl_bps(s);
+  mt_pacing_train_bps_result_search(impl, s_port, flow.bytes_per_sec,
+                                    &flow.bytes_per_sec);
   mtl_memcpy(&flow.dip_addr, &s->ops.dip_addr[s_port], MTL_IP_ADDR_LEN);
   flow.dst_port = s->ops.udp_port[s_port];
   s->queue[s_port] = mt_txq_get(impl, port, &flow);

include/st20_api.h
@@ -62,9 +62,11 @@ extern "C" {
 #define ST20_TX_FLAG_ENABLE_VSYNC (MTL_BIT32(5))
 /**
  * Flag bit in flags of struct st20_tx_ops.
- * If disable the static RL pad interval profiling.
+ * If enable the static RL pad interval profiling.
+ * Static padding is trained only for e810, it is not recommended to use this flag
+ * for other NICs.
  */
-#define ST20_TX_FLAG_DISABLE_STATIC_PAD_P (MTL_BIT32(6))
+#define ST20_TX_FLAG_ENABLE_STATIC_PAD_P (MTL_BIT32(6))
 /**
  * Flag bit in flags of struct st20_tx_ops.
  * If enable the rtcp.

include/st_pipeline_api.h
@@ -446,9 +446,11 @@ enum st20p_tx_flag {
    */
   ST20P_TX_FLAG_ENABLE_VSYNC = (MTL_BIT32(5)),
   /**
-   * If disable the static RL pad interval profiling.
+   * If enable the static RL pad interval profiling.
+   * Static padding is trained only for e810, it is not recommended to use this flag
+   * for other NICs.
    */
-  ST20P_TX_FLAG_DISABLE_STATIC_PAD_P = (MTL_BIT32(6)),
+  ST20P_TX_FLAG_ENABLE_STATIC_PAD_P = (MTL_BIT32(6)),
   /**
    * If enable the rtcp.
    */

lib/src/mt_main.h
@@ -48,7 +48,7 @@
 #define MT_MAX_SCH_NUM (18) /* max 18 scheduler lcore */
 
 /* max RL items */
-#define MT_MAX_RL_ITEMS (64)
+#define MT_MAX_RL_ITEMS (128)
 
 #define MT_ARP_ENTRY_MAX (60)
 
@@ -616,13 +616,9 @@ struct mt_sch_mgr {
   enum mt_lcore_type local_lcores_type[RTE_MAX_LCORE];
 };
 
-struct mt_audio_pacing_train_result {
-  uint64_t input_bps;    /* input, byte per sec */
-  uint64_t profiled_bps; /* profiled result */
-};
-
 struct mt_pacing_train_result {
-  uint64_t rl_bps;           /* input, byte per sec */
+  uint64_t input_bps;        /* input, byte per sec */
+  uint64_t profiled_bps;     /* profiled result */
   float pacing_pad_interval; /* result */
 };
 
@@ -742,8 +738,6 @@ struct mt_interface {
   bool tx_rl_root_active;
   /* video rl pacing train result */
   struct mt_pacing_train_result pt_results[MT_MAX_RL_ITEMS];
-  /* audio rl pacing train result */
-  struct mt_audio_pacing_train_result audio_pt_results[MT_MAX_RL_ITEMS];
 
   /* function ops per interface(pf/vf) */
   uint64_t (*ptp_get_time_fn)(struct mtl_main_impl* impl, enum mtl_port port);

lib/src/mt_util.c
@@ -251,13 +251,13 @@ int mt_build_port_map(struct mtl_main_impl* impl, char** ports, enum mtl_port* m
   return 0;
 }
 
-int mt_pacing_train_result_add(struct mtl_main_impl* impl, enum mtl_port port,
-                               uint64_t rl_bps, float pad_interval) {
+int mt_pacing_train_pad_result_add(struct mtl_main_impl* impl, enum mtl_port port,
+                                   uint64_t input_bps, float pad_interval) {
   struct mt_pacing_train_result* ptr = &mt_if(impl, port)->pt_results[0];
 
   for (int i = 0; i < MT_MAX_RL_ITEMS; i++) {
-    if (ptr[i].rl_bps) continue;
-    ptr[i].rl_bps = rl_bps;
+    if (ptr[i].input_bps) continue;
+    ptr[i].input_bps = input_bps;
     ptr[i].pacing_pad_interval = pad_interval;
     return 0;
   }
@@ -266,12 +266,12 @@ int mt_pacing_train_result_add(struct mtl_main_impl* impl, enum mtl_port port,
   return -ENOMEM;
 }
 
-int mt_pacing_train_result_search(struct mtl_main_impl* impl, enum mtl_port port,
-                                  uint64_t rl_bps, float* pad_interval) {
+int mt_pacing_train_pad_result_search(struct mtl_main_impl* impl, enum mtl_port port,
+                                      uint64_t rl_bps, float* pad_interval) {
   struct mt_pacing_train_result* ptr = &mt_if(impl, port)->pt_results[0];
 
   for (int i = 0; i < MT_MAX_RL_ITEMS; i++) {
-    if (rl_bps == ptr[i].rl_bps) {
+    if (rl_bps == ptr[i].input_bps && ptr[i].pacing_pad_interval) {
       *pad_interval = ptr[i].pacing_pad_interval;
       return 0;
     }
@@ -281,9 +281,9 @@ int mt_pacing_train_result_search(struct mtl_main_impl* impl, enum mtl_port port
   return -EINVAL;
 }
 
-int mt_audio_pacing_train_result_add(struct mtl_main_impl* impl, enum mtl_port port,
-                                     uint64_t input_bps, uint64_t profiled_bps) {
-  struct mt_audio_pacing_train_result* ptr = &mt_if(impl, port)->audio_pt_results[0];
+int mt_pacing_train_bps_result_add(struct mtl_main_impl* impl, enum mtl_port port,
+                                   uint64_t input_bps, uint64_t profiled_bps) {
+  struct mt_pacing_train_result* ptr = &mt_if(impl, port)->pt_results[0];
 
   for (int i = 0; i < MT_MAX_RL_ITEMS; i++) {
     if (ptr[i].input_bps) continue;
@@ -296,12 +296,12 @@ int mt_audio_pacing_train_result_add(struct mtl_main_impl* impl, enum mtl_port p
   return -ENOMEM;
 }
 
-int mt_audio_pacing_train_result_search(struct mtl_main_impl* impl, enum mtl_port port,
-                                        uint64_t input_bps, uint64_t* profiled_bps) {
-  struct mt_audio_pacing_train_result* ptr = &mt_if(impl, port)->audio_pt_results[0];
+int mt_pacing_train_bps_result_search(struct mtl_main_impl* impl, enum mtl_port port,
+                                      uint64_t input_bps, uint64_t* profiled_bps) {
+  struct mt_pacing_train_result* ptr = &mt_if(impl, port)->pt_results[0];
 
   for (int i = 0; i < MT_MAX_RL_ITEMS; i++) {
-    if (input_bps == ptr[i].input_bps) {
+    if (input_bps == ptr[i].input_bps && ptr[i].profiled_bps) {
       *profiled_bps = ptr[i].profiled_bps;
       return 0;
     }

lib/src/mt_util.h
@@ -56,15 +56,15 @@ int mt_ring_dequeue_clean(struct rte_ring* ring);
 
 void mt_mbuf_sanity_check(struct rte_mbuf** mbufs, uint16_t nb, char* tag);
 
-int mt_pacing_train_result_add(struct mtl_main_impl* impl, enum mtl_port port,
-                               uint64_t rl_bps, float pad_interval);
-int mt_pacing_train_result_search(struct mtl_main_impl* impl, enum mtl_port port,
-                                  uint64_t rl_bps, float* pad_interval);
+int mt_pacing_train_pad_result_add(struct mtl_main_impl* impl, enum mtl_port port,
+                                   uint64_t input_bps, float pad_interval);
+int mt_pacing_train_pad_result_search(struct mtl_main_impl* impl, enum mtl_port port,
+                                      uint64_t input_bps, float* pad_interval);
 
-int mt_audio_pacing_train_result_add(struct mtl_main_impl* impl, enum mtl_port port,
-                                     uint64_t input_bps, uint64_t profiled_bps);
-int mt_audio_pacing_train_result_search(struct mtl_main_impl* impl, enum mtl_port port,
-                                        uint64_t input_bps, uint64_t* profiled_bps);
+int mt_pacing_train_bps_result_add(struct mtl_main_impl* impl, enum mtl_port port,
+                                   uint64_t input_bps, uint64_t profiled_bps);
+int mt_pacing_train_bps_result_search(struct mtl_main_impl* impl, enum mtl_port port,
+                                      uint64_t input_bps, uint64_t* profiled_bps);
 
 enum mtl_port mt_port_by_name(struct mtl_main_impl* impl, const char* name);
 int mt_build_port_map(struct mtl_main_impl* impl, char** ports, enum mtl_port* maps,

lib/src/st2110/pipeline/st20_pipeline_tx.c
@@ -299,8 +299,8 @@ static int tx_st20p_create_transport(struct mtl_main_impl* impl, struct st20p_tx
   if (ops->flags & ST20P_TX_FLAG_USER_TIMESTAMP)
     ops_tx.flags |= ST20_TX_FLAG_USER_TIMESTAMP;
   if (ops->flags & ST20P_TX_FLAG_ENABLE_VSYNC) ops_tx.flags |= ST20_TX_FLAG_ENABLE_VSYNC;
-  if (ops->flags & ST20P_TX_FLAG_DISABLE_STATIC_PAD_P)
-    ops_tx.flags |= ST20_TX_FLAG_DISABLE_STATIC_PAD_P;
+  if (ops->flags & ST20P_TX_FLAG_ENABLE_STATIC_PAD_P)
+    ops_tx.flags |= ST20_TX_FLAG_ENABLE_STATIC_PAD_P;
   if (ops->flags & ST20P_TX_FLAG_ENABLE_RTCP) {
     ops_tx.flags |= ST20_TX_FLAG_ENABLE_RTCP;
     ops_tx.rtcp = ops->rtcp;

lib/src/st2110/st_tx_audio_session.c
@@ -1257,8 +1257,8 @@ static int tx_audio_session_init_rl(struct mtl_main_impl* impl,
     port = mt_port_logic2phy(s->port_maps, i);
 
     uint64_t initial_bytes_per_sec = tx_audio_session_initial_rl_bps(s);
-    int profiled = mt_audio_pacing_train_result_search(impl, port, initial_bytes_per_sec,
-                                                       &profiled_per_sec);
+    int profiled = mt_pacing_train_bps_result_search(impl, port, initial_bytes_per_sec,
+                                                     &profiled_per_sec);
 
     /* pad pkt */
     rl_port->pad = mt_build_pad(impl, mt_sys_tx_mempool(impl, port), port,
@@ -1297,7 +1297,7 @@ static int tx_audio_session_init_rl(struct mtl_main_impl* impl,
           return -EIO;
         }
 
-        mt_audio_pacing_train_result_add(impl, port, initial_bytes_per_sec, trained);
+        mt_pacing_train_bps_result_add(impl, port, initial_bytes_per_sec, trained);
         info("%s(%d), trained bytes_per_sec %" PRIu64 "\n", __func__, idx, trained);
         int ret = mt_txq_set_tx_bps(rl_port->queue[j], trained);
         if (ret < 0) {

commit 5f72fddbdec4269403765d430fa4db15d66face9

Fix: Stop the RL pacing retrain wrapping above 34 Gbps
When the measured rate is too far off to pad, tv_train_pacing()
retrains the queue at 1.005 * rl_bps^2 / measured_bps, but it squared
rl_bps in uint64. That wraps once rl_bps reaches 2^32 B/s (34.36 Gbps),
e.g. for 7680x4320 p30 12-bit YUV 4:4:4 or RGB (37.52 Gbps with GPM),
where the target came out 6.2x too low. The next pass then failed as
out of range, and the session fell back to TSC pacing on a queue still
shaped to the wrapped rate, sending about 5 fps instead of 30.

Square in double, in tv_retrain_bps() so a unit test can reach it.
Below 2^32 B/s the uint64 product is exact and rounds to the same
double, so those targets are bit-identical to before.

Fixes: 5bbfe980a099 ("Fix: handling case where RL is lower then expected (#1205)")
Signed-off-by: Wilczynski, Andrzej <andrzej.wilczynski@intel.com>

lib/src/st2110/st_tx_video_session.c
@@ -330,6 +330,14 @@ static int uint64_t_cmp(const void* a, const void* b) {
   return 0;
 }
 
+/* headroom so the retrained rate still meets or exceeds the required rate */
+#define INCREASE_BPS_FACTOR 1.005
+
+static uint64_t tv_retrain_bps(uint64_t rl_bps, double measured_bps) {
+  /* a uint64 square wraps once rl_bps reaches 2^32 B/s (34.36 Gbps) */
+  return INCREASE_BPS_FACTOR * ((double)rl_bps * rl_bps) / measured_bps;
+}
+
 static int tv_train_pacing(struct mtl_main_impl* impl, struct st_tx_video_session_impl* s,
                            enum mtl_session_port s_port) {
   enum mtl_port port = mt_port_logic2phy(s->port_maps, s_port);
@@ -477,12 +485,7 @@ static int tv_train_pacing(struct mtl_main_impl* impl, struct st_tx_video_sessio
     return -EINVAL;
   }
 
-/* Slightly increase the target bitrate to compensate for measurement inaccuracies,
- * rounding errors, and system overhead. This helps ensure the actual transmission bitrate
- * meets or exceeds the required rate
- */
-#define INCREASE_BPS_FACTOR 1.005
-  bps_to_set = INCREASE_BPS_FACTOR * (rl_bps * rl_bps) / measured_bps;
+  bps_to_set = tv_retrain_bps(rl_bps, measured_bps);
   info("%s(%d), Retrain pacing with bps changed to %" PRIu64 "\n", __func__, idx,
        bps_to_set);
   mt_pacing_train_bps_result_add(impl, port, rl_bps, bps_to_set);

tests/unit/session/st20_tx/pacing_test.cpp
@@ -870,6 +870,16 @@ INSTANTIATE_TEST_SUITE_P(AllSoftwareWaitPaths, St20TxTransmitterBoundaryTest,
                          ::testing::Values(UT_TXV_PACING_TSC, UT_TXV_PACING_PTP,
                                            UT_TXV_PACING_RL));
 
+TEST(St20TxRetrainBpsTest, RlBpsAbove34GbpsDoesNotWrap) {
+  /* 8K p30 YUV 4:4:4 12-bit GPM; a wrapped uint64 square retrains to 710533710 */
+  EXPECT_EQ(ut_txv_retrain_bps(4690316160ULL, 5024509272.6566), 4400242851ULL);
+}
+
+TEST(St20TxRetrainBpsTest, RlBpsBelow34GbpsScalesByRlOverMeasured) {
+  /* 8K p30 YUV 4:4:4 10-bit GPM */
+  EXPECT_EQ(ut_txv_retrain_bps(3907812300ULL, 4186994732.3145), 3665481553ULL);
+}
+
 TEST_F(St20TxSyncPacingTest, ExactTargetBeyondOneSecondFallsBackBeforePacketBuild) {
   uint64_t packet_tsc = 0;
   uint64_t packet_ptp = 0;

tests/unit/session/st20_tx_harness.c
@@ -297,6 +297,10 @@ uint64_t ut_txv_pacing_required_tai(ut_txv_ctx* ctx, enum st10_timestamp_fmt tfm
   return tv_pacing_required_tai(&ctx->session, tfmt, timestamp);
 }
 
+uint64_t ut_txv_retrain_bps(uint64_t rl_bps, double measured_bps) {
+  return tv_retrain_bps(rl_bps, measured_bps);
+}
+
 /* Header mempool and TX ring on port P, as tv_mempool_init()/tv_init_hw() would.
  * Shared by every driver below that lets the production builders run. */
 static int ut_txv_tx_path_init(struct ut_txv_ctx* ctx) {

tests/unit/session/st20_tx_harness.h
@@ -96,6 +96,8 @@ uint64_t ut_txv_calc_frame_count_since_epoch(ut_txv_ctx* ctx, uint64_t cur_tai,
 int ut_txv_sync_pacing(ut_txv_ctx* ctx, uint64_t required_tai);
 uint64_t ut_txv_pacing_required_tai(ut_txv_ctx* ctx, enum st10_timestamp_fmt tfmt,
                                     uint64_t timestamp);
+/* Drives tv_retrain_bps(); both rates in bytes/s. */
+uint64_t ut_txv_retrain_bps(uint64_t rl_bps, double measured_bps);
 int ut_txv_run_frame_tasklet(ut_txv_ctx* ctx, enum st10_timestamp_fmt tfmt,
                              uint64_t timestamp, uint64_t* packet_tsc,
                              uint64_t* packet_ptp);